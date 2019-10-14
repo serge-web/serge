1600e92f3030559b781b62c79580fb28f34b6a84
foxleigh81 <alex@alexfoxleigh.com>
2019-10-14 14:38:21 +0100

    ran standard --fix to fix simple linting errors on server package

--- packages/server/bin.js
+++ packages/server/bin.js
@@ -1,32 +1,31 @@
-const server = require("./server");
-const opn = require('opn');
+const server = require('./server')
+const opn = require('opn')
 
-if(process.argv[2]) {
-  console.log(`running client with remote server "${process.argv[2]}"`);
-}
-else {
-  console.log("running client with local server");
-  console.log("(You can give the second parameter as the remote server URL)");
+if (process.argv[2]) {
+  console.log(`running client with remote server "${process.argv[2]}"`)
+} else {
+  console.log('running client with local server')
+  console.log('(You can give the second parameter as the remote server URL)')
 }
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8080
 
 server(
-  82,                                   //event emmiter max listeners
-  { prefix: 'db/', adapter: 'websql' }, //PouchDb Options
-  {                                     //cors options
+  82, // event emmiter max listeners
+  { prefix: 'db/', adapter: 'websql' }, // PouchDb Options
+  { // cors options
     credentials: true,
     origin: [
       'https://serge-dev.herokuapp.com',
       'http://localhost:3000',
       'http://localhost:8080',
-      'http://localhost:8000',
+      'http://localhost:8000'
     ]
   },
-  './db',                               //database directory
-  './img',                              //images directory
-  port,                                 //port
-  process.argv[2] || null,              //remote server path
-);
+  './db', // database directory
+  './img', // images directory
+  port, // port
+  process.argv[2] || null // remote server path
+)
 
-opn(`http://localhost:${port}/serge/admin`);
+opn(`http://localhost:${port}/serge/admin`)
--- packages/server/main.js
+++ packages/server/main.js
@@ -1,19 +1,19 @@
-const server = require("./server");
+const server = require('./server')
 
 server(
-  82,                                   //event emmiter max listeners
-  { prefix: 'db/', adapter: 'websql' }, //PouchDb Options
-  {                                     //cors options
+  82, // event emmiter max listeners
+  { prefix: 'db/', adapter: 'websql' }, // PouchDb Options
+  { // cors options
     credentials: true,
     origin: [
       'https://serge-dev.herokuapp.com',
       'http://localhost:3000',
       'http://localhost:8080',
-      'http://localhost:8000',
+      'http://localhost:8000'
     ]
   },
-  './db',                               //database directory
-  './img',                              //images directory
-  process.env.PORT || 8080,             //port
-  null,                                 //remote server path
-);
+  './db', // database directory
+  './img', // images directory
+  process.env.PORT || 8080, // port
+  null // remote server path
+)
