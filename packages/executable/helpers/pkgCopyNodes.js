83d896c1fae76bdeb1fcf1e27a1a50908f4366ca
Lilit Karapetyan <lilit.karapetyan.web@gmail.com>
2019-10-30 10:05:34 +0400

    fix syntax and update gitignore

--- packages/executable/helpers/pkgCopyNodes.js
+++ packages/executable/helpers/pkgCopyNodes.js
@@ -1,40 +1,39 @@
-const find = require('find');
-const fs = require('fs');
+const find = require('find')
+const fs = require('fs')
+const path = require('path')
 
 const nodeFiles = [
   {
-    name: "sqllite",
-    path: __dirname + "/../../../node_modules/sqlite3/lib/binding",
-    file: "node_sqlite3.node"
+    name: 'sqllite',
+    path: path.resolve(__dirname, '../../../node_modules/sqlite3/lib/binding'),
+    file: 'node_sqlite3.node'
   }
-];
+]
 
-const pkgServerPath = __dirname + "/../builds";
+const pkgServerPath = path.resolve(__dirname, '../builds')
 
-console.log("Copy node files...");
+console.log('Copy node files...')
 
-let start = 1;
+let start = 1
 
-for(let {name, path, file} of nodeFiles) {
-  let prefix = `[${start}/${nodeFiles.length}] ${name}:`;
-  start ++;
-  console.log(`${prefix} searching "${file}" file in directory ${path}...`);
-  find.file(file, path, function(files) {
-    if(files.length) {
-      console.log(`${prefix} file "${file}" was founded`);
-      console.log(`${prefix} copying to ${pkgServerPath}...`);
+for (const { name, path, file } of nodeFiles) {
+  const prefix = `[${start}/${nodeFiles.length}] ${name}:`
+  start++
+  console.log(`${prefix} searching "${file}" file in directory ${path}...`)
+  find.file(file, path, function (files) {
+    if (files.length) {
+      console.log(`${prefix} file "${file}" was founded`)
+      console.log(`${prefix} copying to ${pkgServerPath}...`)
       fs.copyFile(files[0], `${pkgServerPath}/${file}`, (err) => {
-        if(err) {
-          console.log(`${prefix} copying failed`);
-          console.log(err);
+        if (err) {
+          console.log(`${prefix} copying failed`)
+          console.log(err)
+        } else {
+          console.log(`${prefix} ${file} successfully copied`)
         }
-        else {
-          console.log(`${prefix} ${file} successfully copied`);
-        }
-      });
-    }
-    else {
-      console.log(`${prefix} file "${file}" not found`);
+      })
+    } else {
+      console.log(`${prefix} file "${file}" not found`)
     }
   })
 }
