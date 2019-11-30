944c7d058bbb89c1a88c542f00c19a00de6e3e97
Lilit Karapetyan <lilit.karapetyan.web@gmail.com>
2019-11-30 11:27:26 +0400

    game turn field and code style fixes

--- packages/client/src/Views/ExportMessages.jsx
+++ packages/client/src/Views/ExportMessages.jsx
@@ -1,69 +1,99 @@
-import React, { Component } from 'react';
-import { connect } from "react-redux";
-import flatten from 'flat';
-import { createExportItem } from "../ActionsAndReducers/ExportItems/ExportItems_ActionsCreators";
-import ExcelExport from '../Components/ExcelExport';
-import ExportItem from '../Components/ExportItem';
-import ExportView from "./ExportView";
+import React from 'react'
+import { connect } from 'react-redux'
+import flatten from 'flat'
+import { createExportItem } from '../ActionsAndReducers/ExportItems/ExportItems_ActionsCreators'
+import ExcelExport from '../Components/ExcelExport'
+import ExportItem from '../Components/ExportItem'
+import ExportView from './ExportView'
 
-class ExportMessages extends Component {
-
-  createExportItem = () => {
-    const infoTypeMessages = this.props.wargame.exportMessagelist.filter(({infoType, data}) => (
+const ExportMessages = ({ wargame, exportItems, savExportItem }) => {
+  // generate export item for current wargame
+  const createExportItem = () => {
+    const infoTypeMessages = wargame.exportMessagelist.filter(({ infoType, data }) => (
       infoType && data && data.channels && Array.isArray(data.channels.channels)
-    ));
+    ))
 
-    let channelTitles = {};
+    // add game turn to message items
+    let key = 0
+    wargame.exportMessagelist.map(message => {
+      if (message._id === infoTypeMessages[key]._id) key++
+      else if (message.details) message.details.gameTurn = infoTypeMessages[key].gameTurn
+      return message
+    })
 
-    for(const { data } of infoTypeMessages) {
-      for(const { uniqid, name } of data.channels.channels) {
-        if(channelTitles[uniqid]) continue;
-        channelTitles[uniqid] = name;
+    const channelTitles = {}
+
+    for (const { data } of infoTypeMessages) {
+      for (const { uniqid, name } of data.channels.channels) {
+        if (channelTitles[uniqid]) continue
+        channelTitles[uniqid] = name
       }
     }
 
-    this.props.savExportItem({
-      wargame: this.props.wargame.currentWargame,
-      data: this.exportDataGrouped(this.props.wargame.exportMessagelist, channelTitles),
-    });
+    savExportItem({
+      wargame: wargame.currentWargame,
+      data: exportDataGrouped(wargame.exportMessagelist, channelTitles)
+    })
   }
 
-  messageFilterByType = (type) => {
+  const messageFilterByType = type => {
     return msg => msg.details && msg.details.messageType === type
-  };
+  }
 
-  exportDataGrouped(messages, channelTitles) {
-    let data = [];
-    let messageTypes = {};
+  const keysSimplyfy = row => {
+    const newRow = {}
 
-    for(let message of messages) {
-      if(message.details && message.details.messageType && !messageTypes[message.details.messageType]) {
-        messageTypes[message.details.messageType] = true;
+    for (const key of Object.keys(row)) {
+      const subkeys = key.split('.')
+      let mainKey = subkeys[subkeys.length - 1]
+      const newKey = []
 
-        const messagesFiltered = messages.filter(this.messageFilterByType(message.details.messageType));
+      if (!isNaN(mainKey)) mainKey = ''
 
-        let fields = [];
-        let rows = [];
+      for (var i = 0; i < subkeys.length; i++) {
+        if (!isNaN(subkeys[i])) {
+          newKey.push(`${subkeys[i - 1] || 'messages'}_${subkeys[i]}`)
+        }
+      }
 
-        for(let msg of messagesFiltered) {
+      if (mainKey) {
+        newKey.push(mainKey)
+      }
 
-          if(msg.details && msg.details.channel && channelTitles[msg.details.channel]) {
-            msg.details.channel = channelTitles[msg.details.channel];
+      newRow[newKey.join(' ')] = row[key]
+    }
+
+    return newRow
+  }
+
+  const exportDataGrouped = (messages, channelTitles) => {
+    const data = []
+    const messageTypes = {}
+
+    for (const message of messages) {
+      if (message.details && message.details.messageType && !messageTypes[message.details.messageType]) {
+        messageTypes[message.details.messageType] = true
+
+        const messagesFiltered = messages.filter(messageFilterByType(message.details.messageType))
+
+        const fields = []
+        const rows = []
+
+        for (const msg of messagesFiltered) {
+          if (msg.details && msg.details.channel && channelTitles[msg.details.channel]) {
+            msg.details.channel = channelTitles[msg.details.channel]
           }
 
-          const flatMsg = this.keysSimplyfy(flatten(msg));
-          let row = Array(fields.length).fill("");
+          const flatMsg = keysSimplyfy(flatten(msg))
+          const row = Array(fields.length).fill('')
 
-          for(let key of Object.keys(flatMsg)) {
-            //check if fields/titles have no current key then add
-            if(!fields.includes(key)) {
-              fields.push(key);
-            }
-            //check position for field then add value to rigth position in row
-            row[fields.indexOf(key)] = flatMsg[key];
+          for (const key of Object.keys(flatMsg)) {
+            // check if fields/titles have no current key then add
+            if (!fields.includes(key)) fields.push(key)
+            // check position for field then add value to rigth position in row
+            row[fields.indexOf(key)] = flatMsg[key]
           }
-
-          rows.push(row);
+          rows.push(row)
         }
 
         data.push({
@@ -76,52 +106,22 @@ class ExportMessages extends Component {
       }
     }
 
-    return data;
+    return data
   }
 
-  keysSimplyfy(row) {
-    let newRow = {};
-
-    for(let key of Object.keys(row)) {
-      let subkeys = key.split('.');
-      let mainKey = subkeys[subkeys.length - 1];
-      let newKey = [];
-
-      if(!isNaN(mainKey)) {
-        mainKey = "";
-      }
-
-      for (var i = 0; i < subkeys.length; i++) {
-        if(!isNaN(subkeys[i])) {
-          newKey.push(`${subkeys[i-1] || 'messages'}_${subkeys[i]}`);
-        }
-      }
-
-      if(mainKey) {
-        newKey.push(mainKey);
-      }
-
-      newRow[newKey.join(" ")] = row[key];
-    }
-
-    return newRow;
-  }
-
-  render() {
-    return (
-      <ExportView>
-        <h2>Export messages</h2>
-        <span className="link link--noIcon" onClick={this.createExportItem}>Create Export</span>
-        <ul>
-          {this.props.exportItems.map((item, key) => (
-            <ExportItem item={item} key={key}>
-              <ExcelExport exp={item} index={key}/>
-            </ExportItem>
-          ))}
-        </ul>
-      </ExportView>
-    );
-  }
+  return (
+    <ExportView>
+      <h2>Export messages</h2>
+      <span className="link link--noIcon" onClick={createExportItem}>Create Export</span>
+      <ul>
+        {exportItems.map((item, key) => (
+          <ExportItem item={item} key={key}>
+            <ExcelExport exp={item} index={key}/>
+          </ExportItem>
+        ))}
+      </ul>
+    </ExportView>
+  )
 }
 
 const mapDispatchToProps = dispatch => ({
@@ -129,13 +129,13 @@ const mapDispatchToProps = dispatch => ({
     dispatch(createExportItem({
       type: 'messages',
       title: `Export ${new Date().toISOString().slice(0, 19).replace('T', ' ')}`,
-      ...data,
-    }));
+      ...data
+    }))
   }
-});
+})
 
 const mapStateToProps = ({ wargame, exportItems }) => ({
   wargame, exportItems: exportItems.filter(item => item.type === 'messages')
-});
+})
 
-export default connect(mapStateToProps, mapDispatchToProps)(ExportMessages);
+export default connect(mapStateToProps, mapDispatchToProps)(ExportMessages)
