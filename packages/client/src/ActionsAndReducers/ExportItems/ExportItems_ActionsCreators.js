62fa9dba0f5c9beb3269e6e1ceb3806bd4bdc5bc
Lilit Karapetyan <lilit.karapetyan.web@gmail.com>
2019-12-10 11:48:48 +0400

    codeclimate issues fix

--- packages/client/src/ActionsAndReducers/ExportItems/ExportItems_ActionsCreators.js
+++ packages/client/src/ActionsAndReducers/ExportItems/ExportItems_ActionsCreators.js
@@ -6,34 +6,39 @@ export const createExportItem = (exportData) => ({
   payload: exportData
 })
 
-export const createMessageExportItem = (wargame) => {
-  const infoTypeMessages = wargame.exportMessagelist.filter(({ infoType, data }) => (
-    infoType && data && data.channels && Array.isArray(data.channels.channels)
-  ))
+const getInfoTypeMessagesFromWargameExportMessageList = list => list.filter(({ infoType, data }) => (
+  infoType && data && data.channels && Array.isArray(data.channels.channels)
+))
 
+// just for codeclimate Cognitive Complexity
+const addChannelTitle = ({ uniqid, name }, channelTitles) => {
+  if (!channelTitles[uniqid]) channelTitles[uniqid] = name
+  return channelTitles
+}
+
+const getChannelTitlesFromInfoTypeMessages = messages => {
+  let channelTitles = {}
+  for (const { data } of messages) {
+    for (const channel of data.channels.channels) {
+      channelTitles = addChannelTitle(channel, channelTitles)
+    }
+  }
+  return channelTitles
+}
+
+export const createMessageExportItem = ({ currentWargame, exportMessagelist }) => {
+  const infoTypeMessages = getInfoTypeMessagesFromWargameExportMessageList(exportMessagelist)
+  const channelTitles = getChannelTitlesFromInfoTypeMessages(infoTypeMessages)
   // add game turn to message items
   let key = 0
-  wargame.exportMessagelist.map(message => {
+  exportMessagelist.map(message => {
     if (message._id === infoTypeMessages[key]._id) key++
     else if (message.details) message.details.gameTurn = infoTypeMessages[key].gameTurn
     return message
   })
-
-  const channelTitles = {}
-
-  for (const { data } of infoTypeMessages) {
-    for (const { uniqid, name } of data.channels.channels) {
-      if (channelTitles[uniqid]) continue
-      channelTitles[uniqid] = name
-    }
-  }
-
-  return createExportItem({
-    type: 'messages',
-    title: `Export ${new Date().toISOString().slice(0, 19).replace('T', ' ')}`,
-    wargame: wargame.currentWargame,
-    data: exportDataGrouped(wargame.exportMessagelist, channelTitles)
-  })
+  const data = exportDataGrouped(exportMessagelist, channelTitles)
+  const title = `Export ${new Date().toISOString().slice(0, 19).replace('T', ' ')}`
+  return createExportItem({ type: 'messages', title, wargame: currentWargame, data })
 }
 
 const messageFilterByType = type => {
@@ -46,24 +51,25 @@ const keysSimplyfy = row => {
   for (const key of Object.keys(row)) {
     const subkeys = key.split('.')
     let mainKey = subkeys[subkeys.length - 1]
-    const newKey = []
-
     if (!isNaN(mainKey)) mainKey = ''
-
-    for (var i = 0; i < subkeys.length; i++) {
-      if (!isNaN(subkeys[i])) {
-        newKey.push(`${subkeys[i - 1] || 'messages'}_${subkeys[i]}`)
-      }
-    }
-
-    if (mainKey) {
-      newKey.push(mainKey)
-    }
-
+    const newKey = keysSimplyfyGetNewKey(key, subkeys, mainKey)
     newRow[newKey.join(' ')] = row[key]
   }
 
   return newRow
+}
+
+const keysSimplyfyGetNewKey = (key, subkeys, mainKey) => {
+  const newKey = []
+  for (var i = 0; i < subkeys.length; i++) {
+    if (!isNaN(subkeys[i])) {
+      newKey.push(`${subkeys[i - 1] || 'messages'}_${subkeys[i]}`)
+    }
+  }
+  if (mainKey) {
+    newKey.push(mainKey)
+  }
+  return newKey
 }
 
 const exportDataGrouped = (messages, channelTitles) => {
@@ -73,38 +79,46 @@ const exportDataGrouped = (messages, channelTitles) => {
   for (const message of messages) {
     if (message.details && message.details.messageType && !messageTypes[message.details.messageType]) {
       messageTypes[message.details.messageType] = true
-
-      const messagesFiltered = messages.filter(messageFilterByType(message.details.messageType))
-
-      const fields = []
-      const rows = []
-
-      for (const msg of messagesFiltered) {
-        if (msg.details && msg.details.channel && channelTitles[msg.details.channel]) {
-          msg.details.channel = channelTitles[msg.details.channel]
-        }
-
-        const flatMsg = keysSimplyfy(flatten(msg))
-        const row = Array(fields.length).fill('')
-
-        for (const key of Object.keys(flatMsg)) {
-          // check if fields/titles have no current key then add
-          if (!fields.includes(key)) fields.push(key)
-          // check position for field then add value to rigth position in row
-          row[fields.indexOf(key)] = flatMsg[key]
-        }
-        rows.push(row)
-      }
-
+      const rowsAndFields = exportDataGroupedGetRowsAndFields(messages, message, channelTitles)
       data.push({
         title: message.details.messageType,
         items: [
-          fields.map(field => (field.toUpperCase())),
-          ...rows
+          rowsAndFields.fields.map(field => (field.toUpperCase())),
+          ...rowsAndFields.rows
         ]
       })
     }
   }
-
   return data
+}
+
+const exportDataGroupedGetRowsAndFields = (messages, message, channelTitles) => {
+  const messagesFiltered = messages.filter(messageFilterByType(message.details.messageType))
+
+  const fields = []
+  const rows = []
+
+  for (const msg of messagesFiltered) {
+    if (msg.details && msg.details.channel && channelTitles[msg.details.channel]) {
+      msg.details.channel = channelTitles[msg.details.channel]
+    }
+    const row = exportDataGroupedGetRow(msg, fields)
+    rows.push(row)
+  }
+
+  return { fields, rows }
+}
+
+const exportDataGroupedGetRow = (msg, fields) => {
+  const flatMsg = keysSimplyfy(flatten(msg))
+  const row = Array(fields.length).fill('')
+
+  for (const key of Object.keys(flatMsg)) {
+    // check if fields/titles have no current key then add
+    if (!fields.includes(key)) fields.push(key)
+    // check position for field then add value to rigth position in row
+    row[fields.indexOf(key)] = flatMsg[key]
+  }
+
+  return row
 }
