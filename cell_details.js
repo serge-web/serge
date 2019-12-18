15574414bbe4efa253b374313dde6baf35a018db
Ian Mayo <ian@planetmayo.com>
2019-12-18 12:05:15 +0000

    Switch to reading coords from json file

---
+++ cell_details.js
@@ -0,0 +1,11 @@
+/** expected format is index, sea, land */
+const cell_types = {
+    "A00":[true, false],
+    "A01":[true, false],
+    "A02":[false, false],
+    "A03":[false, false],
+    "O03":[false, true],
+    "P03":[false, true],
+    "P02":[false, true],
+    "O04":[false, true],
+}
