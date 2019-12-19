d5672c815bd7b3c4cb0fcf22e5dc01af644134c0
Alex Foxleigh <alex@alexfoxleigh.com>
2019-12-19 15:34:40 +0000

    mapping component now outputting a blank map

--- packages/mapping/webpack.config.js
+++ packages/mapping/webpack.config.js
@@ -18,7 +18,18 @@ module.exports = {
             presets: ['@babel/env']
           }
         }
-      }
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader',
+        ],
+      },
     ]
   },
   externals: {
