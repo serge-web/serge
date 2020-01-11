6edde10fba0b12e3b66a6f0ad96c7f61b9105abd
Ian Mayo <ian@planetmayo.com>
2020-01-11 08:37:27 +0000

    Generate two types of listener

--- packages/client/src/Helpers/MapAdjudicatingListener.js
+++ packages/client/src/Helpers/MapAdjudicatingListener.js
@@ -1,7 +1,7 @@
 import L from 'leaflet'
 import defaultHexStyle from './data/default-hex-style'
 
-export default class MovementListener {
+export default class MapAdjudicatingListener {
   constructor (map, grid) {
     this.grid = grid
 
