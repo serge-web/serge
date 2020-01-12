555bc30542003695c3e094784988544f904ebdf5
Ian Mayo <ian@planetmayo.com>
2020-01-12 16:00:12 +0000

    Use radio list of potential speeds

--- packages/client/src/Helpers/plannedStateFor.js
+++ packages/client/src/Helpers/plannedStateFor.js
@@ -1,15 +1,23 @@
-export default function plannedStateFor (/* String */ currentState, /* number */ curentSpeed, /* dictionary */ platformStates) {
-  var popup = 'Proposed State:<ul>'
-  for (const key in platformStates) {
+export default function plannedStateFor (/* String */ currentState, /* number */ currentSpeed, /* dictionary */ platformStates, /* array */ platformSpeeds) {
+  var popup = 'Proposed State:<fieldset id="state"><ul>'
+  for (const key in platformStates.states) {
     // TODO: only show the speed box if this state is mobile
     // const stateDetail = platformStates[key]
     const checked = key === currentState ? 'checked' : ''
     // TODO: attach onclick handler in next line
-    const stateCtrl = '<input type="radio" name="vehicle3" ' + checked + ' value="' + key + '">' + key + '</input><br/>'
+    const stateCtrl = '<li><input name="state" type="radio" name="vehicle3" ' + checked + ' value="' + key + '">' + key + '</input></li>'
     popup += stateCtrl
   }
   popup += '</ul>'
-  popup += 'Speed:<input type="text" name="vehicle3"  value="' + curentSpeed + '"></input><br/>'
-  popup += '<hr/>'
+  // and the speeds
+  popup += 'Proposed Speed:<ul>'
+  if (platformSpeeds) {
+    platformSpeeds.forEach((speed) => {
+      const checked = speed === currentSpeed ? 'checked' : ''
+      // TODO: attach handler to changes in next field
+      popup += '<li><input name="speed" type="radio" name="vehicle3" ' + checked + '>' + speed + '</input><br/></li>'
+    })
+  }
+  popup += '</ul><hr/>'
   return popup
 };
