37a65fc24280163f9ba85087901e2cd06af11691
Ian Mayo <ian@planetmayo.com>
2020-01-15 17:52:19 +0000

    New infrastructure for Umpire planning

--- packages/client/src/Components/Mapping/helpers/MapPlanningUmpireListener.js
+++ packages/client/src/Components/Mapping/helpers/MapPlanningUmpireListener.js
@@ -1,214 +1,96 @@
-import L from 'leaflet'
-import defaultHexStyle from '../../../Helpers/data/default-hex-style'
+import createButton from '../../../Helpers/createDebugButton'
 
 export default class MapPlanningPlayerListener {
   constructor (map, grid) {
     this.grid = grid
 
-    // create our two lines, one for planning, one for history
-    this.planningLine = L.polyline([], {
-      color: '#fff',
-      dashArray: [1, 4]
-    })
-    this.planningLine.addTo(map)
-    this.historyLine = L.polyline([], {
-      color: '#0f0'
-    })
-    this.historyLine.addTo(map)
+    this.registeredListeners = []
 
-    this.routeHexes = [] // hexes representing route
-    this.routeLats = [] // lad-lngs for route
-    this.achievableCells = [] // hexes representing achievable area
-    this.startHex = null // hex for start drag operation
-    this.lastHex = null // most recent cell travelled through
+
+    // don't bother with these if we're only running in TDD
+    if (!map) {
+      return
+    }
+    const context = this
+    this.btnChangeBlueOn = createButton(false, 'Blue on', function (btn, map) {
+      context.changeVisibility(context.dbgForce, context.dbgName, 'Blue', true)
+      context.disableButtons()
+    }).addTo(map)
+    this.btnChangeBlueOff = createButton(false, 'Blue off', function (btn, map) {
+      context.changeVisibility(context.dbgForce, context.dbgName, 'Blue', false)
+      context.disableButtons()
+    }).addTo(map)
+    this.btnChangeRedOn = createButton(false, 'Red on', function (btn, map) {
+      context.changeVisibility(context.dbgForce, context.dbgName, 'Red', true)
+      context.disableButtons()
+    }).addTo(map)
+    this.btnChangeRedOff = createButton(false, 'Red off', function (btn, map) {
+      context.changeVisibility(context.dbgForce, context.dbgName, 'Red', false)
+      context.disableButtons()
+    }).addTo(map)
+  }
+
+  disableButtons () {
+    this.dbgForce = null
+    this.dbgName = null
+    this.btnChangeRedOn.disable()
+    this.btnChangeRedOff.disable()
+    this.btnChangeBlueOn.disable()
+    this.btnChangeBlueOff.disable()
+  }
+
+  changeVisibility (/* string */ assetForce, /* string */ assetName, /* string */ forcePerceivedBy, /* boolean */ newVis) {
+    console.log('change vis of ' + assetName + ' by:' + forcePerceivedBy + ' to: ' + newVis)
+    // ok, send the message
+  }
+
+  clearListeners () {
+    this.registeredListeners.forEach(marker => {
+      // next lines commented out, until we've refactored JS into functions
+      // marker.off('drag', dragHandler)
+      // marker.off('drag', dragEndHandler)
+    })
+
+    // and empty the array
+    this.registeredListeners = []   
+  }
+
+  changeVisPopupFor (asset, assetPerception) {
+    // find what the force's perception of this asset
+    return '<b>' + asset.name + '</b>'
   }
 
   /** listen to drag events on the supplied marker */
   listenTo (marker) {
-    marker.on('drag', e => {
-      const cursorLoc = e.latlng
+    const popupContent = this.changeVisPopupFor(marker.asset, null)
+    marker.bindPopup(popupContent).openPopup()
 
-      const rangeStyle = {
-        fill: true,
-        color: '#ccc',
-        opacity: 1.0
+    marker.on('click', e => {
+      // we have to trick module by pushing capturing marker - so we know
+      // who to advance.
+      this.currentMarker = marker
+      this.dbgForce = marker.asset.force
+      this.dbgName = marker.asset.name
+
+      const force = marker.asset.force
+      switch (force) {
+        case 'Blue':
+          this.btnChangeRedOn.enable()
+          this.btnChangeRedOff.enable()
+          break
+        case 'Red':
+          this.btnChangeBlueOn.enable()
+          this.btnChangeBlueOff.enable()
+          break
+        case 'Green':
+          this.btnChangeRedOn.enable()
+          this.btnChangeRedOff.enable()
+          this.btnChangeBlueOn.enable()
+          this.btnChangeBlueOff.enable()
+          break
+        default:
+          console.error('encountered unexpected force:' + force)
       }
-      const routeStyle = {
-        fill: true,
-        color: '#249',
-        opacity: 0.2
-      }
-
-      // hvae we calculated the achievable cells?
-      if (this.achievableCells.length === 0) {
-        // no, we must be starting a new line
-
-        // is this a mobile element
-        if (marker.mobile) {
-          this.planningLine.setLatLngs([cursorLoc, cursorLoc])
-        }
-
-        this.startHex = this.grid.cellFor(cursorLoc)
-
-        // limit distance of travel
-        if (marker.stepRemaining) {
-          this.achievableCells = this.grid.hexesInRange(this.startHex, marker.stepRemaining)
-        } else {
-          // nope, allow travel to anywhere
-          this.achievableCells = this.grid.cells
-        }
-
-        // set the route-line color
-        let hisColor
-        if (marker.force === 'Red') {
-          hisColor = '#ff0000'
-        } else if (marker.force === 'Blue') {
-          hisColor = '#000fff'
-        } else if (marker.force === 'Green') {
-          hisColor = '#19bd37'
-        }
-        this.planningLine.setStyle({
-          color: hisColor
-        })
-        this.historyLine.setStyle({
-          color: hisColor
-        })
-
-        //
-        this.achievableCells = this.achievableCells.filter(cell => {
-          if (marker.travelMode === 'land') {
-            return cell.land
-          } else if (marker.travelMode === 'sea') {
-            return cell.sea
-          } else if (marker.travelMode === 'air') {
-            return true
-          } else {
-            console.error('Unexpected terrain type')
-            return false
-          }
-        })
-
-        // apply styling to the achievable cells
-        this.achievableCells.forEach(cell => cell.polygon.setStyle(rangeStyle))
-
-        // is this an achievable cell?
-        const curCell = this.grid.cellFor(cursorLoc)
-
-        if (this.achievableCells.includes(curCell)) {
-          // ok, remember it
-          this.lastHex = curCell
-        }
-
-        // and the track history
-        if (marker.history) {
-          // ok, draw the history line
-          const historyLocs = []
-          marker.history.forEach(cellName => {
-            const cell = this.grid.hexNamed(cellName)
-            historyLocs.push(cell.centrePos)
-          })
-
-          this.historyLine.setLatLngs(historyLocs)
-        }
-      } else {
-        // retrieve the start point of the line
-
-        // are we plotting a line?
-        if (this.planningLine.length > 0) {
-          this.start = this.planningLine.getLatLngs()[0]
-          this.planningLine.setLatLngs([this.startHex.centrePos, cursorLoc])
-        }
-
-        // are we in a safe cell
-        const curCell = this.grid.cellFor(cursorLoc)
-
-        // is this an achievable cell?
-        if (this.achievableCells.includes(curCell)) {
-          // ok, remember it
-          this.lastHex = curCell
-        }
-
-        // clear the old cells
-        this.routeHexes.forEach(cell => {
-          if (this.achievableCells.includes(cell)) {
-            cell.polygon.setStyle(rangeStyle)
-          } else {
-            cell.polygon.setStyle(defaultHexStyle)
-          }
-        })
-
-        // do we have a valid current cell?
-        if (this.lastHex != null) {
-          // get the route
-          let newRoute = this.grid.hexesBetween(this.startHex, this.lastHex)
-
-          // if we have a restricted possible region,
-          // trim to it
-          if (this.achievableCells) {
-            newRoute = newRoute.filter(cell => this.achievableCells.includes(cell))
-          }
-
-          // and generate new cells
-          this.routeLats = []
-          this.routeHexes = newRoute
-          if (marker.mobile) {
-            this.routeHexes.forEach(cell => {
-              cell.polygon.setStyle(routeStyle)
-              this.routeLats.push(cell.centrePos)
-            })
-          } else {
-            // insert the current location twice,
-            // to give us a point marker
-            if (this.lastHex) {
-              this.routeLats.push(this.lastHex.centrePos)
-              this.routeLats.push(this.lastHex.centrePos)
-            }
-          }
-
-          if (this.routeLats.length > 1) {
-            this.planningLine.setLatLngs(this.routeLats)
-          }
-        }
-      }
-    })
-    marker.on('dragend', e => {
-      // ooh, see if it had restricted travel
-      if (marker.allowance && this.routeHexes.length > 0) {
-        // consume some of it
-
-        // calculate distance
-        const start = this.routeHexes[0]
-        const end = this.routeHexes[this.routeHexes.length - 1]
-        const distance = start.distance(end)
-
-        marker.stepRemaining -= distance
-
-        // cheat. if we've consumed distance, give it
-        // another allowance
-        if (marker.stepRemaining === 0) {
-          marker.stepRemaining = marker.allowance
-        }
-
-        // add these new cells as history
-        if (!marker.history) {
-          marker.history = []
-        }
-        this.routeHexes.forEach(cell => marker.history.push(cell.name))
-      }
-
-      // move the marker to the last valid location
-      marker.setLatLng(this.lastHex.centrePos)
-
-      // clear the line objects
-      this.routeLats = []
-      this.planningLine.setLatLngs([])
-      this.historyLine.setLatLngs([])
-
-      // clear the shaded cells
-      this.routeHexes.forEach(cell => cell.polygon.setStyle(defaultHexStyle))
-      this.routeHexes = []
-      this.achievableCells.forEach(cell => cell.polygon.setStyle(defaultHexStyle))
-      this.achievableCells = []
     })
   }
 }
