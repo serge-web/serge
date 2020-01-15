c18b506356403e9a805443f642304d5d1fff08f3
Alex Foxleigh <alex@alexfoxleigh.com>
2020-01-15 14:22:58 +0000

    created at test for the fake class

---
+++ packages/client/src/Tests/helpers/ExampleHelperClass.test.js
@@ -0,0 +1,20 @@
+/* global it expect */
+import ExampleHelperClass from '../../Helpers/ExampleHelperClass'
+
+const exhelper = new ExampleHelperClass('Joe', 'Bloggs', 39, 'Male')
+
+it('correctly connects the first and last names', () => {
+  expect(exhelper.fullName).toEqual('Joe Bloggs')
+})
+
+it('Outputs the bio as expected', () => {
+  expect(exhelper.generateBio()).toEqual({ age: 39, name: 'Joe Bloggs', sex: 'Male' })
+})
+
+it('Creates a "restricted access" guest pass as expected', () => {
+  expect(exhelper.createGuestPass('limited')).toEqual({ age: 39, name: 'Joe Bloggs', passType: 'Restricted Access', sex: 'Male' })
+})
+
+it('Creates a "festricted access" guest pass as expected', () => {
+  expect(exhelper.createGuestPass('full')).toEqual({ age: 39, name: 'Joe Bloggs', passType: 'All Access', sex: 'Male' })
+})
