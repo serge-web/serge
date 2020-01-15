912d73d7a824ecf695801c262f9aaff41e667139
Ian Mayo <ian@planetmayo.com>
2020-01-15 20:08:17 +0000

    Placeholders for handling vis change

--- packages/client/src/Components/Mapping/helpers/MapMarkersControl.js
+++ packages/client/src/Components/Mapping/helpers/MapMarkersControl.js
@@ -4,10 +4,14 @@ const MapMarkersControl = (markers, grid, allForces) => {
     if (force && marker.asset) {
       const asset = force.assets.find(({ name }) => name === marker.asset.name)
       if (asset) {
+        // check the positions match
         if (marker.asset.position !== asset.position) {
           // update marker
           marker.setLatLng(grid.hexNamed(asset.position).centrePos)
         }
+
+        // check that this force should be able to see this marker
+        // TODO: implementations
       } else {
         // asset not found
       }
