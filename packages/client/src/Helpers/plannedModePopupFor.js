3f741a7b378f798b387231fb1c3b0becaf05082b
Alex Foxleigh <alex@alexfoxleigh.com>
2020-01-15 14:22:29 +0000

    plannedModePopupFor minor refactor

--- packages/client/src/Helpers/plannedModePopupFor.js
+++ packages/client/src/Helpers/plannedModePopupFor.js
@@ -1,9 +1,9 @@
 import plannedStateFor from './plannedStateFor'
 
 const plannedModePopupFor = asset => {
-  var popup = '<b>' + asset.name + '</b><br/>'
+  let popup = '<b>' + asset.name + '</b><br/>'
   // states
-  // TODO: do we have concept of current speed?  Maybe take from history
+  // TODO: do we have concept of current speed? Maybe take from history
   popup += plannedStateFor(asset.state, 0, asset.platformTypeDetail, asset.platformSpeeds)
 
   // reset the route
