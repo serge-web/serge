1a9a66722f218416c343386177539870c5cfceab
Alex Foxleigh <alex@alexfoxleigh.com>
2020-01-15 16:56:42 +0000

    split 'resetCurrentLeg' into it's own file

---
+++ packages/client/src/Components/Mapping/helpers/resetCurrentLeg.js
@@ -0,0 +1,14 @@
+/** the user has clicked on a waypoint along a leg, and wishes to clear from that
+   * one onwards
+   */
+const resetCurrentLeg = (/* dictionary */ plannedLegs, /* string */ turnName, /* object */ planningMarker) => {
+  // loop through the legs
+
+  // is this the leg?
+
+  // delete it, and all following legs
+
+  // and also reset the state of the map interactions (update the state/position of the planning marker)
+}
+
+export default resetCurrentLeg
