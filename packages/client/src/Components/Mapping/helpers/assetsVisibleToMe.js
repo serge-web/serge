b27b834f3d4af29ce297e79aaba202c79ad6e415
Ian Mayo <ian@planetmayo.com>
2020-01-16 14:16:17 +0000

    testable method for assets that I can see

--- packages/client/src/Components/Mapping/helpers/assetsVisibleToMe.js
+++ packages/client/src/Components/Mapping/helpers/assetsVisibleToMe.js
@@ -1,3 +1,5 @@
+import { UMPIRE_FORCE } from '../../../consts'
+
 /** provide a list of the assets that are currently visible to me
  * i.e. either from my Force, or currently visible (has a perception entry for my force)
  * Oh, or all forces if I'm white force
@@ -5,24 +7,26 @@
 export default function assetsVisibleToMe (/* object */ allForces, /* string */ myForce) {
   const res = []
   allForces.forEach(force => {
-    force.asset.forEach(asset => {
-      if (myForce === 'White') {
-        // ok, white force I see all
-        res.push(asset)
-      } else if (asset.force === myForce) {
-        // ok, I know where my assets are
-        res.push(asset)
-      } else {
-        const perceptions = asset.perceptions
-        // do we have perceptions defined?
-        if (perceptions) {
-          // am I one of the perceptions?
-          if (perceptions[myForce]) {
-            res.push(asset)
+    if (force.assets) {
+      force.assets.forEach(asset => {
+        if (myForce === UMPIRE_FORCE) {
+          // ok, white force I see all
+          res.push(asset)
+        } else if (force.name === myForce) {
+          // ok, I know where my assets are
+          res.push(asset)
+        } else {
+          const perceptions = asset.perceptions
+          // do we have perceptions defined?
+          if (perceptions) {
+            // am I one of the perceptions?
+            if (perceptions[myForce]) {
+              res.push(asset)
+            }
           }
         }
-      }
-    })
+      })
+    }
   })
   return res
 }
