cc83282ea9f7e0a7dffaaed7895abc893f10ff13
Ian Mayo <ian@planetmayo.com>
2020-01-17 14:13:26 +0000

    avoid null pointer

--- packages/client/src/ActionsAndReducers/playerUi/helpers/handleVisibilityChanges.js
+++ packages/client/src/ActionsAndReducers/playerUi/helpers/handleVisibilityChanges.js
@@ -8,6 +8,11 @@ export default (/* object */ message, /* object */ allForces) => {
     if (force.assets) {
       const asset = force.assets.find(item => item.name === visChange.asset)
       if (asset) {
+        if (visChange.by && !asset.perceptions) {
+          // ok, something's becoming visible - make sure there's 
+          // a perceptions entry for it.
+          asset.perceptions = []
+        }
         // can it already see it?
         if (asset.perceptions[visChange.by]) {
           // ok, clear that entry
