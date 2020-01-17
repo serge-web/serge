e287f85a104369899abf2c186be4c1011d77dc03
Alex Foxleigh <alex@alexfoxleigh.com>
2020-01-17 10:46:42 +0000

    added test and function for submitClearWholeRoute

--- packages/client/src/Components/Mapping/helpers/submitClearWholeRoute.js
+++ packages/client/src/Components/Mapping/helpers/submitClearWholeRoute.js
@@ -1,10 +1,6 @@
 /**  a route has been planned, but the user wishes to start again. clear it */
-const submitClearWholeRoute = (/* dictionary */ plannedLegs, /* object */ planningMarker) => {
-  // erase all planned legs
-
-  // update the planning counter
-
-  // reset the initial available locations
+const submitClearWholeRoute = () => {
+  return []
 }
 
 export default submitClearWholeRoute
