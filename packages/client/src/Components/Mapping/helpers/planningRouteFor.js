4553052dbac6426ea4be298228f67128a52d61a8
Ian Mayo <ian@planetmayo.com>
2020-01-18 12:57:06 +0000

    Lots more implementation

--- packages/client/src/Components/Mapping/helpers/planningRouteFor.js
+++ packages/client/src/Components/Mapping/helpers/planningRouteFor.js
@@ -1,5 +1,14 @@
 import L from 'leaflet'
 
+// the images we use to annotate planning legs
+import turnLeft from '../images/turn-left.png'
+import turnRight from '../images/turn-right.png'
+import turnEnd from '../images/turn-end.png'
+import noTurn from '../images/no-turn.png'
+import lightTurn from '../images/light-turn.png'
+
+import turnNameFor from './turnNameFor'
+
 // const defPlannedTurns = [
 //   {
 //     "turn": 1,
@@ -22,8 +31,11 @@ import L from 'leaflet'
 
 function lineFor (/* array */ plannedTurns, /* latLng */ start,
   /* boolean */ lightweight, /* grid */ grid) {
+  const weight = lightweight ? 2 : 4
+  const dashArray = lightweight ? [1, 4] : [4, 8]
   const polyLine = L.polyline([], {
-    dashArray: [1, 4]
+    dashArray: dashArray,
+    weight: weight
   })
   const latLongs = [start]
   plannedTurns.forEach(turn => {
@@ -40,12 +52,13 @@ function lineFor (/* array */ plannedTurns, /* latLng */ start,
     }
   })
   polyLine.setLatLngs(latLongs)
+
   return polyLine
 }
 
 function bearingBetween (/* latLng */ p1, /* latLng */ p2) {
-  var d2r = L.LatLng.DEG_TO_RAD
-  var r2d = L.LatLng.RAD_TO_DEG
+  var d2r = Math.PI / 180
+  var r2d = 180 / Math.PI
   var lat1 = p1.lat * d2r
   var lat2 = p2.lat * d2r
   var dLon = (p2.lng - p1.lng) * d2r
@@ -57,15 +70,67 @@ function bearingBetween (/* latLng */ p1, /* latLng */ p2) {
   return brng
 };
 
+/** we plot a line marker perpendicular to the direction
+ * of travel at route waypoints.  Determine the marker to use for this bearing
+ */
+function bearingMarkerFor (/* number */ angleResult) {
+  let icon
+
+  if (angleResult === 180 || angleResult === 360 || angleResult === 0) {
+    // for the 180 and 360, which are flat icons so end icon is used
+    icon = turnEnd
+  } else if (angleResult > 0 && angleResult < 90) {
+    // North to East
+    icon = turnLeft
+  } else if (angleResult < 180) {
+    // East to South
+    icon = turnRight
+  } else if (angleResult < 271) {
+    // South to West
+    icon = turnLeft
+  } else if (angleResult < 360) {
+    // West to North
+    icon = turnRight
+  } else if (angleResult === undefined || angleResult === null) {
+    // as some angles are undefined just use the end icon until this can be figured out
+    icon = turnEnd
+  } else {
+    icon = turnEnd
+  }
+  return icon
+}
+
 function turnFor (/* latLng */ minus2, /* latLng */ minus1, /* latLng */ current) {
+  let bearing = null
   if (!minus2) {
     // ok, just the angle to current from minus 1
-    const bearing = bearingBetween(minus2, minus1)
+    bearing = bearingBetween(minus1, current)
+  } else if (!current) {
+    // ok, for the previous 2 legs
+    bearing = bearingBetween(minus2, minus2)
   } else {
     // sort out the two directions
     const bearing1 = bearingBetween(minus2, minus1)
     const bearing2 = bearingBetween(minus1, current)
+    bearing = (bearing1 + bearing2) / 2
   }
+  console.log(minus2, minus1, current)
+  return bearing
+}
+
+function createMarker (/* string */ icon, /* latLng */ location, /* boolean */ lightweight, /* string */ title) {
+  const iconToUse = lightweight ? lightTurn : icon
+  const turnIcon = L.icon({
+    iconUrl: iconToUse,
+    iconSize: [15, 15]
+  })
+  // console.log('North to East')
+  const marker = L.marker(location, {
+    icon: turnIcon, title: title, zIndexOffset: 1000
+  })
+  marker.bindPopup(title).openPopup()
+
+  return marker
 }
 
 function markersFor (/* array */ plannedTurns, /* latLng */ start,
@@ -73,9 +138,13 @@ function markersFor (/* array */ plannedTurns, /* latLng */ start,
   const result = L.layerGroup()
   let minus1 = null
   let minus2 = null
-  let pendingTurn = null
+  let pendingTurnLocation = null
+  let pendingTurnName = null
+  let current = start
   plannedTurns.forEach(turn => {
-    pendingTurn = null
+    const stateSuffix = turn.speed ? ' at ' + turn.speed + 'kts' : ''
+    const turnName = turnNameFor(turn.turn) + ' - ' + turn.state + stateSuffix
+
     // loop through the routes
     if (turn.route) {
       // loop through the steps in this route
@@ -83,13 +152,16 @@ function markersFor (/* array */ plannedTurns, /* latLng */ start,
         const ptHex = grid.hexNamed(step)
         if (ptHex) {
           // remember the coords
-          const current = ptHex.centrePos
+          current = ptHex.centrePos
 
           // are we waiting to populate a marker?
-          if (pendingTurn) {
+          if (pendingTurnLocation) {
             // have we got enough data?
             if (minus1) {
               const angle = turnFor(minus2, minus1, current)
+              const iconName = bearingMarkerFor(angle)
+              result.addLayer(createMarker(iconName, pendingTurnLocation, lightweight, pendingTurnName))
+              pendingTurnLocation = false
             }
           }
 
@@ -98,12 +170,28 @@ function markersFor (/* array */ plannedTurns, /* latLng */ start,
           minus1 = current
         }
       })
+      pendingTurnLocation = current
+      pendingTurnName = turnName
     } else {
       // ok, nothing happening. add a static marker
+      result.addLayer(createMarker(noTurn, current, lightweight, turnName))
+
+      // forget about waiting for more coords
+      pendingTurnLocation = null
     }
   })
+  // are we waiting to populate a marker?
+  // do we have a trailing pending turn?
+  if (pendingTurnLocation) {
+    // have we got enough data?
+    if (minus1) {
+      const angle = turnFor(minus2, minus1, null)
+      const icon = bearingMarkerFor(angle)
+      result.addLayer(createMarker(icon, current, lightweight, pendingTurnName))
+      pendingTurnLocation = false
+    }
+  }
   return result
-
 }
 
 /** create a Leaflet elememt for this set of routes
