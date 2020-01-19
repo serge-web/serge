3a6d894737572a74c715f99502252d75d4879a4c
Ian Mayo <ian@planetmayo.com>
2020-01-19 17:57:35 +0000

    Introduce new utility functions

---
+++ packages/client/src/Components/Mapping/helpers/findPerceivedAsClassName.js
@@ -0,0 +1,21 @@
+export default function findPerceivedAsClasses (/* string */ myForce, /* string */ hisForce, /* his type */ hisType,
+  /* array percept */ hisPerceptions, /* boolean */ userIsUmpire, /* string */ assetName) {
+  let perception
+  if (myForce === hisForce || userIsUmpire) {
+    perception = { force: hisForce, type: hisType }
+  } else {
+    const hisPerception = hisPerceptions[myForce]
+    if (hisPerception != null) {
+      perception = { force: hisPerception.force, type: hisPerception.type }
+    } else {
+      perception = null
+    }
+  }
+  if (perception) {
+    const forceClass = perception && perception.force ? perception.force.toLowerCase() : 'unknown'
+    const typeClass = perception && perception.type ? perception.type.replace(/ /g, '-').toLowerCase() : 'unknown'
+    return `platform-counter platform-force-${forceClass} platform-type-${typeClass}`
+  } else {
+    return perception
+  }
+}
