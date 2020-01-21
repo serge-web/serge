8e098b279b47a3aef861e9946ca50bf19cb698d6
Alex Foxleigh <alex@alexfoxleigh.com>
2020-01-21 16:59:24 +0000

    added an index to make importing helpers en masse easier

---
+++ packages/client/src/Components/Mapping/helpers/index.js
@@ -0,0 +1,63 @@
+import GridImplementation from './GridImplementation'
+import MapAdjudicatingListener from './MapAdjudicatingListener'
+import MapAdjudicatingPlayerListener from './MapAdjudicatingPlayerListener'
+import MapAdjudicatingUmpireListener from './MapAdjudicatingUmpireListener'
+import MapAdjudicationPendingListener from './MapAdjudicationPendingListener'
+import MapPlanningPlayerListener from './MapPlanningPlayerListener'
+import MapPlanningUmpireListener from './MapPlanningUmpireListener'
+import assetsVisibleToMe from './assetsVisibleToMe'
+import colorFor from './colorFor'
+import findPerceivedAsClasses from './findPerceivedAsClassName'
+import createDebugButton from './createDebugButton'
+import forceFor from './forceFor'
+import plannedModePopupFor from './plannedModePopupFor'
+import createPerceivedStateButtonsFor from './createPerceivedStateButtonsFor'
+import getClearedRoute from './getClearedRoute'
+import plannedStateFor from './plannedStateFor'
+import createStateButtonsFor from './createStateButtonsFor'
+import getLastLegRemoved from './getLastLegRemoved'
+import planningRouteFor from './planningRouteFor'
+import declutterLayer from './declutterLayer'
+import hasPendingForces from './hasPendingForces'
+import removeClassNamesFrom from './removeClassNamesFrom'
+import declutterMarkers from './declutterMarkers'
+import resetCurrentLeg from './resetCurrentLeg'
+import findAsset from './findAsset'
+import mapPopupHelper from './mapPopupHelper'
+import roundToNearest from './roundToNearest'
+import findLastRouteLocation from './findLastRouteLocation'
+import markerFor from './markerFor'
+import turnNameFor from './turnNameFor'
+
+export {
+  GridImplementation,
+  MapAdjudicatingListener,
+  MapAdjudicatingPlayerListener,
+  MapAdjudicatingUmpireListener,
+  MapAdjudicationPendingListener,
+  MapPlanningPlayerListener,
+  MapPlanningUmpireListener,
+  assetsVisibleToMe,
+  colorFor,
+  findPerceivedAsClasses,
+  createDebugButton,
+  forceFor,
+  plannedModePopupFor,
+  createPerceivedStateButtonsFor,
+  getClearedRoute,
+  plannedStateFor,
+  createStateButtonsFor,
+  getLastLegRemoved,
+  planningRouteFor,
+  declutterLayer,
+  hasPendingForces,
+  removeClassNamesFrom,
+  declutterMarkers,
+  resetCurrentLeg,
+  findAsset,
+  mapPopupHelper,
+  roundToNearest,
+  findLastRouteLocation,
+  markerFor,
+  turnNameFor
+}
