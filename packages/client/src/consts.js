a98b46df31bad597a359ad71a63e0fa23eabcec8
Ian Mayo <ian@planetmayo.com>
2020-01-21 10:50:53 +0000

    Correct case for LocationPending

--- packages/client/src/consts.js
+++ packages/client/src/consts.js
@@ -37,7 +37,7 @@ export const MAX_LISTENERS = 82
 
 export const UMPIRE_FORCE = 'umpire'
 
-export const LOCATION_PENDING = 'locationPending' // special state where platforms can be moved at turn zero
+export const LOCATION_PENDING = 'LocationPending' // special state where platforms can be moved at turn zero
 
 // series of constants used for `messageType` when sending map events
 export const FORCE_LAYDOWN = 'ForceLaydown'
