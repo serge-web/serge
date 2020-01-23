a9a4561c6e134e3a705deb1d000a275ecdb967b2
Ian Mayo <ian@planetmayo.com>
2020-01-23 10:11:21 +0000

    We need to keep the current leg, and only delete legs after it

--- packages/client/src/Components/Mapping/helpers/resetCurrentLeg.js
+++ packages/client/src/Components/Mapping/helpers/resetCurrentLeg.js
@@ -1,13 +1,13 @@
 /** the user has clicked on a waypoint along a leg, and wishes to clear from that
    * one onwards
    */
-const resetCurrentLeg = (/* dictionary */ plannedLegs, /* string */ turnNumber) => {
+const resetLegsFrom = (/* dictionary */ plannedLegs, /* string */ turnNumber) => {
   // loop through the legs and delete any turns after 'turnNumber
 
   return plannedLegs
     .filter(leg => {
-      return leg.turn < turnNumber
+      return leg.turn <= turnNumber
     })
 }
 
-export default resetCurrentLeg
+export default resetLegsFrom
