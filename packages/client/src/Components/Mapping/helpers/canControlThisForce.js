420b576a04f7c62b986cc1cded1e8218486f6fdd
Ian Mayo <ian@planetmayo.com>
2020-01-23 11:52:47 +0000

    Introduce test for one force can control another

---
+++ packages/client/src/Components/Mapping/helpers/canControlThisForce.js
@@ -0,0 +1,9 @@
+export default function canControlThisForce (/* array */ allForces, /* string */ subjectForceId, /* String */ myForceId) {
+  if (myForceId === subjectForceId) {
+    return true
+  } else {
+    // get this force
+    const thisForce = allForces.find(force => force.uniqid === subjectForceId)
+    return thisForce.controlledBy && thisForce.controlledBy.find(name => name === myForceId)
+  }
+};
