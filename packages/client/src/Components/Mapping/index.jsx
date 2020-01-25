6a5c71b9ceef683ee61586030cde0535c6d2fdbd
Ian Mayo <ian@planetmayo.com>
2020-01-25 13:43:56 +0000

    tidying

--- packages/client/src/Components/Mapping/index.jsx
+++ packages/client/src/Components/Mapping/index.jsx
@@ -11,7 +11,6 @@ import {
   MapAdjudicatingUmpireListener,
   MapAdjudicationPendingListener,
   MapPlanningPlayerListener,
-  MapPlanningUmpireListener,
   MapPopupHelper,
   markerFor
 } from './helpers'
