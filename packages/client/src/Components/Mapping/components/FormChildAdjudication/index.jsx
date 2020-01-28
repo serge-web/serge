064bb7f7d0703e482e63eb96871620bb8773fa8c
Alex Foxleigh <alex@alexfoxleigh.com>
2020-01-28 16:11:38 +0000

    [WIP] creating adjudication component

---
+++ packages/client/src/Components/Mapping/components/FormChildAdjudication/index.jsx
@@ -0,0 +1,90 @@
+import React, { useState } from 'react'
+import _ from 'lodash'
+
+const Adjudication = ({ store, onStoreUpdate, callbackFunction }) => {
+  const [currentMarker] = useState(store.currentMarker)
+  const [allForces] = useState(store.allForces)
+  const [allPlatforms] = useState(store.allPlatforms)
+  const [markerStatus, setMarkerStatus] = useState(store.currentMarkerStatus)
+  const [markerSpeed, setMarkerSpeed] = useState(store.currentMarkerSpeed)
+
+  // Get all of the possible states and speeds
+  const { states, speedKts } = currentMarker.platformTypeDetail
+
+  // A copy of the store to capture the updates
+  const newStore = store
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    callbackFunction(newStore)
+  }
+
+  const handlePlatformForceChange = ({ target }) => {
+    // setForcePerception(target.value)
+
+    // newStore.perception.force = target.value
+    // // save data in helper class to not lose it after popup recreate
+    // onStoreUpdate(newStore)
+  }
+
+  const handlePlatformTypeChange = ({ target }) => {
+    // setTypePerception(target.value)
+    // newStore.perception.type = target.value
+    // // save data in helper class to not lose it after popup recreate
+    // onStoreUpdate(newStore)
+  }
+
+  return (
+    <form action="">
+      <h2>{currentMarker.name}</h2>
+      <fieldset>
+        <span className="label">Planned</span>
+        <div className="input-container button-group">
+          <button>Accept</button>
+          <button>Reject</button>
+        </div>
+        <div className="input-container radio">
+          <label htmlFor="state">Status</label>
+          <ul>
+            {
+              states.map(status =>
+                <li key={status.name}>
+                  <label>
+                    {status.name}
+                    <input name="state" type="radio" value={status.name} checked={markerStatus === status.name}/>
+                  </label>
+                </li>
+              )
+            }
+          </ul>
+        </div>
+        { (speedKts && speedKts.length && markerStatus !== 'Moored') &&
+        <div className="input-container radio">
+          <label htmlFor="speed">Speed (kts)</label>
+          <ul>
+            {
+              speedKts.map(speed =>
+                <li key={speed}>
+                  <label>
+                    {speed}
+                    <input name="speed" type="radio" value={speed} checked={markerSpeed === speed} />
+                  </label>
+                </li>
+              )
+            }
+          </ul>
+        </div>
+        }
+      </fieldset>
+      <fieldset>
+        <div className="input-container checkbox">
+          <label htmlFor="visibility">Visible to</label>
+        </div>
+
+      </fieldset>
+      <button onClick={handleSubmit}>Save</button>
+    </form>
+  )
+}
+
+export default Adjudication
--- packages/client/src/Components/Mapping/components/FormContainer/index.jsx
+++ packages/client/src/Components/Mapping/components/FormContainer/index.jsx
@@ -1,16 +1,19 @@
 import React from 'react'
 
 import Perception from '../FormChildPerception'
+import PlannedStatus from '../FormChildPlannedStatus'
+import Adjudication from '../FormChildAdjudication'
 
 import './styles.scss'
-import PlannedStatus from '../FormChildPlannedStatus'
 
 const MappingForm = props => {
   return (
     <section className="mapping-form-container">
-      { (props.store.currentForce !== props.store.currentMarkerForce && props.store.currentForce !== 'umpire')
-        ? <Perception store={props.store} onStoreUpdate={props.onStoreUpdate} callbackFunction={props.callbackFunction}></Perception>
-        : <PlannedStatus store={props.store} onStoreUpdate={props.onStoreUpdate} callbackFunction={props.callbackFunction}></PlannedStatus>
+      { props.store.currentForce !== 'umpire'
+        ? (props.store.currentForce !== props.store.currentMarkerForce)
+          ? <Perception store={props.store} onStoreUpdate={props.onStoreUpdate} callbackFunction={props.callbackFunction}></Perception>
+          : <PlannedStatus store={props.store} onStoreUpdate={props.onStoreUpdate} callbackFunction={props.callbackFunction}></PlannedStatus>
+        : <Adjudication store={props.store} onStoreUpdate={props.onStoreUpdate} callbackFunction={props.callbackFunction}></Adjudication>
       }
     </section>
   )
