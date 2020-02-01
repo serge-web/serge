d8df9db66047575203fc848cd05ca9ab5335b2bb
Ian Mayo <ian@planetmayo.com>
2020-02-01 15:52:05 +0000

    Correctly retrieve the new condition & perception values

--- packages/client/src/ActionsAndReducers/playerUi/helpers/handleStateOfWorldChanges.js
+++ packages/client/src/ActionsAndReducers/playerUi/helpers/handleStateOfWorldChanges.js
@@ -13,8 +13,8 @@ export default (/* object */ payload, /* object */ allForces) => {
       asset.status = entry.newState.status
       asset.position = entry.newState.position
       asset.route = entry.newState.route
-      asset.condition = entry.newState.condition
-      asset.perceptions = entry.newState.perceptions
+      asset.condition = entry.condition
+      asset.perceptions = entry.perceptions
     })
   })
   return allForces
