951c82c1cb7f3f0db29b78994a03fd08cb11cf6c
Alex Foxleigh <alex@alexfoxleigh.com>
2020-02-13 13:54:34 +0000

    storybook installed and configured with demo component in place

---
+++ packages/storybook/.storybook/main.js
@@ -0,0 +1,7 @@
+module.exports = {
+    // Look for stories both in the components package and also in the `src` folder of this package
+    stories: [
+        '../../components/**/*.stories.[tj]s',
+        '../src/**/*.stories.[tj]s'
+    ],
+};
