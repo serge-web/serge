366a3cda1ae101be294e3ff9814035678bba105a
Alex Foxleigh <alex@alexfoxleigh.com>
2020-03-06 17:24:11 +0000

    Moved click handler into the component as it is a void function

--- packages/components/src/local/marker-name/helpers/click-handler.ts
+++
@@ -1,7 +0,0 @@
-const clickHandler = (currentForce: string, currentMarkerForce: string, setClicked: Function): Function => {
-  return currentForce !== 'umpire' &&
-  currentForce !== currentMarkerForce &&
-  setClicked(true)
-}
-
-export default clickHandler
--- packages/components/src/local/marker-name/index.tsx
+++ packages/components/src/local/marker-name/index.tsx
@@ -6,15 +6,15 @@ import Props from './types/props.interface'
 /* Import Stylesheet */
 import styles from './styles.module.scss'
 
-/* Import Helpers */
-import clickHandler from './helpers/click-handler'
-
 /* Render component */
 export const MarkerName: React.FunctionComponent<Props> = ({ currentMarkerName, currentMarkerForce, currentForce, perception }: Props) => {
   const [clicked, setClicked] = useState(false)
   const [perceived, setPerceived] = useState(perception)
 
-  const handleClick = (): Function => clickHandler(currentForce, currentMarkerForce, setClicked)
+  const handleClick = (): void => {
+    const val = currentForce !== 'umpire' && currentForce !== currentMarkerForce
+    setClicked(val)
+  }
 
   const name = perceived && perceived.name ? perceived.name : currentMarkerName
 
