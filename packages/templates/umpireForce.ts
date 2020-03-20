98ecd1e096c02a8aaeca450e022359ed69639b50
Alex Foxleigh <alex@alexfoxleigh.com>
2020-03-20 14:08:12 +0000

    creates new templates package and transferred templates from costs over

---
+++ packages/templates/umpireForce.ts
@@ -0,0 +1,21 @@
+import uniqId from 'uniqid'
+import { serverPath } from '@serge/config'
+
+export default {
+  name: 'White',
+  uniqid: 'umpire',
+  overview: 'Umpire force.',
+  roles: [
+    {
+      name: 'Game Control',
+      password: `p${uniqId.time()}`,
+      control: true,
+      isObserver: true,
+      isInsightViewer: true
+    }
+  ],
+  icon: serverPath + 'default_img/umpireDefault.png',
+  color: '#FCFBEE',
+  umpire: true,
+  dirty: false
+}
