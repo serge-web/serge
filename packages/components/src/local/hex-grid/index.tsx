0dced83f4426646c2a7a64123ae7faf70bb3808b
Ian Mayo <ian@planetmayo.com>
2020-04-09 17:57:01 +0100

    Give the grid something to render

--- packages/components/src/local/hex-grid/index.tsx
+++ packages/components/src/local/hex-grid/index.tsx
@@ -1,39 +1,19 @@
 import React from 'react'
-import { withLeaflet } from 'react-leaflet'
-import VectorGridDefault from 'react-leaflet-vectorgrid'
+import L from 'leaflet'
+import { Marker, Tooltip } from 'react-leaflet'
 
 /* Import Types */
 import PropTypes from './types/props'
 
 /* Render component */
-export const HexGrid: React.FC<PropTypes> = ({ tileSize }: PropTypes) => {
-  const VectorGrid = withLeaflet(VectorGridDefault)
-  const options = {
-    type: 'slicer',
-    idField: 'OBJECTID',
-    tooltip: 'NAME',
-    popup: (layer: any): any => `<div>${layer.properties.NAME}</div>`,
-    style: {
-      weight: 0.5,
-      opacity: 1,
-      color: '#ccc',
-      fillColor: '#390870',
-      fillOpacity: 0.6,
-      fill: true,
-      stroke: true
-    },
-    hoverStyle: {
-      fillColor: '#390870',
-      fillOpacity: 1
-    },
-    activeStyle: {
-      fillColor: '#390870',
-      fillOpacity: 1
-    },
-    zIndex: tileSize
-  }
-
-  return <VectorGrid {...options} />
+export const HexGrid: React.FC<PropTypes> = ({ /*tileSize, width, height,*/ origin }: PropTypes) => {
+  const divIcon = L.divIcon({
+    iconSize: [40, 40],
+  })
+  const tooltip = "the tooltip"
+  return <Marker position={origin} icon={divIcon} classNa>
+      <Tooltip>{tooltip}</Tooltip>
+  </Marker>
 }
 
 export default HexGrid
