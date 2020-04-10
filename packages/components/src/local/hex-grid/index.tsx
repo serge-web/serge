9db262634d93f2bb07438cadb6250cf92e9c6569
Ian Mayo <ian@planetmayo.com>
2020-04-10 12:09:06 +0100

    Start building up grid

--- packages/components/src/local/hex-grid/index.tsx
+++ packages/components/src/local/hex-grid/index.tsx
@@ -1,18 +1,34 @@
 import React from 'react'
 import L from 'leaflet'
 import { Marker, Tooltip } from 'react-leaflet'
+import { defineGrid, PointyCompassDirection } from 'honeycomb-grid'
 
 /* Import Types */
 import PropTypes from './types/props'
+import toWorld from './helpers/toWorld'
 
 /* Render component */
-export const HexGrid: React.FC<PropTypes> = ({ /*tileSize, width, height,*/ origin }: PropTypes) => {
-  const divIcon = L.divIcon({
-    iconSize: [40, 40],
+export const HexGrid: React.FC<PropTypes> = ({ tileSize, width, height, origin }: PropTypes) => {
+
+  const grid = defineGrid()
+  const grid_cells = grid.rectangle({
+    width: width,
+    height: height,
+    direction: PointyCompassDirection.E
   })
-  const tooltip = "the tooltip"
+
+  // create a polygon for each hex, add it to the parent
+  grid_cells.forEach(hex => {
+    const centreHex = hex.toPoint()
+    const centreWorld = toWorld(centreHex, origin, tileSize)
+  })
+
+  const divIcon = L.divIcon({
+    iconSize: [40, 40]
+  })
+  const tooltip = 'the tooltip'
   return <Marker position={origin} icon={divIcon} classNa>
-      <Tooltip>{tooltip}</Tooltip>
+    <Tooltip>{tooltip}</Tooltip>
   </Marker>
 }
 
