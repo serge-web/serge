f6d7784a44528f49b05a4dc58079a8a344917e7d
Ian Mayo <ian@planetmayo.com>
2020-04-17 16:28:33 +0100

    use cached name value

--- packages/components/src/local/assets/helpers/hexNamed.ts
+++ packages/components/src/local/assets/helpers/hexNamed.ts
@@ -1,12 +1,9 @@
-import cellName from './cellName'
 import SergeHex from '../../mapping/types/serge-hex'
 import SergeGrid from '../../mapping/types/serge-grid'
 
 /** retrive the cell at the supplied human-readable coords ("A01") */
 const hexNamed = (name: string, gridCells: SergeGrid<SergeHex<{}>>): SergeHex<{}> | undefined => {
-    // TODO: it's expensive to create the cell name each time we do this loop.
-    // we should do it once, and store it in the cell
-    return gridCells.find(cell => cellName(cell) === name)
+    return gridCells.find(cell => cell.name === name)
 }
 
 export default hexNamed;
