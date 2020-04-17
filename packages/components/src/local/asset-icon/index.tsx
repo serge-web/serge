af25beeaf0b864c7d1f0b58292061a203969d034
Alex Foxleigh <alex@alexfoxleigh.com>
2020-04-17 16:52:54 +0100

    WIP adds dialogue popup to asset icon

--- packages/components/src/local/asset-icon/index.tsx
+++ packages/components/src/local/asset-icon/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import cx from 'classnames'
 import { Marker, Tooltip } from 'react-leaflet'
 import L from 'leaflet'
@@ -11,17 +11,20 @@ import styles from './styles.module.scss'
 
 /* Render component */
 export const AssetIcon: React.FC<PropTypes> = ({ position, type, force, tooltip }: PropTypes) => {
+  const [showDialogue, setShowDialogue] = useState(false)
+
   const divIcon = L.divIcon({
     iconSize: [40, 40],
-    className: cx(styles['asset-icon'], styles[force], styles[`platform-type-${type}`]),
+    className: cx(styles['asset-icon'], styles[force], styles[`platform-type-${type}`])
   })
 
   const eventChild = <div>Hello</div>
 
-  const clickEvent = () => console.log('This is the event')
+  const clickEvent = (): void => setShowDialogue(true)
 
   return <Marker position={position} icon={divIcon} onclick={clickEvent}>
-      <Tooltip>{tooltip}</Tooltip>
+    <Tooltip>{tooltip}</Tooltip>
+    { showDialogue && eventChild}
   </Marker>
 }
 
