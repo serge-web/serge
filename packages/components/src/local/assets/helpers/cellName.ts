9c930b1155a57a8cb265486d2b9b93c316f58af7
Ian Mayo <ian@planetmayo.com>
2020-04-17 16:23:15 +0100

    Correct construction of cell name

--- packages/components/src/local/assets/helpers/cellName.ts
+++ packages/components/src/local/assets/helpers/cellName.ts
@@ -3,7 +3,7 @@ import SergeHex from '../../mapping/types/serge-hex'
 
 /** retrive the cell at the supplied human-readable coords ("A01") */
 const cellName = (cell:SergeHex<{}>) => {
-  return String.fromCharCode(65 + cell.y) + padInt(cell.x)
+  return String.fromCharCode(65 + cell.x) + padInt(cell.y)
 }
 
 export default cellName;
