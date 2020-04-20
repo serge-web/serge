e9332b35cec8e44d49378625c0358f0bb2c61998
Ian Mayo <ian@planetmayo.com>
2020-04-20 15:02:35 +0100

    improve JSDoc

--- packages/components/src/local/hex-grid/helpers/to-world.ts
+++ packages/components/src/local/hex-grid/helpers/to-world.ts
@@ -1,10 +1,16 @@
 import L from 'leaflet'
 
-/** convert this point in cell coordinates to lat/long */
+/** convert this point in cell coordinates to lat/long
+ * @param {any} point location in 2d Hex coordinate system
+ * @param {L.latLng} origin location of hex origin (0,0) in world degree system
+ * @param {number} tileSize diamater of hex tiles in Degrees (used as
+ * scaling factor)
+ * @returns {L.LatLng} coordinate in world degrees
+ */
 const toWorld = (point: any, origin: L.LatLng, tileSize: number): L.LatLng => {
-    const newLat = origin.lat - point.y * tileSize
-    const newLng = origin.lng + point.x * tileSize
-    return L.latLng(newLat, newLng)
+  const newLat = origin.lat - point.y * tileSize
+  const newLng = origin.lng + point.x * tileSize
+  return L.latLng(newLat, newLng)
 }
 
 export default toWorld
