b5009b93aab06113f8a61d4337cbd9888d644922
Ian Mayo <ian@planetmayo.com>
2020-04-20 16:09:26 +0100

    Correct JSDoc label

--- packages/components/src/local/assets/helpers/cellName.ts
+++ packages/components/src/local/assets/helpers/cellName.ts
@@ -1,7 +1,7 @@
 import padInt from '@serge/helpers/padInteger'
 import SergeHex from '../../mapping/types/serge-hex'
 
-/** retrive the cell at the supplied human-readable coords ("A01") */
+/** Generate the Serge name for the supplied cell (1,3) == "B03" */
 const cellName = (cell: SergeHex<{}>): string => {
   return String.fromCharCode(65 + cell.x) + padInt(cell.y)
 }
