a814127167e34590ac298bdc57c1e034c9cbfff6
Ian Mayo <ian@planetmayo.com>
2020-04-20 15:55:55 +0100

    minor tidying

--- packages/components/src/local/assets/helpers/cellName.ts
+++ packages/components/src/local/assets/helpers/cellName.ts
@@ -2,10 +2,8 @@ import padInt from '@serge/helpers/padInteger'
 import SergeHex from '../../mapping/types/serge-hex'
 
 /** retrive the cell at the supplied human-readable coords ("A01") */
-const cellName = (cell:SergeHex<{}>) => {
+const cellName = (cell: SergeHex<{}>): string => {
   return String.fromCharCode(65 + cell.x) + padInt(cell.y)
 }
 
-export default cellName;
-
-
+export default cellName
