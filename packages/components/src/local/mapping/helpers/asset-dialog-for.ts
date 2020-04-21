cfe894161422f662606bedc9ed555bfee1bd784e
Ian Mayo <ian@planetmayo.com>
2020-04-21 12:33:00 +0100

    Introduce logic to determine which form to display for an asset

---
+++ packages/components/src/local/mapping/helpers/asset-dialog-for.ts
@@ -0,0 +1,39 @@
+import Phase from '../types/phases'
+
+/** determine which form to show on this click
+ * @param {string} playerForce the force for the current player
+ * @param {string} assetForce the force for the selected asset
+ * @param {string} assetControlledBy which forces can control this asset
+ * @param {Phase} gamePhase the name of the current game phase
+ * @return {string} name of dialog to show
+ */
+const assetDialogFor = (playerForce:string, assetForce:string, assetControlledBy: [string?], 
+  gamePhase: Phase): string => {
+    let res: string = ''
+    switch(gamePhase)
+    {
+      case Phase.Planning:
+        if (assetForce === playerForce) {
+          res = "Planning"
+        } else if (assetControlledBy != null && assetControlledBy.includes(playerForce)) {
+          res = "Planning"
+        } else if(playerForce !== 'Umpire') {
+          res = "PerceivedAs"
+        } else {
+          res = ''
+        }
+        break;
+      case Phase.Adjudication:
+        if (playerForce === 'Umpire') {
+          res = "Adjudication"
+        } else if (assetForce !== playerForce) {
+          res = "PerceivedAs"
+        } else {
+          res = ''
+        }
+        break;      
+    }
+    return res
+}
+
+export default assetDialogFor
---
+++ packages/components/src/local/mapping/types/phases.d.ts
@@ -0,0 +1,8 @@
+enum Phase {
+  /* players are planning their next turn
+   */
+  Planning = "Planning",
+  /** umpire is resolving planned turns */
+  Adjudication = "Adjudication"
+}
+export default Phase
