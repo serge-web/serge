d0307b19b090ab248f182e85179a4cfeb3cecf16
Alex Foxleigh <alex@alexfoxleigh.com>
2020-04-29 17:30:52 +0100

    Begins addition of 'force' label - CSS POC only

--- packages/components/src/local/form-elements/rcb/index.tsx
+++ packages/components/src/local/form-elements/rcb/index.tsx
@@ -3,6 +3,9 @@ import React from 'react'
 import InputContainer from '../../input-container'
 import { FormControlLabel, RadioGroup } from '@material-ui/core'
 
+/* Import styles */
+import styles from './styles.module.scss'
+
 /* Import types */
 import PropTypes from './types/props'
 
@@ -10,14 +13,14 @@ import PropTypes from './types/props'
 import { ConditionalWrapper, componentSelector } from './helpers'
 
 /* Render component */
-export const RCB: React.FC<PropTypes> = ({ type, label, options }) => 
+export const RCB: React.FC<PropTypes> = ({ type, label, options, force }) => 
   <InputContainer label={label}>
     <ConditionalWrapper 
       condition={type === 'radio'}
       wrapper = {(children: any) => <RadioGroup aria-label={label} name={label.toLowerCase()}>{children}</RadioGroup> }
       >
     {
-      options.map(option => <FormControlLabel control={componentSelector(type, option)} label={option} />)
+      options.map(option => <FormControlLabel key={option.toString()} className={force ? styles['with-force'] : 'default'} control={componentSelector(type, option)} label={option} />)
     }
     </ConditionalWrapper>
   </InputContainer>
