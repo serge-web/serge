3cbe082a9733b9854857248f06a2e06a8a46e93e
Alex Foxleigh <alex@alexfoxleigh.com>
2020-04-29 17:32:28 +0100

    Removes unused stylesheet

--- packages/components/src/local/plan-turn-form/index.tsx
+++ packages/components/src/local/plan-turn-form/index.tsx
@@ -7,10 +7,6 @@ import RCB from '../form-elements/rcb'
 import { Button } from '@material-ui/core'
 import Turns from '../form-elements/turns'
 
-
-/* Import Stylesheet */
-// import styles from './styles.module.scss'
-
 /* Render component */
 export const PlanTurnForm: React.FC<Props> = ({}: Props) =>
   <Form type="planning" headerText="placeholder">
