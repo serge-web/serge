c31bfde0bf80c2aab66d986c9696f7784c371252
Alex Foxleigh <alex@alexfoxleigh.com>
2020-04-29 17:31:18 +0100

    Adds 'Perception' Form

---
+++ packages/components/src/local/perception-form/index.spec.tsx
@@ -0,0 +1,13 @@
+/* global it expect */
+
+import React from 'react'
+import renderer from 'react-test-renderer'
+
+import PerceptionForm from './index'
+
+it('PerceptionForm renders correctly', () => {
+  const tree = renderer
+    .create(<PerceptionForm />)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
---
+++ packages/components/src/local/perception-form/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+/* Import Types */
+import Props from './types/props'
+import Form from '../form'
+import RCB from '../form-elements/rcb'
+import { Button } from '@material-ui/core'
+import Turns from '../form-elements/turns'
+
+/* Render component */
+export const PerceptionForm: React.FC<Props> = ({}: Props) =>
+  <Form type="perceived-as" headerText="placeholder">
+    <fieldset>
+      <RCB type="radio" label="Perceived Force" options={['Red', 'Green']} force={true}/>
+      <Turns />
+    </fieldset>
+    <Button>Save</Button>
+  </Form>
+
+export default PerceptionForm
