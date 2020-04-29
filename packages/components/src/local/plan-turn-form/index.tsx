1d4b96eb3599b1d46e5d4d277af8ad221105dbf9
Alex Foxleigh <alex@alexfoxleigh.com>
2020-04-29 15:43:46 +0100

    Updates planning form to use RCB

--- packages/components/src/local/plan-turn-form/index.tsx
+++ packages/components/src/local/plan-turn-form/index.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 /* Import Types */
 import Props from './types/props'
 import Form from '../form'
-import RadioType from '../form-elements/radio-type'
+import RCB from '../form-elements/rcb'
 import { Button } from '@material-ui/core'
 import Turns from '../form-elements/turns'
 
@@ -15,7 +15,7 @@ import Turns from '../form-elements/turns'
 export const PlanTurnForm: React.FC<Props> = ({}: Props) =>
   <Form type="planning" headerText="placeholder">
     <fieldset>
-      <RadioType label="Status" options={['Moored', 'Transiting']} />
+      <RCB type="radio" label="Status" options={['Moored', 'Transiting']} />
       <Turns />
     </fieldset>
     <Button>Save</Button>
