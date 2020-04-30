24bf72f08b760aa720b10a3cf289288e058dd386
Alex Foxleigh <alex@alexfoxleigh.com>
2020-04-30 16:03:49 +0100

    Updated eslintrc to correctly remove semi-colons from the codebase

--- packages/components/.eslintrc.js
+++ packages/components/.eslintrc.js
@@ -16,8 +16,7 @@ module.exports = {
     'react'
   ],
   'rules': {
-    "semi": "off",
-    "@typescript-eslint/semi": ["error"],
+    "semi": [2, "never"],
     "@typescript-eslint/ban-ts-ignore": "off",
     '@typescript-eslint/no-use-before-define': 'off', // Allows us to hoist variables and functions which I am a fan of, functions not variables that is.
     '@typescript-eslint/no-explicit-any': 'off', // Too strict for my case, sometimes I need an any type
