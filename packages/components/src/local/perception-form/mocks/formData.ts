62839fe83c24b595edbb62f663837f9bfac8c5ca
Alex Foxleigh <alex@alexfoxleigh.com>
2020-05-04 16:53:08 +0100

    Adds mocks for perception form

---
+++ packages/components/src/local/perception-form/mocks/formData.ts
@@ -0,0 +1,18 @@
+const formData = {
+  perceived_force: [
+    {
+      name: 'Blue Force',
+      colour: '#69c'
+    },
+    {
+      name: 'Red Force',
+      colour: '#f00'
+    },
+    {
+      name: 'White Force',
+      colour: '#fff'
+    }
+  ]
+}
+
+export default formData
