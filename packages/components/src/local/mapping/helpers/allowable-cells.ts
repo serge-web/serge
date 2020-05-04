7ce51d39ffc5ad9795a24ab049a97cb7c6c3f6ee
Ian Mayo <ian@planetmayo.com>
2020-05-04 13:03:21 +0100

    tidy API

--- packages/components/src/local/mapping/helpers/allowable-cells.ts
+++ packages/components/src/local/mapping/helpers/allowable-cells.ts
@@ -1,20 +1,19 @@
 import SergeHex from '../types/serge-hex'
 import SergeGrid from '../types/serge-grid'
-import PlanMobileAsset from '../types/plan-mobile-asset'
 
 /**
  *  create hexagonal grid
  * @param {SergeGrid<SergeHex<{}>> | undefined} grid grid of hex cells
- * @param {PlanMobileAsset} constraints Description of what this platform can do
+ * @param {SergeHex<{}>} origin start for this planning turn
+ * @param {number} range allowed distance of travel in this turn
  * @returns {SergeHex<{}>[]} List of cells for where this asset could travel to
  */
-const allowableCells = (grid: SergeGrid<SergeHex<{}>> | undefined, constraints: PlanMobileAsset, range: number): SergeHex<{}>[] => {
+const allowableCells = (grid: SergeGrid<SergeHex<{}>> | undefined, originHex: SergeHex<{}>, range: number): SergeHex<{}>[] => {
   if(grid) {
-    const originHex = grid.find(cell => cell.name === constraints.origin)
     if(originHex) {
       return grid.hexesInRange(originHex, range, true)
     } else {
-      console.log('AllowableCells function couldnt find cell for', constraints.origin)
+      console.log('AllowableCells function couldnt find cell for', origin)
       return []
     }  
   } else {
