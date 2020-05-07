1fbc38ddf02bcc657c74f47c8e50d2b37712895b
Alex Foxleigh <alex@alexfoxleigh.com>
2020-05-07 17:41:09 +0100

    Turns component can now post back data

--- packages/components/src/local/form-elements/turns/index.tsx
+++ packages/components/src/local/form-elements/turns/index.tsx
@@ -7,9 +7,15 @@ import { TextField } from '@material-ui/core'
 import PropTypes from './types/props'
 
 /* Render component */
-export const Turns: React.FC<PropTypes> = ({ turns }) =>
-  <InputContainer label="For">
-    <TextField value={turns}></TextField> Turns
+export const Turns: React.FC<PropTypes> = ({ turns, updateState }) => {
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    updateState(event.target.value);
+  }
+
+ return <InputContainer label="For">
+    <TextField value={turns} onChange={handleChange}></TextField> Turns
   </InputContainer>
+}
 
 export default Turns
--- packages/components/src/local/form-elements/turns/types/props.d.ts
+++ packages/components/src/local/form-elements/turns/types/props.d.ts
@@ -3,4 +3,8 @@ export default interface PropTypes {
    * The number of turns
    */
   turns: number
+  /**
+   * Sets the onChange state on a parent
+   */
+  updateState?: any
 }
