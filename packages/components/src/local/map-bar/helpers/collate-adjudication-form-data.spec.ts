541e138dac7ac89bdc6961f7e78fd5afc331fb01
Ian Mayo <ian@planetmayo.com>
2020-05-19 14:14:03 +0100

    apply linting

--- packages/components/src/local/map-bar/helpers/collate-adjudication-form-data.spec.ts
+++ packages/components/src/local/map-bar/helpers/collate-adjudication-form-data.spec.ts
@@ -7,9 +7,9 @@ import forces from '@serge/mocks/forces.mock'
 import { AdjudicateTurnFormValues, ColorOption, Status } from '@serge/custom-types'
 
 it('contains relevant population results', () => {
-  const {condition, speed, status, visibleTo}: 
-    {condition:Array<string>, speed:Array<number>, status:Array<Status>, visibleTo:Array<ColorOption>} 
-    = collateAdjudicationFormData(platformTypes, selectedAsset, forces).populate
+  const { condition, speed, status, visibleTo }:
+    {condition: Array<string>, speed: Array<number>, status: Array<Status>, visibleTo: Array<ColorOption>} =
+    collateAdjudicationFormData(platformTypes, selectedAsset, forces).populate
   expect(condition.length).toEqual(5)
   expect(condition[0]).toEqual('Full capability')
   expect(speed.length).toEqual(3)
@@ -21,9 +21,9 @@ it('contains relevant population results', () => {
 })
 
 it('contains relevant current results', () => {
-  const {plannedRouteStatusVal, statusVal, speedVal, visibleToVal, conditionVal}: 
-    { plannedRouteStatusVal: string, statusVal: Status, speedVal: number, visibleToVal: Array<string>, conditionVal: string} 
-    = collateAdjudicationFormData(platformTypes, selectedAsset, forces).values
+  const { plannedRouteStatusVal, statusVal, speedVal, visibleToVal, conditionVal }:
+    { plannedRouteStatusVal: string, statusVal: Status, speedVal: number, visibleToVal: Array<string>, conditionVal: string} =
+    collateAdjudicationFormData(platformTypes, selectedAsset, forces).values
   expect(plannedRouteStatusVal).toEqual('pending')
   expect(statusVal).toEqual({ mobile: true, name: 'Transiting' })
   expect(speedVal).toEqual(10)
