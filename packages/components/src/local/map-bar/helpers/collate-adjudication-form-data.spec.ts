f1dbb9938d78a22eabb5893742df591f80feb7cf
Ian Mayo <ian@planetmayo.com>
2020-05-19 14:13:06 +0100

    dereference return value from collate method

--- packages/components/src/local/map-bar/helpers/collate-adjudication-form-data.spec.ts
+++ packages/components/src/local/map-bar/helpers/collate-adjudication-form-data.spec.ts
@@ -4,26 +4,30 @@ import collateAdjudicationFormData from './collate-adjudication-form-data'
 import platformTypes from '@serge/mocks/platform-types.mock'
 import selectedAsset from '@serge/mocks/selected-asset.mock'
 import forces from '@serge/mocks/forces.mock'
-import { AdjudicateTurnFormPopulate, AdjudicateTurnFormValues } from '@serge/custom-types'
+import { AdjudicateTurnFormValues, ColorOption, Status } from '@serge/custom-types'
 
 it('contains relevant population results', () => {
-  const res: AdjudicateTurnFormPopulate = collateAdjudicationFormData(platformTypes, selectedAsset, forces).populate
-  expect(res.condition.length).toEqual(5)
-  expect(res.condition[0]).toEqual('Full capability')
-  expect(res.speed.length).toEqual(3)
-  expect(res.speed[0]).toEqual(10)
-  expect(res.status.length).toEqual(3)
-  expect(res.status[0]).toEqual({ mobile: true, name: 'Transiting' })
-  expect(res.visibleTo.length).toEqual(5)
-  expect(res.visibleTo[0]).toEqual({ colour: '#FCFBEE', name: 'White' })
+  const {condition, speed, status, visibleTo}: 
+    {condition:Array<string>, speed:Array<number>, status:Array<Status>, visibleTo:Array<ColorOption>} 
+    = collateAdjudicationFormData(platformTypes, selectedAsset, forces).populate
+  expect(condition.length).toEqual(5)
+  expect(condition[0]).toEqual('Full capability')
+  expect(speed.length).toEqual(3)
+  expect(speed[0]).toEqual(10)
+  expect(status.length).toEqual(3)
+  expect(status[0]).toEqual({ mobile: true, name: 'Transiting' })
+  expect(visibleTo.length).toEqual(5)
+  expect(visibleTo[0]).toEqual({ colour: '#FCFBEE', name: 'White' })
 })
 
 it('contains relevant current results', () => {
-  const res: AdjudicateTurnFormValues = collateAdjudicationFormData(platformTypes, selectedAsset, forces).values
-  expect(res.plannedRouteStatusVal).toEqual('pending')
-  expect(res.statusVal).toEqual({ mobile: true, name: 'Transiting' })
-  expect(res.speedVal).toEqual(10)
-  expect(res.visibleToVal.length).toEqual(2)
-  expect(res.visibleToVal[0]).toEqual('red')
-  expect(res.conditionVal).toEqual('Full capability')
+  const {plannedRouteStatusVal, statusVal, speedVal, visibleToVal, conditionVal}: 
+    { plannedRouteStatusVal: string, statusVal: Status, speedVal: number, visibleToVal: Array<string>, conditionVal: string} 
+    = collateAdjudicationFormData(platformTypes, selectedAsset, forces).values
+  expect(plannedRouteStatusVal).toEqual('pending')
+  expect(statusVal).toEqual({ mobile: true, name: 'Transiting' })
+  expect(speedVal).toEqual(10)
+  expect(visibleToVal.length).toEqual(2)
+  expect(visibleToVal[0]).toEqual('red')
+  expect(conditionVal).toEqual('Full capability')
 })
