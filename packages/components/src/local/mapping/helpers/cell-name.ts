28bb0f9b2ed0689f7e31e9d3c46561f0fc933955
Alex Foxleigh <alex@alexfoxleigh.com>
2020-05-19 13:58:01 +0100

    Corrects error in directly importing padInteger

--- packages/components/src/local/mapping/helpers/cell-name.ts
+++ packages/components/src/local/mapping/helpers/cell-name.ts
@@ -1,9 +1,9 @@
-import padInt from '@serge/helpers/padInteger'
+import { padInteger } from '@serge/helpers'
 import { SergeHex } from '@serge/custom-types'
 
 /** Generate the Serge name for the supplied cell (1,3) == "B03" */
 const cellName = (cell: SergeHex<{}>): string => {
-  return String.fromCharCode(65 + cell.x) + padInt(cell.y)
+  return String.fromCharCode(65 + cell.x) + padInteger(cell.y)
 }
 
 export default cellName
