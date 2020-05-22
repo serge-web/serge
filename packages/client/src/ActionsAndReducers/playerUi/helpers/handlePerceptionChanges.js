9a1b09d02f100721bb27acdcbdb1f41c9e7b54e2
Ian Mayo <ian@planetmayo.com>
2020-05-22 16:27:42 +0100

    Export hex grid

--- packages/client/src/ActionsAndReducers/playerUi/helpers/handlePerceptionChanges.js
+++ packages/client/src/ActionsAndReducers/playerUi/helpers/handlePerceptionChanges.js
@@ -4,13 +4,23 @@ import findAsset from '../../../Components/Mapping/helpers/findAsset'
 export default (/* object */ payload, /* object */ allForces) => {
   const asset = findAsset(allForces, payload.asset)
   if (!asset) {
-    console.warn('failed to find asset while handling vis id', asset)
+    console.warn('failed to find asset while handling vis id', payload.asset)
   } else {
     const perception = payload.perception
     if (!asset.perceptions) {
       asset.perceptions = []
     }
-    asset.perceptions[payload.force] = perception
+    // indicate which force we're storing the perception of
+    perception.by = payload.force
+
+    // filter out perceptions for forces other than this one
+    const otherPerceptions = asset.perceptions.filter(perception => (perception.by !== payload.force))
+
+    // now add the new perception
+    otherPerceptions.push(perception)
+
+    // store the updated perceptions object
+    asset.perceptions = otherPerceptions
   }
   return allForces
 }
