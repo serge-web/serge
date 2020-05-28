c79a93b4a91da94d94901c49a43d62221ff2bffd
Ian Mayo <ian@planetmayo.com>
2020-05-28 16:35:20 +0100

    Store the color for each route - so we can handle perceived routes

--- packages/helpers/src/route-create-store.ts
+++ packages/helpers/src/route-create-store.ts
@@ -1,5 +1,6 @@
 import { RouteStore, RouteForce } from '@serge/custom-types'
 import routeCreateRoute from './route-create-route'
+import { UMPIRE_FORCE } from '@serge/config'
 
 /** determine which forces this player can control
  * @param {any} forces array of forces
@@ -16,6 +17,25 @@ export const forcesControlledBy = (forces: any, playerForce: string): Array<stri
   return res;
 }
 
+const isPerceivedBy = (perceptions: any, playerForce: string, forceColors: Array<{force: string, color: string}>,
+  undefinedColor: string): string | undefined => {
+  if(perceptions) {
+    const p = perceptions.find((p:any) => p.by === playerForce)
+    if(p) {
+      // do we know force?
+      if(p.force) {
+        const color = forceColors.find((f:any) => f.force === p.force)
+        if(color) {
+          return color.color
+        }
+      } else {
+        return undefinedColor
+      }
+    }
+  }
+  return undefined
+}
+
 /** process the forces, to create a route store - used to manage 
  * display and edits to planned routes
  * @param {any} forces array of forces
@@ -28,17 +48,32 @@ const routeCreateStore = (forces: any, playerForce: string, adjudication: boolea
   const store: RouteStore = { forces: []}
 
   const controls: Array<string> = forcesControlledBy(forces, playerForce)
+  const forceColors: Array<{force: string, color: string}> = forces.map((force: any) => {
+    return {force: force.uniqid, color: force.color}
+  })
 
   forces.forEach((force: any) => {
     // see if we control it
     const thisForce = force.uniqid
-    if (adjudication || thisForce === playerForce || controls.includes(thisForce)) {
-      if (force.assets) {
-        const routeForce: RouteForce = { uniqid: thisForce, routes: [], color: force.color }
+    if (force.assets) {
+          const routeForce: RouteForce = { uniqid: thisForce, routes: [], color: force.color }
         // loop through assets
         force.assets.forEach((asset: any) => {
-          // create route for this asset
-          routeForce.routes.push(routeCreateRoute(asset, adjudication))
+          if (playerForce === UMPIRE_FORCE || // umpire sees all
+              thisForce === playerForce ||   // this is one of our assets
+              controls.includes(thisForce))  // we can control this force
+            {
+            // create route for this asset
+            routeForce.routes.push(routeCreateRoute(asset, adjudication, force.color))
+          } else {
+            // can't see it directly. See if we can perceive it
+            const undefinedColor = '#999' // TODO: this color should not be hard-coded
+            const perceivedAs: string | undefined = isPerceivedBy(asset.perceptions, playerForce, forceColors, undefinedColor)
+            if(perceivedAs) {
+                // create route for this asset
+                routeForce.routes.push(routeCreateRoute(asset, adjudication, perceivedAs))
+            }
+          }
         })
 
         // did we create any?
@@ -46,8 +81,7 @@ const routeCreateStore = (forces: any, playerForce: string, adjudication: boolea
           store.forces.push(routeForce)
         }
       }
-    }
-  })
+    })
 
   // loop through forces
   return store
