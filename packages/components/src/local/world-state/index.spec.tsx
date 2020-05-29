dc77fef90f28247c3de6566ded147720220a1f4b
Ian Mayo <ian@planetmayo.com>
2020-05-29 16:47:24 +0100

    reduce padding, to make content more compact

--- packages/components/src/local/world-state/index.spec.tsx
+++ packages/components/src/local/world-state/index.spec.tsx
@@ -4,13 +4,12 @@ import React from 'react'
 import renderer from 'react-test-renderer'
 
 import WorldState from './index'
-// import { RouteStore } from '@serge/custom-types'
-// const store:RouteStore = {routes: []}
-//  store={store}
+import { RouteStore } from '@serge/custom-types'
+const store:RouteStore = {routes: []}
 
 it('WorldState renders correctly', () => {
   const tree = renderer
-    .create(<WorldState name="test" />)
+    .create(<WorldState store={store} name="test" />)
     .toJSON()
   expect(tree).toMatchSnapshot()
 })
