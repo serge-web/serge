db9b094faeb0af3fe05f36a627ac4b6b30bf6f26
Ian Mayo <ian@planetmayo.com>
2020-06-01 14:51:47 +0100

    minor code fix

--- packages/helpers/is-valid-url.ts
+++ packages/helpers/is-valid-url.ts
@@ -1,4 +1,4 @@
-const isValidURI = (str: string): boolean {
+const isValidURI = (str: string): boolean => {
   const httpPattern = new RegExp('^(https?:\\/\\/)?' + // protocol
     '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
     '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
