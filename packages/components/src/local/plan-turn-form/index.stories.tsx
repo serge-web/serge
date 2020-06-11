ce908bab0e7209534fba4f407133e055e73e1e08
Ian Mayo <ian@planetmayo.com>
2020-06-11 16:56:31 +0100

    Introduce knobs, to specify number of speeds

--- packages/components/src/local/plan-turn-form/index.stories.tsx
+++ packages/components/src/local/plan-turn-form/index.stories.tsx
@@ -3,14 +3,15 @@ import React from 'react'
 // Import component files
 import PlanTurnForm from './index'
 import docs from './README.md'
-
-import formData from './mocks/formData'
+import { withKnobs, radios } from '@storybook/addon-knobs'
 import { PlanTurnFormValues } from '@serge/custom-types'
+
+const wrapper: React.FC = (storyFn: any) => <div style={{ height: '600px' }}>{storyFn()}</div>
 
 export default {
   title: 'local/PlanTurnForm',
   component: PlanTurnForm,
-  decorators: [],
+  decorators: [withKnobs, wrapper],
   parameters: {
     readme: {
       // Show readme before story
@@ -26,11 +27,45 @@ const postback = (step: PlanTurnFormValues): void => {
   console.log('plan turn postback', step)
 }
 
-// TODO: Add some state handling here
+const speedLabel = 'Speed values'
+const speedValues = {
+  One: 'One',
+  Four: 'Four'
+}
 
 export const Default: React.FC = () => <PlanTurnForm
   turnPlanned={postback}
   formHeader="Planning header"
-  formData={formData}
+  formData={{
+    populate: {
+      status: [{
+        name: 'Moored',
+        mobile: false
+      },
+      {
+        name: 'Transiting',
+        mobile: true
+      }],
+      speed: radios(speedLabel, speedValues, 'Four') === 'One' ? [10] : [10, 20, 30, 40]
+    },
+    values: {
+      statusVal: {
+        name: 'Transiting',
+        mobile: true
+      },
+      speedVal: 10,
+      turnsVal: 5
+    }
+  }}
   icon={iconData}
 />
+
+// @ts-ignore TS belives the 'story' property doesn't exist but it does.
+Default.story = {
+  parameters: {
+    options: {
+      // This story requires addons but other stories in this component do not
+      showPanel: true
+    }
+  }
+};
