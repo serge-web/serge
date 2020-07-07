9e664b7e20ef9688cf3c80a56597f563b14f8d5f
Ian Mayo <ian@planetmayo.com>
2020-07-07 17:59:52 +0100

    Correctly collate data for test

--- packages/components/src/local/map-bar/helpers/collate-state-of-world.spec.ts
+++ packages/components/src/local/map-bar/helpers/collate-state-of-world.spec.ts
@@ -7,7 +7,7 @@ import { routeCreateStore } from '@serge/helpers'
 import { RouteStore } from '@serge/custom-types'
 
 it('contains relevant population results', () => {
-  const store: RouteStore = routeCreateStore(forces, 'Blue', false, platforms)
+  const store: RouteStore = routeCreateStore(forces, 'Red', false, platforms)
   const stateOfWorld = collateStateOfWorld(store.routes, 3)
   expect(stateOfWorld).toBeDefined()
   expect(stateOfWorld.turn).toEqual(4)
@@ -15,7 +15,7 @@ it('contains relevant population results', () => {
   const force = stateOfWorld.detail[1]
   expect(force.name).toEqual('Red')
   const asset = force.assets[0]
-  expect(asset.name).toEqual('C065')
+  expect(asset.name).toEqual('Dhow-A')
   expect(asset.plannedTurns.length).toEqual(3)
   expect(asset.history.length).toEqual(1)
   expect(asset.newState).toEqual({ speedKts: 10, state: 'Transiting' })
