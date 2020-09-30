fbfcfc147c22f5e656805ca4cf311f9f3299b4fd
Heri Setiawan <heri.dehero@gmail.com>
2020-09-30 09:11:57 +0700

    Implement turn progression component to GameChannels

--- packages/client/src/Views/GameChannels.jsx
+++ packages/client/src/Views/GameChannels.jsx
@@ -1,12 +1,11 @@
-import React, { Component } from "react";
-import classNames from "classnames";
-import { showHideObjectives } from "../ActionsAndReducers/playerUi/playerUi_ActionCreators";
-import TurnProgression from "../Components/TurnProgression";
-import ChannelTabsContainer from "./ChannelTabsContainer";
-import AdminAndInsightsTabsContainer from "./AdminAndInsightsTabsContainer";
-import { PlayerStateContext } from "../Store/PlayerUi";
-
-import "@serge/themes/App.scss";
+import React, { Component } from 'react';
+import classNames from 'classnames';
+import { TurnProgression } from '@serge/components';
+import { nextGameTurn, showHideObjectives } from '../ActionsAndReducers/playerUi/playerUi_ActionCreators';
+import ChannelTabsContainer from './ChannelTabsContainer';
+import AdminAndInsightsTabsContainer from './AdminAndInsightsTabsContainer';
+import { PlayerStateContext } from '../Store/PlayerUi';
+import '@serge/themes/App.scss';
 
 class GameChannels extends Component {
   static contextType = PlayerStateContext;
@@ -14,6 +13,11 @@ class GameChannels extends Component {
   showHideForceObjectives = () => {
     const [ , dispatch ] = this.context;
     dispatch(showHideObjectives());
+  };
+
+  nextTurn = () => {
+    const [ state ] = this.context;
+    nextGameTurn(state.currentWargame)();
   };
 
   render() {
@@ -35,26 +39,35 @@ class GameChannels extends Component {
           <ChannelTabsContainer ref={el => window.channelTabsContainer[force.uniqid] = el} />
         </div>
         <div className={classNames({"message-feed": true, "out-of-game-feed": true, "umpire-feed": state.controlUi})} data-tour="fifth-step">
-          <TurnProgression />
+          <TurnProgression
+            adjudicationStartTime={state.adjudicationStartTime}
+            controlUi={state.controlUi}
+            currentTurn={state.currentTurn}
+            gameDate={state.gameDate}
+            onNextTurn={this.nextTurn}
+            phase={state.phase}
+            timeWarning={state.timeWarning}
+            turnEndTime={state.turnEndTime}
+          />
           <AdminAndInsightsTabsContainer />
         </div>
         { state.showObjective &&
-          <div className="force-objectives" style={{borderColor: state.forceColor}}>
-            <h3>Objectives</h3>
-            <div className="objective-text">
-              {force.overview}
-            </div>
+        <div className="force-objectives" style={{borderColor: state.forceColor}}>
+          <h3>Objectives</h3>
+          <div className="objective-text">
+            {force.overview}
+          </div>
 
-            <div className="role-info" style={{ backgroundColor: state.forceColor, }}>
-                <span className="role-type">&nbsp;</span>
-                <div className="contain-force-skin">
-                    <div className="force-skin">
-                        <span className="force-type">{ force.name }</span>
-                        <img className="role-icon" src={force.icon} alt="" onClick={this.showHideForceObjectives} />
-                    </div>
-                </div>
+          <div className="role-info" style={{ backgroundColor: state.forceColor, }}>
+            <span className="role-type">&nbsp;</span>
+            <div className="contain-force-skin">
+              <div className="force-skin">
+                <span className="force-type">{ force.name }</span>
+                <img className="role-icon" src={force.icon} alt="" onClick={this.showHideForceObjectives} />
+              </div>
             </div>
           </div>
+        </div>
         }
       </div>
     )
