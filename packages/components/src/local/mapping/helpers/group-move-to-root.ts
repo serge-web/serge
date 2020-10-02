a9ec928f27b2adf6d4248967ef980f0eb163e363
Ian Mayo <ian@planetmayo.com>
2020-10-02 18:04:17 +0100

    tests passing

--- packages/components/src/local/mapping/helpers/group-move-to-root.ts
+++ packages/components/src/local/mapping/helpers/group-move-to-root.ts
@@ -19,34 +19,69 @@ const groupMoveToRoot = (uniqid: string, forces: any): any => {
   newForces.forEach((force: any) => {
     if(force.assets && !parentForce) {
       force.assets.forEach((asset:any) => {
-        const hosted = findInList(uniqid, asset.hosting)    
-        const contains = findInList(uniqid, asset.comprising)
-        if(hosted || contains) {
-          parentAsset = asset
-          parentListType = hosted ? listType.HOSTING : listType.COMPRISING
-          parentForce = force
-          theAsset = hosted || contains
+        // only carry on hunting if we haven't found it
+        if(!parentForce) {
+          const hosted = findInList(uniqid, asset.hosting)    
+          const contains = findInList(uniqid, asset.comprising)
+          if(hosted || contains) {
+            parentAsset = asset
+            parentListType = hosted ? listType.HOSTING : listType.COMPRISING
+            parentForce = force
+            theAsset = hosted || contains
+          } else {
+            // we have to search a layer deeper
+            if(asset.hosting) {
+              asset.hosting.forEach((asset2: any) => {
+                const hosted = findInList(uniqid, asset2.hosting)    
+                const contains = findInList(uniqid, asset2.comprising)
+                if(hosted || contains) {
+                  parentAsset = asset2
+                  parentListType = hosted ? listType.HOSTING : listType.COMPRISING
+                  parentForce = force
+                  theAsset = hosted || contains
+                }      
+              })
+            }
+            if(asset.comprising) {
+              asset.comprising.forEach((asset2: any) => {
+                const hosted = findInList(uniqid, asset2.hosting)    
+                const contains = findInList(uniqid, asset2.comprising)
+                if(hosted || contains) {
+                  parentAsset = asset2
+                  parentListType = hosted ? listType.HOSTING : listType.COMPRISING
+                  parentForce = force
+                  theAsset = hosted || contains
+                }      
+              })             
+            }
+          }
         }
       })
     }
   })
 
-  // remove from the parent
-  switch(parentListType) {
-    case listType.HOSTING : {
-      parentAsset.hosting = parentAsset.hosting.filter((asset: any) => asset.uniqid !== uniqid)
-      break
+  // did it work?
+  if(!parentForce) {
+    return undefined
+  } else {
+    // remove from the parent
+    switch(parentListType) {
+      case listType.HOSTING : {
+        parentAsset.hosting = parentAsset.hosting.filter((asset: any) => asset.uniqid !== uniqid)
+        break
+      }
+      case listType.COMPRISING : {
+        parentAsset.comprising = parentAsset.comprising.filter((asset: any) => asset.uniqid !== uniqid)
+        break
+      }
     }
-    case listType.COMPRISING : {
-      parentAsset.comprising = parentAsset.comprising.filter((asset: any) => asset.uniqid !== uniqid)
-      break
-    }
+
+    // add at the top level
+    parentForce.assets.push(theAsset)
+
+    return newForces
+
   }
-
-  // add at the top level
-  parentForce.assets.push(theAsset)
-
-  return newForces
 }
 
 const findInList = (uniqid: string, items: any): any => {
