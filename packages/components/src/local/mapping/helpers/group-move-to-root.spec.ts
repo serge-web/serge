3f99a84619ba5b198e55885a517d04455df8ee7f
Ian Mayo <ian@planetmayo.com>
2020-10-02 16:58:22 +0100

    partially working implementation

--- packages/components/src/local/mapping/helpers/group-move-to-root.spec.ts
+++ packages/components/src/local/mapping/helpers/group-move-to-root.spec.ts
@@ -5,13 +5,76 @@ import { forces } from '@serge/mocks'
 
 import groupMoveToRoot from './group-move-to-root'
 
-it('returns correct combine with answers', () => {
+it('Moves merlin to top level from hosting', () => {
+  const merlinId = 'a0pra11002'
   // check merlin is where we expect it to be
-  // if()
-  // expect(forces[1].a)
+  if(forces[1].assets && forces[1].assets[1]) {
+    const frigate:any = forces[1].assets[1]
+    expect(frigate.hosting[0].uniqid).toEqual(merlinId)
+  }
 
   // move the merlin to the top level
-  const merlinId = 'a0pra11002'
   const forces2: any = groupMoveToRoot(merlinId, forces)
   expect(forces2).toBeTruthy()
+
+  // check merlin isn't on frigate
+  if(forces2[1].assets && forces2[1].assets[1]) {
+    const uavId = 'a0pra18702'
+    const frigate:any = forces2[1].assets[1]
+    expect(frigate.hosting[0].uniqid).toEqual(uavId)
+  }
+
+  // check merlin is where we expect it to be
+  if(forces2[1].assets && forces2[1].assets[4]) {
+    expect(forces2[1].assets[4].uniqid).toEqual(merlinId)
+  }
 })
+
+it('Moves mcmv to top level from hosting', () => {
+  const mcmvId = 'a0traa6790'
+  // check mcm is where we expect it to be
+  if(forces[1].assets && forces[1].assets[0]) {
+    const taskGroup:any = forces[1].assets[0]
+    expect(taskGroup.comprising.length).toEqual(2)
+    expect(taskGroup.comprising[1].uniqid).toEqual(mcmvId)
+  }
+
+  // move the mcm to the top level
+  const forces2: any = groupMoveToRoot(mcmvId, forces)
+  expect(forces2).toBeTruthy()
+
+  // check mcm isn't in task group
+  if(forces2[1].assets && forces2[1].assets[0]) {
+    const taskGroup:any = forces2[1].assets[0]
+    expect(taskGroup.comprising.length).toEqual(1)
+  }
+
+  // check mcm is where we expect it to be
+  if(forces2[1].assets && forces2[1].assets[4]) {
+    expect(forces2[1].assets[4].uniqid).toEqual(mcmvId)
+  }
+})
+
+// it('Moves uav to top level from comprising and hosting', () => {
+//   const uavId = 'a0pra43302'
+//   // check merlin is where we expect it to be
+//   if(forces[1].assets && forces[1].assets[0]) {
+//     const taskGroup:any = forces[1].assets[0]
+//     expect(taskGroup).toBeTruthy()
+//     const frigate = taskGroup.comprising[0]
+//     expect(frigate).toBeTruthy()
+//     const uav = frigate.hosting[0]
+//     expect(uav).toBeTruthy()
+//     expect(uav.uniqid).toEqual(uavId)
+//   }
+
+//   // move the merlin to the top level
+//   const forces2: any = groupMoveToRoot(uavId, forces)
+//   expect(forces2).toBeTruthy()
+
+//   // check it has moved
+//   // check merlin is where we expect it to be
+//   if(forces2[1].assets && forces2[1].assets[5]) {
+//     expect(forces2[1].assets[4].uniqid).toEqual(uavId)
+//   }
+// })
--- packages/components/src/local/mapping/helpers/group-move-to-root.ts
+++ packages/components/src/local/mapping/helpers/group-move-to-root.ts
@@ -1,10 +1,57 @@
-
+import { cloneDeep } from 'lodash'
 /**
  * Move the specified child asset to the root level for that force
+ * @param (string) uniqid the asset to move to root
+ * @param (any) forces list of forces
+ * @returns modified list of forces
  */
 const groupMoveToRoot = (uniqid: string, forces: any): any => {
-  console.log('moving to root', uniqid, forces && forces.length)
-  return forces
+  const newForces: any = cloneDeep(forces)
+  // find the force
+  var parentForce: any
+  var parentListType: listType | undefined
+  var parentAsset: any
+  var theAsset: any
+  enum listType  {
+    HOSTING,
+    COMPRISING
+  }
+  newForces.forEach((force: any) => {
+    if(force.assets && !parentForce) {
+      force.assets.forEach((asset:any) => {
+        const hosted = findInList(uniqid, asset.hosting)    
+        const contains = findInList(uniqid, asset.comprising)
+        if(hosted || contains) {
+          parentAsset = asset
+          parentListType = hosted ? listType.HOSTING : listType.COMPRISING
+          parentForce = force
+          theAsset = hosted || contains
+        }
+      })
+    }
+  })
+
+  // remove from the parent
+  switch(parentListType) {
+    case listType.HOSTING : {
+      parentAsset.hosting = parentAsset.hosting.filter((asset: any) => asset.uniqid !== uniqid)
+      break
+    }
+    case listType.COMPRISING : {
+      parentAsset.comprising = parentAsset.comprising.filter((asset: any) => asset.uniqid !== uniqid)
+      break
+    }
+  }
+
+  // add at the top level
+  parentForce.assets.push(theAsset)
+
+  return newForces
 }
+
+const findInList = (uniqid: string, items: any): any => {
+  return items && items.length && items.find((item: any) => item.uniqid === uniqid)
+}
+
 
 export default groupMoveToRoot
