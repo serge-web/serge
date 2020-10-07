a5789bb5d1242d99cb0b7d6a47e84b2038d49e36
Ian Mayo <ian@planetmayo.com>
2020-10-07 16:13:43 +0100

    Allow more diverse expression of coordinates

--- packages/custom-types/route-step.d.ts
+++ packages/custom-types/route-step.d.ts
@@ -1,9 +1,18 @@
 import Role from './role'
 import RouteStatus from './route-status'
 
+/** when coords come from database they frequently
+ * aren't in L.LatLng format, they're simpler. 
+ * Allow this simpler structure
+ */
+interface SimpleCoords {
+  lat: number,
+  lng: number
+}
+
 export default interface RouteStep {
   turn: number,
   coords?: Array<string>,
-  locations?: Array<L.LatLng>,
+  locations?: Array<L.LatLng | SimpleCoords>,
   status: RouteStatus
 }
