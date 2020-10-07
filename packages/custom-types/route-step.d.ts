e538f6e731e3fb6195080f94161207f9c40cb681
Ian Mayo <ian@planetmayo.com>
2020-10-07 16:20:43 +0100

    Reinstate previous type-definition. Wider impacts weren't identified.

--- packages/custom-types/route-step.d.ts
+++ packages/custom-types/route-step.d.ts
@@ -1,18 +1,9 @@
 import Role from './role'
 import RouteStatus from './route-status'
 
-/** when coords come from database they frequently
- * aren't in L.LatLng format, they're simpler. 
- * Allow this simpler structure
- */
-interface SimpleCoords {
-  lat: number,
-  lng: number
-}
-
 export default interface RouteStep {
   turn: number,
   coords?: Array<string>,
-  locations?: Array<L.LatLng | SimpleCoords>,
+  locations?: Array<L.LatLng>,
   status: RouteStatus
 }
