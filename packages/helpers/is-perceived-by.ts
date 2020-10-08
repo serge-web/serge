5f218a60447b590eb5c45b6975b75f569b850f0a
Ian Mayo <ian@planetmayo.com>
2020-10-08 13:55:04 +0100

    return undefined color, if it can be perceived but hte force is unknown

--- packages/helpers/is-perceived-by.ts
+++ packages/helpers/is-perceived-by.ts
@@ -4,10 +4,11 @@ import { upperFirst } from 'lodash'
  * @param {any} perceptions how an asset is perceived
  * @param {string} playerForce the force for the current player
  * @param {Array<{force: string, color: string}>} forceColors couplets of force & color to use for that color
+ * @param {string} undefinedColor the color to use for non-perceived types
  * @return {string | undefined} color shade to use, or undefined if asset isn't visible
  */
 const isPerceivedBy = (perceptions: any, playerForce: string, 
-  forceColors: Array<{force: string, color: string}>): string | undefined => {
+  forceColors: Array<{force: string, color: string}>, undefinedColor: string): string | undefined => {
   if(perceptions) {
     if(Array.isArray(perceptions)) {
       const p = perceptions.find((p:any) => p.by.toLowerCase() === playerForce.toLowerCase())
@@ -18,13 +19,15 @@ const isPerceivedBy = (perceptions: any, playerForce: string,
           if(color) {
             return color.color
           } else {
-            // force color not know, so probably 'unknown'. Return unknown shade
-            return undefined
+            // force color not known, so probably 'unknown'. Return unknown shade
+            return undefinedColor
           }
         } else {
-          return undefined
+          // perceive it, but don't know force
+          return undefinedColor
         }
       } else {
+        // not perceived
         return undefined
       }  
     } else {
