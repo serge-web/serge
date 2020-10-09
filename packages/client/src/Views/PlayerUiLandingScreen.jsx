0a1f18f2cda2dc2d8c9da6f37bb6a61675b5d7f9
Heri Setiawan <heri.dehero@gmail.com>
2020-10-09 19:15:06 +0700

    Add storybook shortcut

--- packages/client/src/Views/PlayerUiLandingScreen.jsx
+++ packages/client/src/Views/PlayerUiLandingScreen.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import lineBreak from '../Helpers/splitNewLineBreak'
-import { ADMIN_ROUTE } from '../consts'
+import { ADMIN_ROUTE, STORYBOOK_ROUTE } from '../consts'
 import { faTools } from '@fortawesome/free-solid-svg-icons'
+import { ReactComponent as StorybookIcon } from '../storybook-logo.svg'
 
 export default function PlayerUiLandingScreen ({ gameInfo, enterSerge }) {
   return (
@@ -14,9 +15,14 @@ export default function PlayerUiLandingScreen ({ gameInfo, enterSerge }) {
               gameInfo.imageUrlSet ? <img className="serge-logo" src={gameInfo.imageUrl} alt="Serge gaming" /> : null
             }
           </div>
-          <a href={ADMIN_ROUTE} className="link-admin">
-            <FontAwesomeIcon icon={faTools} />
-          </a>
+          <div className="shortcuts">
+            <a href={ADMIN_ROUTE} className="link-admin">
+              <FontAwesomeIcon icon={faTools} />
+            </a>
+            <a href={STORYBOOK_ROUTE} className="link-admin">
+              <StorybookIcon />
+            </a>
+          </div>
           <div className="welcome-desc">
             <h1>{gameInfo.title}</h1>
             {lineBreak(gameInfo.description)}
