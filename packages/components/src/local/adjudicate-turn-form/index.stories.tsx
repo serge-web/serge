a81146afa8444d457ca75ea75283029ac22aadde
Ian Mayo <ian@planetmayo.com>
2020-10-12 12:55:59 +0100

    linter tidying

--- packages/components/src/local/adjudicate-turn-form/index.stories.tsx
+++ packages/components/src/local/adjudicate-turn-form/index.stories.tsx
@@ -24,6 +24,6 @@ const postback = (messageType: string, payload: any): void => {
   console.log('postback', messageType, payload)
 }
 
-export const Default: React.FC = () => <AdjudicateTurnForm postBack={postback} icon={iconData}  plansSubmitted={false} formHeader="Adjudicate header" formData={formData} />
+export const Default: React.FC = () => <AdjudicateTurnForm postBack={postback} icon={iconData} plansSubmitted={false} formHeader="Adjudicate header" formData={formData} />
 
 export const TurnsPlanned: React.FC = () => <AdjudicateTurnForm postBack={postback} icon={iconData} plansSubmitted={true} formHeader="Adjudicate header" formData={formData} />
