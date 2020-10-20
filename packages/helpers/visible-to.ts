124d5afd5cf2429bd34a3a453b8ae0710ac0e132
Ian Mayo <ian@planetmayo.com>
2020-10-20 09:15:14 +0100

    Support legacy perception model. Unit test to verify

--- packages/helpers/visible-to.ts
+++ packages/helpers/visible-to.ts
@@ -1,5 +1,12 @@
 /** produce a list of the forces that can see this asset */
 export default function visibleTo ( perceptions: any): string[] {
-  const visibleTo: Array<string> = perceptions && perceptions.length ? perceptions.map((p: any) => p.by) : []
-  return visibleTo
+  if(Array.isArray(perceptions)) {
+    // current perceptions schema
+    const visibleTo: Array<string> = perceptions && perceptions.length ? perceptions.map((p: any) => p.by) : []
+    return visibleTo
+  } else {
+    // legacy perceptions schema
+    const dictAsArray: Array<any> = Object.keys(perceptions)
+    return dictAsArray
+  }
 };
