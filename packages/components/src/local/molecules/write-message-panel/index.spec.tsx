2741dee3e3744aad622f70b6640abc12d537fa93
Ian Mayo <ian@planetmayo.com>
2020-10-26 14:08:29 +0000

    Remove `describe` block

--- packages/components/src/local/molecules/write-message-panel/index.spec.tsx
+++ packages/components/src/local/molecules/write-message-panel/index.spec.tsx
@@ -3,13 +3,11 @@ import React from 'react'
 import renderer from 'react-test-renderer'
 import WriteMessagePanel from './index'
 
-describe('WriteMessagePanel component:', () => {
-  it('renders correctly', () => {
-    const tree = renderer
-      .create(
-        <WriteMessagePanel isUmpire={true}/>
-      )
-      .toJSON()
-    expect(tree).toMatchSnapshot()
-  })
+it('renders correctly', () => {
+  const tree = renderer
+    .create(
+      <WriteMessagePanel isUmpire={true}/>
+    )
+    .toJSON()
+  expect(tree).toMatchSnapshot()
 })
