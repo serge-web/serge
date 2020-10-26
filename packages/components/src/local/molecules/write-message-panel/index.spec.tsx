f0a9fe0d304ad1b4d9bfedcc87f771e024823ed1
Ian Mayo <ian@planetmayo.com>
2020-10-26 15:54:37 +0000

    Introduce workaround to make JEST tests pass

--- packages/components/src/local/molecules/write-message-panel/index.spec.tsx
+++ packages/components/src/local/molecules/write-message-panel/index.spec.tsx
@@ -3,10 +3,22 @@ import React from 'react'
 import renderer from 'react-test-renderer'
 import WriteMessagePanel from './index'
 
+/** mock function to workaround issue where multiline textarea
+ * failing, as documented here:
+ * https://github.com/mui-org/material-ui/issues/16491
+ */
+const createNodeMock = (element: any): any => {
+  if (element.type === "textarea") {
+    return document.createElement("textarea");
+  } else {
+    return null;
+  }
+};
+
 it('renders correctly', () => {
   const tree = renderer
     .create(
-      <WriteMessagePanel isUmpire={true}/>
+      <WriteMessagePanel isUmpire={true}/>, {createNodeMock}
     )
     .toJSON()
   expect(tree).toMatchSnapshot()
