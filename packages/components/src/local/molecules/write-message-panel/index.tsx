f7e65eff9b557ce99d658d8b9308dbdfcd0d65a1
Ian Mayo <ian@planetmayo.com>
2020-10-26 12:03:21 +0000

    Remove knobs, and normal-message placeholder

--- packages/components/src/local/molecules/write-message-panel/index.tsx
+++ packages/components/src/local/molecules/write-message-panel/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import PrivateChatTextInput from '../../atoms/private-chat-text-input'
 import ChatTextInput from '../../atoms/chat-text-input'
-import { boolean, number } from '@storybook/addon-knobs'
 
 /* Import Types */
 import Props from './types/props'
@@ -9,18 +8,12 @@ import Props from './types/props'
 /* Import Stylesheet */
 import styles from './styles.module.scss'
 
-const rowValues = {
-  range: true,
-  min: 1,
-  max: 4,
-  step: 1
-}
-
 /* Render component */
 export const WriteMessagePanel: React.FC<Props> = () => {
   return (
     <div className={styles['write-message-panel']}>
-      { <ChatTextInput multiline={boolean('Multi-Line', true)} fullWidth variant="filled" placeholder="A private message is sent at the same time as the main message, but is only visible to other umpires." rowsMax={number('Max rows', 4, rowValues)} rows={number('Rows', 2, rowValues)} /> }
+      <ChatTextInput multiline={true} fullWidth variant="filled" 
+          rowsMax={4} rows={2} />
       <div className={styles['send-message-icon']}>
         <img src="./images/send_message.png" />
       </div>
