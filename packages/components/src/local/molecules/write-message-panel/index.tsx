1a88573b42f3bb1d675411cbfbb02dacecc41fc3
Ian Mayo <ian@planetmayo.com>
2020-10-26 15:54:57 +0000

    Make it multi-line again

--- packages/components/src/local/molecules/write-message-panel/index.tsx
+++ packages/components/src/local/molecules/write-message-panel/index.tsx
@@ -12,8 +12,13 @@ import styles from './styles.module.scss'
 export const WriteMessagePanel: React.FC<Props> = ({ isUmpire }: Props) => {
   return (
     <div className={styles['write-message-panel']}>
-      <ChatTextInput multiline={false} fullWidth variant="filled"
-        rowsMax={4} rows={2}/>
+      <ChatTextInput 
+        multiline={true} 
+        fullWidth 
+        variant="filled"
+       rowsMax={4} 
+       rows={2}
+        />
       <div className={styles['send-message-icon']}>
         <img src="./images/send_message.png" />
       </div>
