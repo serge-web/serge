c3fde40beb8ed1c5f1af63386a1746b0b7ef7b2f
Heena Trivedi <trivediheenalove@gmail.com>
2020-12-09 12:41:22 +0530

    fixed private message focus issue

--- packages/components/src/local/atoms/private-chat-text-input/index.tsx
+++ packages/components/src/local/atoms/private-chat-text-input/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Collapsible from '../../helper-elements/collapsible'
+import Collapsible from 'react-collapsible';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faUserSecret, faPaperclip } from '@fortawesome/free-solid-svg-icons'
 import ChatTextInput from '../chat-text-input'
@@ -12,12 +12,10 @@ import styles from './styles.module.scss'
 
 /* Render component */
 export const PrivateChatTextInput: React.FC<Props> = ({ onChange, privateMessage }: Props) => {
-  const CollapsibleHeader = ({ onExpand, collapsed }: any): React.ReactElement => {
-    const handleOnExpand = (): void => {
-      onExpand(!collapsed)
-    }
-    return (
-      <div onClick={handleOnExpand} className={styles['header-collapsible']}>
+  return (
+    <div className={styles['private-chat-text-input']}>
+      <Collapsible transitionTime={300} trigger={<>
+        <div className={styles['header-collapsible']}>
         <span>
           <span className={styles['icon-paperclip']}>
             <FontAwesomeIcon size='1x' icon={faPaperclip} />
@@ -28,31 +26,12 @@ export const PrivateChatTextInput: React.FC<Props> = ({ onChange, privateMessage
           Private message
         </span>
       </div>
-    )
-  }
-
-  const CollapsibleContent = ({ collapsed }: any): React.ReactElement => {
-    return (
-      <>
-        {!collapsed
-          ? <div className={styles['chat-text-input']}>
-            <ChatTextInput multiline={true} fullWidth variant="filled"
+      </>}>
+      <ChatTextInput multiline={true} fullWidth variant="filled"
               placeholder="A private message is sent at the same time as the main message, but is only visible to other umpires."
               rowsMax={4} rows={2} content={privateMessage} updateState={(event: HTMLInputElement): void => onChange(event)}/>
-          </div> : null
-        }
-      </>
-    )
-  }
-
-  return (
-    <div className={styles['private-chat-text-input']}>
-      <Collapsible
-        header={<CollapsibleHeader />}
-        content={<CollapsibleContent />}
-      />
+      </Collapsible>
     </div>
   )
 }
-
 export default PrivateChatTextInput
