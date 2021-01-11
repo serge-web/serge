1202936a2519a09b9d52a23071aafd2a541c602d
Ian Mayo <ian@planetmayo.com>
2021-01-11 17:10:38 +0000

    restore all tests

--- packages/components/src/local/route/helpers/planned-routes-for.spec.tsx
+++ packages/components/src/local/route/helpers/planned-routes-for.spec.tsx
@@ -6,36 +6,40 @@ import { routeCreateStore } from '@serge/helpers'
 import { plannedRoutesFor } from './planned-routes-for'
 import RouteData from '../types/route-data'
 
-// it('Provides valid planned for multi-point planned with filter', () => {
-//   const store: RouteStore = routeCreateStore(undefined, forces, 'Red', platformTypes, undefined, true, true)
-//   expect(store).toBeTruthy()
-//   expect(store.routes.length).toEqual(9)
-//   const route: Route | undefined = store.routes.find(route => route.name === 'Dhow-A')
-//   expect(route).toBeTruthy()
-//   if (route) {
-//     const data: RouteData = plannedRoutesFor(route.currentLocation, route.planned)
-//     expect(data.turnEnds.length).toEqual(1)
-//     expect(data.turnEnds[0].current.pos.lat).toEqual(12.2)
-//     expect(data.turnEnds[0].current.pos.lng).toEqual(23.4)
-//     // and the polyline
-//     expect(data.polyline.length).toEqual(3)
-//   }
-// })
+it('Provides valid planned for multi-point planned with filter', () => {
+  const store: RouteStore = routeCreateStore(undefined, forces, 'Red', platformTypes, undefined, true, true)
+  expect(store).toBeTruthy()
+  expect(store.routes.length).toEqual(9)
+  const route: Route | undefined = store.routes.find(route => route.name === 'Dhow-A')
+  expect(route && route.currentLocation).toBeTruthy()
+  if (route && route.currentLocation) {
+    const data: RouteData = plannedRoutesFor(route.currentLocation, route.planned)
+    expect(data.turnEnds.length).toEqual(1)
+    expect(data.turnEnds[0].current.pos.lat).toEqual(12.2)
+    expect(data.turnEnds[0].current.pos.lng).toEqual(23.4)
+    // and the polyline
+    expect(data.polyline.length).toEqual(3)
+  } else {
+    expect(false).toBeTruthy() // should have route
+  }
+})
 
-// it('Provides valid planned for multi-point including static steps planned without filter', () => {
-//   const store: RouteStore = routeCreateStore('a0pra000100', forces, 'Red', platformTypes, undefined, true, false)
-//   expect(store.routes.length).toEqual(9)
-//   const route: Route | undefined = store.selected
-//   expect(route).toBeTruthy()
-//   if (route) {
-//     const data: RouteData = plannedRoutesFor(route.currentLocation, route.planned)
-//     expect(data.turnEnds.length).toEqual(3)
-//     expect(data.turnEnds[0].current.pos.lat).toEqual(12.2)
-//     expect(data.turnEnds[0].current.pos.lng).toEqual(23.4)
-//     // and the polyline
-//     expect(data.polyline.length).toEqual(4)
-//   }
-// })
+it('Provides valid planned for multi-point including static steps planned without filter', () => {
+  const store: RouteStore = routeCreateStore('a0pra000100', forces, 'Red', platformTypes, undefined, true, false)
+  expect(store.routes.length).toEqual(9)
+  const route: Route | undefined = store.selected
+  expect(route).toBeTruthy()
+  if (route && route.currentLocation) {
+    const data: RouteData = plannedRoutesFor(route.currentLocation, route.planned)
+    expect(data.turnEnds.length).toEqual(3)
+    expect(data.turnEnds[0].current.pos.lat).toEqual(12.2)
+    expect(data.turnEnds[0].current.pos.lng).toEqual(23.4)
+    // and the polyline
+    expect(data.polyline.length).toEqual(4)
+  } else {
+    expect(false).toBeTruthy() // should have route
+  }
+})
 
 it('Provides valid planned for multi-point planned without filter', () => {
   const store: RouteStore = routeCreateStore(undefined, forces, 'Red', platformTypes, undefined, true, false)
@@ -66,36 +70,40 @@ it('Provides valid planned for multi-point planned without filter', () => {
     // and the polyline
     expect(data.polyline.length).toEqual(4)
   } else {
-    expect(false).toBeTruthy()
+    expect(false).toBeTruthy() // should have route
   }
 })
 
-// it('Provides valid planned for single-point planned with filter', () => {
-//   const store: RouteStore = routeCreateStore(undefined, forces, 'Red', platformTypes, undefined, true, true)
-//   expect(store.routes.length).toEqual(9)
-//   const route: Route | undefined = store.routes.find(route => route.name === 'Dhow-B')
-//   expect(route).toBeTruthy()
-//   if (route) {
-//     const data: RouteData = plannedRoutesFor(route.currentLocation, route.planned)
-//     expect(data.turnEnds.length).toEqual(1)
-//     expect(data.turnEnds[0].current.pos.lat).toEqual(12.2)
-//     expect(data.turnEnds[0].current.pos.lng).toEqual(23.4)
-//     // and the polyline
-//     expect(data.polyline.length).toEqual(2)
-//   }
-// })
+it('Provides valid planned for single-point planned with filter', () => {
+  const store: RouteStore = routeCreateStore(undefined, forces, 'Red', platformTypes, undefined, true, true)
+  expect(store.routes.length).toEqual(9)
+  const route: Route | undefined = store.routes.find(route => route.name === 'Dhow-B')
+  expect(route).toBeTruthy()
+  if (route && route.currentLocation) {
+    const data: RouteData = plannedRoutesFor(route.currentLocation, route.planned)
+    expect(data.turnEnds.length).toEqual(1)
+    expect(data.turnEnds[0].current.pos.lat).toEqual(12.2)
+    expect(data.turnEnds[0].current.pos.lng).toEqual(23.4)
+    // and the polyline
+    expect(data.polyline.length).toEqual(2)
+  } else {
+    expect(false).toBeTruthy() // should have route
+  }
+})
 
-// it('Provides valid planned for single-point planned without filter', () => {
-//   const store: RouteStore = routeCreateStore(undefined, forces, 'Red', platformTypes, undefined, true, false)
-//   expect(store.routes.length).toEqual(9)
-//   const route: Route | undefined = store.routes.find(route => route.name === 'Dhow-B')
-//   expect(route).toBeTruthy()
-//   if (route) {
-//     const data: RouteData = plannedRoutesFor(route.currentLocation, route.planned)
-//     expect(data.turnEnds.length).toEqual(5)
-//     expect(data.turnEnds[0].current.pos.lat).toEqual(12.2)
-//     expect(data.turnEnds[0].current.pos.lng).toEqual(23.4)
-//     // and the polyline
-//     expect(data.polyline.length).toEqual(4)
-//   }
-// })
+it('Provides valid planned for single-point planned without filter', () => {
+  const store: RouteStore = routeCreateStore(undefined, forces, 'Red', platformTypes, undefined, true, false)
+  expect(store.routes.length).toEqual(9)
+  const route: Route | undefined = store.routes.find(route => route.name === 'Dhow-B')
+  expect(route).toBeTruthy()
+  if (route && route.currentLocation) {
+    const data: RouteData = plannedRoutesFor(route.currentLocation, route.planned)
+    expect(data.turnEnds.length).toEqual(5)
+    expect(data.turnEnds[0].current.pos.lat).toEqual(12.2)
+    expect(data.turnEnds[0].current.pos.lng).toEqual(23.4)
+    // and the polyline
+    expect(data.polyline.length).toEqual(4)
+  } else {
+    expect(false).toBeTruthy() // should have route
+  }
+})
