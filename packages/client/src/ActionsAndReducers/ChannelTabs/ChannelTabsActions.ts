04dbdd2d85a4fc51ad35e5b5f6b93b5c821f317f
Lilit Karapetyan <lilit.karapetyan.web@gmail.com>
2021-01-19 14:03:59 +0400

    remove channels tab reducer (unused)

--- packages/client/src/ActionsAndReducers/ChannelTabs/ChannelTabsActions.ts
+++
@@ -1,7 +0,0 @@
-import ChannelTabsActionTypes from './types.d'
-export const SET_CHANNEL_TAB = 'SET_CHANNEL_TAB'
-
-export const setChannelTab = (forceId: string): ChannelTabsActionTypes => ({
-  type: SET_CHANNEL_TAB,
-  payload: forceId
-})
