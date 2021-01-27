817153c52ee3f9a36150d57ffda998353f6acf7d
Ian Mayo <ian@planetmayo.com>
2021-01-27 11:46:59 +0000

    Clear planned/historic routes once platform laydown changed

--- packages/client/src/ActionsAndReducers/playerUi/helpers/handleForceLaydownChanges.ts
+++ packages/client/src/ActionsAndReducers/playerUi/helpers/handleForceLaydownChanges.ts
@@ -5,6 +5,9 @@ export default (payload: MessageForceLaydown, allForces: ForceData[]): ForceData
   payload.updates.forEach((update:{uniqid: string, position: string }) => {
     const asset: Asset = findAsset(allForces, update.uniqid)
     asset.position = update.position
+    // also clear planned & historic routes
+    asset.history = undefined
+    asset.plannedTurns = undefined
   })
   return allForces
 }
