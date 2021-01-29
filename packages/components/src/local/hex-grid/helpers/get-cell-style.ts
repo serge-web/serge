1324f85a842868404c71990edaff1bcbffe146dd
Ian Mayo <ian@planetmayo.com>
2021-01-29 11:51:34 +0000

    Correct comment

--- packages/components/src/local/hex-grid/helpers/get-cell-style.ts
+++ packages/components/src/local/hex-grid/helpers/get-cell-style.ts
@@ -3,8 +3,8 @@ import { SergeHex } from '@serge/custom-types'
 /** uility function, to style a style name for a cell, according to if it's in
  * either array
  * @param {cell: SergeHex<{}>} cell the cell in question
- * @param {Array<SergeHex<{}>>} plannedCells the cell in question
- * @param {Array<SergeHex<{}>>} achievableCells the cell in question
+ * @param {Array<SergeHex<{}>>} plannedCells planned route
+ * @param {Array<SergeHex<{}>>} achievableCells cells where the asste could move to
  * @returns string to be used in polygon cell style
  */
 const getCellStyle = (cell: SergeHex<{}>, plannedCells: Array<SergeHex<{}>>, achievableCells: Array<SergeHex<{}>>): string => {
