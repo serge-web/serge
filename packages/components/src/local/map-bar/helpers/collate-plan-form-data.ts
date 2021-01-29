5b9e7628bd179cbc68b620f9c9116d0944dfc505
Ian Mayo <ian@planetmayo.com>
2021-01-29 10:36:13 +0000

    clarify logic

--- packages/components/src/local/map-bar/helpers/collate-plan-form-data.ts
+++ packages/components/src/local/map-bar/helpers/collate-plan-form-data.ts
@@ -11,7 +11,7 @@ const collatePlanFormData = (platforms: PlatformTypeData[], selectedAsset: Selec
 ): PlanTurnFormData => {
   const currentPlatform = platforms && platforms.find((platform: PlatformTypeData) => kebabCase(platform.name) === kebabCase(selectedAsset.type))
   const currentStatus: State | undefined = currentPlatform && currentPlatform.states.find((s: State) => selectedAsset.status && s.name === selectedAsset.status.state)
-  const availableStatus: State | undefined = currentStatus ? currentStatus : currentPlatform && currentPlatform.states[0]
+  const availableStatus: State | undefined = currentStatus || (currentPlatform && currentPlatform.states[0])
   const status: RouteStatus | undefined = selectedAsset.status
   // we're doing extra check that platform type has speeds, in case initialisation
   // data accidentally has speed in current/historic states, but that platform type
