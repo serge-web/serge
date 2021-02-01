741bcf32ba86367125e87201bdb203e31b581cd9
Ian Mayo <ian@planetmayo.com>
2021-02-01 14:48:50 +0000

    Modify find-asset so it walks down through the tree (including tests)

--- packages/helpers/find-asset.ts
+++ packages/helpers/find-asset.ts
@@ -1,12 +1,34 @@
 import { Asset, ForceData } from "@serge/custom-types"
 
+const isAsset = (asset: Asset, assetId: string): Asset | undefined => {
+  if(asset.uniqid === assetId) {
+    return asset
+  } else {
+    let res: Asset | undefined = undefined
+    if(asset.comprising && asset.comprising.length) {
+      res = asset.comprising.find(asset => {
+        return isAsset(asset, assetId)
+      })
+    }
+    if(asset.hosting && asset.hosting.length) {
+      res = asset.hosting.find(asset => {
+        return isAsset(asset, assetId)
+      })
+    }
+    return res
+  }
+}
+
 /** find the asset with the provided id */
 export default (allForces: ForceData[], assetId: string): Asset => {
   let res: Asset | undefined = undefined
   allForces.find((force) => {
     const assets: Asset[] | undefined = force.assets
     if (Array.isArray(assets)) {
-      res = assets.find(asset => asset.uniqid === assetId) || undefined
+      assets.find(asset => {
+        res = isAsset(asset, assetId)
+        return res
+      })
       // if the above find works, we'll return true, which will
       // terminate the find process. If it returns undefined,
       // we'll return false, and carry on to the next force
