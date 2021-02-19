a1c4375872cf05c7a5ee2efa9ad497bf3e3446f7
Lilit Karapetyan <lilit.karapetyan.web@gmail.com>
2021-02-19 16:59:55 +0400

    remove store

--- packages/store/typings.d.ts
+++
@@ -1,8 +0,0 @@
-declare module '*.md' {
-  const content: string
-  export = content;
-}
-
-declare module 'react-leaflet-vectorgrid'
-// TODO: Remove the line below when the store package has been converted
-declare module '@serge/store'
