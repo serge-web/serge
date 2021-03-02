177439dc9ff434c6807b3ee7edbd94655fdc0c0b
Ian Mayo <ian@planetmayo.com>
2021-03-02 19:07:34 +0000

    introduce event handler

--- packages/components/src/local/molecules/turn-progression/index.stories.tsx
+++ packages/components/src/local/molecules/turn-progression/index.stories.tsx
@@ -1,4 +1,4 @@
-import React /*, { useState }*/ from 'react'
+import React, { useState } from 'react'
 
 import { Phase } from '@serge/config'
 import { Story } from '@storybook/react/types-6-0'
@@ -36,16 +36,31 @@ export default {
           Phase.Adjudication
         ]
       }
-    }
+    },
   }
 }
 
 
 const Template: Story<TurnPropTypes> = (args) => {
+  const [state, setState] = useState({
+    phase: Phase.Planning,
+    currentTurn: 1
+  })
+  const updateState = (): void => {
+    const newState = {
+      phase: state.phase === Phase.Planning ? Phase.Adjudication : Phase.Planning,
+      currentTurn: state.phase === Phase.Planning ? state.currentTurn : ++state.currentTurn
+    }
+    console.log('new state', state, newState)
+    setState(newState)
+  }
   // @ts-ignore: Add custom property for storybook
   const { viewAs, store, ...props } = args
   return <TurnProgression 
-    {...props} />
+    {...props}
+    onNextTurn={updateState}
+    currentTurn={state.currentTurn}
+    phase={state.phase} />
 }
 
 export const WithPhases = Template
@@ -53,68 +68,6 @@ WithPhases.args = {
     adjudicationStartTime:"2019-09-30T14:13:22+01:00",
     turnEndTime:"0",
     timeWarning:60000,
-    currentTurn:1,
-    gameDate:"2019-10-01T02:02",
-    phase: Phase.Planning
+    gameDate:"2019-10-01T02:02"
 }
 
-
-// export const Default: React.FC = (args) => (
-//   // @ts-ignore
-//   <TurnProgression
-//     adjudicationStartTime="2019-09-30T14:13:22+01:00"
-//     turnEndTime="0"
-//     timeWarning={60000}
-//     currentTurn={1}
-//     gameDate="2019-10-01T02:02"
-//     isGameControl={boolean('is game control', false)}
-//     {...args}
-//   />
-// )
-
-// export const Planning: React.FC = () => (
-//   <TurnProgression
-//     adjudicationStartTime="2019-09-30T14:13:22+01:00"
-//     turnEndTime={moment(new Date()).add(1.1, 'm').toString()}
-//     timeWarning={60000}
-//     currentTurn={1}
-//     phase={PLANNING_PHASE}
-//     gameDate="2019-10-01T02:02"
-//     isGameControl={false}
-//   />
-// )
-
-// export const WithGameControl: React.FC = () => {
-//   const [state, setState] = useState({
-//     phase: PLANNING_PHASE,
-//     currentTurn: 1
-//   })
-//   const updateState = (): void => {
-//     setState({
-//       phase: ADJUDICATION_PHASE,
-//       currentTurn: 2
-//     })
-//   }
-//   return (
-//     <TurnProgression
-//       adjudicationStartTime="2019-09-30T14:13:22+01:00"
-//       turnEndTime={moment(new Date()).add(1.1, 'm').toString()}
-//       timeWarning={60000}
-//       currentTurn={state.currentTurn}
-//       phase={state.phase}
-//       gameDate="2019-10-01T02:02"
-//       isGameControl={true}
-//       onNextTurn={updateState}
-//     />
-//   )
-// }
-
-// @ts-ignore TS believes the 'story' property doesn't exist but it does.
-// Default.story = {
-//   parameters: {
-//     options: {
-//       // This story requires addons but other stories in this component do not
-//       showPanel: true
-//     }
-//   }
-// }
