539603dc6281ea954d49b8fc7e61fbd5fc0e33ff
Lilit Karapetyan <lilit.karapetyan.web@gmail.com>
2021-03-04 19:44:59 +0400

    update wargame type

--- packages/custom-types/wargame.d.ts
+++ packages/custom-types/wargame.d.ts
@@ -1,8 +1,14 @@
 import WargameList from './wargame-list'
 import WargameData from './wargame-data'
 import Message from './message'
+import { ADJUDICATION_PHASE, PLANNING_PHASE } from '@serge/config'
 
 export default interface Wargame {
+  _id: string,
+  _rev?: string,
+  phase: typeof ADJUDICATION_PHASE | typeof PLANNING_PHASE ,
+  turnEndTime?: string,
+  adjudicationStartTime?: string,
   isLoading: boolean,
   wargameList: Array<WargameList>,
   currentWargame: string,
