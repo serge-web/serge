93ab8b2f832bdfc828b730103fbe2a2c799c6c8e
Ian Mayo <ian@planetmayo.com>
2021-03-17 16:24:36 +0000

    Refactoring

--- packages/helpers/most-recent-only.ts
+++ packages/helpers/most-recent-only.ts
@@ -10,14 +10,14 @@ import { MessageChannel, MessageCustom } from '@serge/custom-types'
  * @returns (MessageCustom[]) latest version of specified messages
  */
 const mostRecentOnly = (messages: MessageChannel[], channel?: string, msgType?: string): MessageCustom[] => {
+  // create dictionary of messages, indexed by _id field
   const matches = new Map<string, MessageCustom>();
   messages.forEach((message: MessageChannel) => {
-    const isCustom = message.messageType === CUSTOM_MESSAGE
-    const correctChannel = channel === undefined || message.details.channel === channel
-    if(isCustom && correctChannel ) {
+    if(message.messageType === CUSTOM_MESSAGE && (channel === undefined || message.details.channel === channel)) {
       const customMessage: MessageCustom = message as MessageCustom
       if(msgType === undefined || msgType === customMessage.details.messageType) {
         if(message._id) {
+          // either store this message, or overwrite previous version
           matches.set(message._id, message as MessageCustom)
         } else {
           console.warn('Encountered message without an _id')
