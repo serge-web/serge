d2f80ee6ee730ed061b82a041e17f389ad3ea9c7
sanjay radadiya <sanjay.radadiya91@gmail.com>
2021-03-26 14:45:35 +0530

    lint fixes

--- packages/components/src/local/form-elements/private-chat-input-toggle/types/props.d.ts
+++ packages/components/src/local/form-elements/private-chat-input-toggle/types/props.d.ts
@@ -1,3 +1,10 @@
-import ChatInputProps from '../../chat-input-text/types/props';
-
-export default interface Props extends ChatInputProps { }
+export default interface Props {
+  /**
+   * Placeholder text
+   */
+  placeholder?: string
+  /**
+   * The method for posting messages out of the component
+   */
+  postBack?: { (value: string): void }
+}
