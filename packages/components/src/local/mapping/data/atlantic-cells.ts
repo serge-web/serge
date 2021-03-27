dd4b4d80123fe89b7393793d63c4f48fa822ee30
Ian Mayo <ian@planetmayo.com>
2021-03-27 17:57:14 +0000

    also skip escape character

--- packages/components/src/local/mapping/data/atlantic-cells.ts
+++ packages/components/src/local/mapping/data/atlantic-cells.ts
@@ -1,4 +1,5 @@
-/* eslint-disable @typescript-eslint/naming-convention */
+/* eslint-disable @typescript-eslint/camelcase */
+/* eslint-disable no-useless-escape */
 export default {
   type: 'FeatureCollection',
   features: [
