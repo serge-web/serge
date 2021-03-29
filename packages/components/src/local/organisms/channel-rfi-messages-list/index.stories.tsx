06f5096a7f01a2693e8707f5c0bf2e41089d117f
Ian Mayo <ian@planetmayo.com>
2021-03-29 17:29:53 +0100

    Switch to new stories

--- packages/components/src/local/organisms/channel-rfi-messages-list/index.stories.tsx
+++ packages/components/src/local/organisms/channel-rfi-messages-list/index.stories.tsx
@@ -3,22 +3,45 @@ import React, { useState } from 'react'
 // Import component files
 import ChannelRFIMessagesList from './index'
 import docs from './README.md'
+import { Story } from '@storybook/react/types-6-0'
 import { GameMessagesMockRFI } from '@serge/mocks'
 import { MessageCustom } from '@serge/custom-types'
 import { mostRecentOnly } from '@serge/helpers'
+import RFIListPropTypes from './types/props'
+
+const wrapper: React.FC = (storyFn: any) => <div style={{ height: '600px' }}>{storyFn()}</div>
 
 export default {
   title: 'local/organisms/ChannelRFIMessagesList',
   component: ChannelRFIMessagesList,
-  decorators: [],
+  decorators: [wrapper],
   parameters: {
     options: {
-      // No addons are used in this story so don't show the addon panel
-      showPanel: false
+      showPanel: true
     },
     readme: {
       // Show readme before story
       content: docs
+    }
+  },
+  argTypes: {
+    isUmpire: {
+      description: 'Player is from umpire force',
+      control: 'boolean'
+    },
+    isRFIManager: {
+      description: 'Player role has RFI Manager attribute',
+      control: 'boolean'
+    },
+    role: {
+      description: 'Player Role',
+      control: {
+        type: 'radio',
+        options: [
+          'Comms',
+          'Logistics'
+        ]
+      }
     }
   }
 }
@@ -28,7 +51,9 @@ export default {
  */
 const newestMessages = mostRecentOnly(GameMessagesMockRFI)
 
-export const Default: React.FC = () => {
+
+const Template: Story<RFIListPropTypes> = (args) => {
+  const { isUmpire, role, isRFIManager } = args
   const icons = ['./images/default_img/forceDefault.png']
   const colors = ['#0F0']
   const [messages, setMessages] = useState<MessageCustom[]>(newestMessages)
@@ -49,15 +74,24 @@ export const Default: React.FC = () => {
     }))
   }
 
-  return <ChannelRFIMessagesList
+  return (
+    <ChannelRFIMessagesList
     messages={messages}
     icons={icons}
     colors={colors}
     onMarkAllAsRead={markAllAsRead}
     onRead={onRead}
     onChange={handleChange}
-    role='CO'
-    isRFIManager={true}
-    isUmpire={true}
+    role={role}
+    isRFIManager={isRFIManager}
+    isUmpire={isUmpire}
   />
+  )
+}
+
+export const Default = Template.bind({})
+Default.args = {
+  isUmpire: true,
+  isRFIManager: true,
+  role: 'CO'
 }
