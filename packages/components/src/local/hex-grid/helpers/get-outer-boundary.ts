3ba3436d0b342249597ab9f766e1ba06932a4c98
Ian Mayo <ian@planetmayo.com>
2021-03-31 16:52:01 +0100

    Generate outer marker as a guide for limit of travel

---
+++ packages/components/src/local/hex-grid/helpers/get-outer-boundary.ts
@@ -0,0 +1,28 @@
+import { SergeHex } from '@serge/custom-types'
+import * as turf from '@turf/turf'
+import L from 'leaflet'
+
+ const generateOuterBoundary = (cells: SergeHex<{}>[]): L.LatLng[] => {
+  const points: turf.Feature<turf.Point>[] = []
+  cells.forEach(hex => {
+    if(hex.poly) {
+      hex.poly.forEach(pt => {
+        points.push(turf.point([pt.lng, pt.lat]))
+      })
+    }
+  })
+  const turfPoints = turf.featureCollection(points)          
+  var hull = turf.concave(turfPoints)//, {units: 'kilometers', maxEdge: 0.002})
+  if(hull) {
+    const latLngs: L.LatLng[] = []
+    hull.geometry.coordinates[0].forEach((pt:any) => {
+      const pos = L.latLng(pt[1], pt[0])
+      latLngs.push(pos)
+    })
+    return latLngs
+  } else {
+    return []
+  }
+}
+
+export default generateOuterBoundary
