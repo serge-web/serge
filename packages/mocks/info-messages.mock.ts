1634bf6c2910e8a674c5da2b2c4d3a458c0f3d87
Ian Mayo <ian@planetmayo.com>
2021-04-01 21:49:55 +0100

    slight improvements to mock data

--- packages/mocks/info-messages.mock.ts
+++ packages/mocks/info-messages.mock.ts
@@ -2,7 +2,7 @@
 import { INFO_MESSAGE_CLIPPED } from '@serge/config'
 import { MessageInfoTypeClipped } from '@serge/custom-types'
 
-const message1: MessageInfoTypeClipped = {
+const messages: MessageInfoTypeClipped[] = [{
   gameTurn: 0,
   messageType: INFO_MESSAGE_CLIPPED,
   infoType: true,
@@ -10,17 +10,22 @@ const message1: MessageInfoTypeClipped = {
   _id: "2021-02-24T17:50:31.603Z",
   isOpen: false,
   hasBeenRead: false
-}
-const message2: MessageInfoTypeClipped = {
+},{
   gameTurn: 1,
   messageType: INFO_MESSAGE_CLIPPED,
   infoType: true,
   details: { channel: 'Net 16'},
-  _id: "2021-02-24T17:52:31.603Z",
+  _id: "2021-02-24T17:52:31.643Z",
   isOpen: false,
   hasBeenRead: false
-}
-
-const messages: MessageInfoTypeClipped[] = [message1, message2]
+},{
+  gameTurn: 1,
+  messageType: INFO_MESSAGE_CLIPPED,
+  infoType: true,
+  details: { channel: 'channel-BlueRFI'},
+  _id: "2021-02-24T17:52:31.613Z",
+  isOpen: false,
+  hasBeenRead: false
+}]
 
 export default messages
