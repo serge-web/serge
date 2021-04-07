8982875cf9f57f1c25054da44e40d7730898f921
Ian Mayo <ian@planetmayo.com>
2021-04-07 16:41:32 +0100

    make deep ocean darker shade

--- packages/components/src/local/mapping/helpers/create-grid-from-geojson.ts
+++ packages/components/src/local/mapping/helpers/create-grid-from-geojson.ts
@@ -10,7 +10,7 @@ const typeFor = (type: number): {terrain: Terrain, type: string, fillColor: stri
     case 0: return { terrain: Terrain.LAND, type: 'land', fillColor: '#0f0' }
     case 1: return { terrain: Terrain.SEA, type: 'shallow', fillColor: '#fff' }
     case 2: return { terrain: Terrain.SEA, type: 'sea', fillColor: '#0ff' }
-    case 3: return { terrain: Terrain.SEA, type: 'deep', fillColor: '#a0a' }
+    case 3: return { terrain: Terrain.SEA, type: 'deep', fillColor: '#0078c7' }
     case 4: return { terrain: Terrain.SEA, type: 'medium', fillColor: '#0a0' }
     default: return { terrain: Terrain.SEA, type: 'sea', fillColor: '#fff' }
   }
