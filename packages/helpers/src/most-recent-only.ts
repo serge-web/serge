09ddecfedb0a18907a573a7bc30df107d8e4b3e0
Ian Mayo <ian@planetmayo.com>
2021-04-08 16:55:08 +0100

    Change into helper function that is used by lodash

--- packages/helpers/src/most-recent-only.ts
+++ packages/helpers/src/most-recent-only.ts
@@ -1,60 +1,21 @@
-import { CUSTOM_MESSAGE } from '@serge/config';
+import { INFO_MESSAGE_CLIPPED } from '@serge/config';
 import { MessageChannel } from '@serge/custom-types'
 
 
-/** helper function to handle an array of messages, and
- * only return the newest version of custom messages. Note: it does not inspect
- * the pouchdb _rev field, it just returns the last version
- * of that message encountered in the list
- * @param (MessageChannel[]) array of messages
- * @returns (MessageCustom[]) latest version of specified messages
+/** helper function to produce unique ids for channel messages
  */
-const mostRecentOnly = (messages: MessageChannel[]): MessageChannel[] => {
-  // create list of docs with refs
-  const refs: string[] = []
-  const withFirstVersion:MessageChannel[] = []
-  messages.forEach((message: MessageChannel) => {
-    if(message.messageType === CUSTOM_MESSAGE) {
-      const custom = message
-      const ref = custom.message.Reference
-      // see if it's a versioned document
-      if(ref !== undefined) {
-        if(refs.find(item => item === ref)){
-          // don't store it - we've got this reference already
-        } else {
-          refs.push(ref)
-          withFirstVersion.push(message)
-        }
-      } else {
-        withFirstVersion.push(message)
-      }  
+const mostRecentOnly = (message: MessageChannel): string => {
+  let res
+  if (message.messageType === INFO_MESSAGE_CLIPPED || message.infoType === true) {
+    res = '' + message.gameTurn
+  } else {
+    const msg = message.message
+    if(msg.Reference !== undefined) {
+      res = msg.Reference
     } else {
-      // ok, it's an info type. We want the last occurring info type
-      withFirstVersion.push(message)
+      res = message._id
     }
-  })
-
-  // now pass through, to remove duplicate turns
-  const reversed = withFirstVersion.reverse()
-  let maxTurn: number = -1
-  const onlyFirstTurn:MessageChannel[] = []
-  reversed.forEach((message: MessageChannel) => {
-    if(message.messageType !== CUSTOM_MESSAGE) {
-      const turn = message.gameTurn
-      if(turn <= maxTurn) {
-        // don't store it - we've got this reference already
-      } else {
-        maxTurn = turn
-        onlyFirstTurn.push(message)
-      }  
-    } else {
-      // ok, it's custom message - we've already sorted them
-      onlyFirstTurn.push(message)
-    }
-  })
-
-  const res = onlyFirstTurn.reverse()
-
+  }
   return res
 }
 export default mostRecentOnly
