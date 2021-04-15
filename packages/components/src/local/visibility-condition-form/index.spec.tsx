6b0c1859fefe8010fdab94be2538960564df812f
Ian Mayo <ian@planetmayo.com>
2021-04-15 20:26:35 +0100

    reinstate tests

--- packages/components/src/local/visibility-condition-form/index.spec.tsx
+++ packages/components/src/local/visibility-condition-form/index.spec.tsx
@@ -2,17 +2,22 @@
 
 // /* global it expect */
 
-// import React from 'react'
-// import renderer from 'react-test-renderer'
+import React from 'react'
+import renderer from 'react-test-renderer'
 
-// import VisibilityForm from './index'
-// import formData from './mocks/formData'
+import VisibilityForm from './index'
+
+/* Import mock data */
+import { forces, selectedAsset, platformTypes } from '@serge/mocks'
+
+// import data types
+import collateVisibilityFormData from '../map-bar/helpers/collate-visibility-form-data'
+const formData = collateVisibilityFormData(platformTypes, selectedAsset, forces)
+const iconData = { platformType: 'merchant-vessel', forceColor: 'blue' }
 
 it('VisibilityForm renders correctly', () => {
-  // TODO: Ian doesn't know how to update snapshots, so he had
-  // to comment out this test (and all the imports)
-  // const tree = renderer
-  //   .create(<VisibilityForm formHeader="Visibility header" formData={formData} />)
-  //   .toJSON()
-  // expect(tree).toMatchSnapshot()
+  const tree = renderer
+    .create(<VisibilityForm formHeader="Visibility header" icon={iconData} formData={formData} />)
+    .toJSON()
+  expect(tree).toMatchSnapshot()
 })
