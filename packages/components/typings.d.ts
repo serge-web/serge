4c72c91acfc616fa23c7f9721fcbeeb6a255ba9e
Sandro <sandromirijanashvili@gmail.com>
2021-04-27 22:53:12 +0400

    Moves common devDependencies in root package.json file. Installs, removes and updates some packages to fix peer dependency warnings.

--- packages/components/typings.d.ts
+++ packages/components/typings.d.ts
@@ -3,7 +3,6 @@ declare module '*.md' {
   export = content;
 }
 
-declare module 'react-leaflet-vectorgrid'
 // TODO: Remove the line below when the store package has been converted
 declare module '@serge/store'
 declare module 'whatwg-fetch'
