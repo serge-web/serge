24565258dacd51ed3cd689305e5c0728025ccbc9
philb89 <everlasting2605@gmail.com>
2021-05-05 22:45:22 +0700

    zip output file after building the executable file

--- packages/executable/helpers/pkhCopyCrossPlatforms.js
+++ packages/executable/helpers/pkhCopyCrossPlatforms.js
@@ -2,6 +2,7 @@ const find = require('find')
 const fs = require('fs')
 const path = require('path')
 const rimraf = require('rimraf')
+const archiver = require('archiver')
 
 const buildTmpDir = path.resolve(process.cwd(), 'build-temp')
 const sqlite3NodeDir = path.resolve(process.cwd(), 'sqlite3.nodes')
@@ -61,30 +62,83 @@ if (!fs.existsSync(buildDir)) {
  * @param {*} finalDir
  * @param {*} cb
  */
-function findAndCopy (file, path, prefix, finalDir, cb) {
-  find.file(file, path, (files) => {
-    if (files.length) {
-      console.log(`${prefix} file "${file}" was founded`)
-      console.log(`${prefix} copying to ${finalDir}...`)
-      fs.copyFile(files[0], `${finalDir}/${file}`, err => {
-        if (err) {
-          throw err
-        } else {
-          console.log(`${prefix} ${file} successfully copied`)
-        }
-        if (cb) cb()
-      })
-    } else {
-      console.log(`${prefix} file "${file}" not found`)
-    }
+async function findAndCopy (file, path, prefix, finalDir) {
+  return new Promise((resolve, reject) => {
+    find.file(file, path, (files) => {
+      if (files.length) {
+        console.log(`${prefix} file "${file}" was founded`)
+        console.log(`${prefix} copying to ${finalDir}...`)
+        fs.copyFile(files[0], `${finalDir}/${file}`, err => {
+          if (err) {
+            reject(err)
+          } else {
+            console.log(`${prefix} ${file} successfully copied`)
+            resolve()
+          }
+        })
+      } else {
+        console.log(`${prefix} file "${file}" not found`)
+        reject(Error('File not found'))
+      }
+    })
   })
 }
 
-nodeFiles.forEach((nodeFile, idx) => {
+const promises = nodeFiles.map(async (nodeFile, idx) => {
   const { name, pathExec, pathSqlNode, fileExec, fileSqlNode } = nodeFile
   const prefix = `[${idx + 1}/${nodeFiles.length}] ${name}:`
-  findAndCopy(fileExec, pathExec, prefix, finalDir[idx])
-  findAndCopy(fileSqlNode, pathSqlNode, prefix, finalDir[idx], () => {
-    rimraf.sync(buildTmpDir)
-  })
+  await findAndCopy(fileExec, pathExec, prefix, finalDir[idx])
+  await findAndCopy(fileSqlNode, pathSqlNode, prefix, finalDir[idx])
 })
+
+Promise.all(promises).then(() => {
+  rimraf.sync(buildTmpDir)
+  finalDir.forEach(async dir => {
+    const zipPath = path.join(__dirname, '..', 'builds')
+    let outputPath = ''
+    if (dir.indexOf('linux') !== -1) {
+      outputPath = `${zipPath}/SERGE_${getDate()}_linux.zip`
+    } else if (dir.indexOf('macos') !== -1) {
+      outputPath = `${zipPath}/SERGE_${getDate()}_macos.zip`
+    } else {
+      outputPath = `${zipPath}/SERGE_${getDate()}_win64.zip`
+    }
+    await zipDirectory(dir, outputPath)
+    rimraf.sync(dir)
+  })
+}, err => {
+  console.log(err)
+})
+
+/**
+ * get date format yyyyddmm
+ * @returns string
+ */
+function getDate () {
+  const d = new Date()
+  const yyyy = d.getFullYear()
+  const month = d.getMonth() + 1
+  const mm = month < 10 ? `0${month}` : month
+  const date = d.getDate()
+  const dd = date < 10 ? `0${date}` : date
+  return `${yyyy}${dd}${mm}`
+}
+/**
+ * @param {String} source
+ * @param {String} out
+ * @returns {Promise}
+ */
+function zipDirectory (source, out) {
+  const archive = archiver('zip', { zlib: { level: 9 } })
+  const stream = fs.createWriteStream(out)
+
+  return new Promise((resolve, reject) => {
+    archive
+      .directory(source, false)
+      .on('error', err => reject(err))
+      .pipe(stream)
+
+    stream.on('close', () => resolve())
+    archive.finalize()
+  })
+}
