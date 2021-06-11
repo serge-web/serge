0ad72eba5dc8462b69e43c8dd9055962360f3df3
Ian Mayo <ian@planetmayo.com>
2021-06-11 15:51:29 +0100

    log error message if zero turn time

--- packages/components/src/local/mapping/index.tsx
+++ packages/components/src/local/mapping/index.tsx
@@ -458,6 +458,10 @@ export const Mapping: React.FC<PropTypes> = ({
         // special handling, a mobile status may not have a speedVal,
         // which represents unlimited travel
         if (plannedTurn.speedVal) {
+          // special case. check turn time is non-zero
+          if (gameTurnTime === 0) {
+            console.error('Cannot plan route with zero game turn time')
+          }
           const speedKts = plannedTurn.speedVal
           const stepSizeHrs = gameTurnTime / 1000 / 60 / 60
           const distancePerTurn = stepSizeHrs * speedKts
