8a034ecde04fd2f6625c22f1dca7ea0661edaa8d
Ian Mayo <ian@planetmayo.com>
2021-06-16 17:32:29 +0100

    Improve walkthrough

--- packages/components/src/local/molecules/admin-panel-footer/index.tsx
+++ packages/components/src/local/molecules/admin-panel-footer/index.tsx
@@ -8,13 +8,13 @@ import styles from './styles.module.scss'
 
 /* Render component */
 export const AdminPanelFooter: React.FC<Props> = ({ force, selectedRole, byPassUrl, onIconClick }: Props) =>
-  <div className={styles['role-info']} style={{ backgroundColor: force.color }}>
+  <div className={styles['role-info']} data-tour='role-info-step' style={{ backgroundColor: force.color }}>
     {
       byPassUrl
         ? <Badge variant="light"><a href={byPassUrl} className={styles['role-type']}>{ selectedRole }</a></Badge>
         : <span className={styles['role-type']}>{ selectedRole }</span>
     }
-    <div className={styles['contain-force-skin']}>
+    <div className={styles['contain-force-skin']} data-tour='objectives-step'>
       <div className={styles['force-skin']}>
         <span className={styles['force-type']}>{ force.name }</span>
         <img className={styles['role-icon']} src={force.icon} alt="" onClick={onIconClick} />
