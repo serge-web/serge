266e3380f2f6a1225d42fe45da7f49ee316dc5d9
Ian Mayo <ian@planetmayo.com>
2021-08-11 17:13:53 +0100

    extend test

--- packages/helpers/src/tests/increment-turn-time.spec.ts
+++ packages/helpers/src/tests/increment-turn-time.spec.ts
@@ -1,9 +1,14 @@
+import moment, { Moment } from 'moment'
 import { MilliTurns, MonthTurns, YearTurns } from '../../../custom-types'
 import incrementGameTime from '../increment-game-time'
 
+const startTime: Moment = moment('2013-02-08 09:30:26')
+const startStr: string = startTime.format('YYYY-MM-DDTHH:mm:ss')
+
 describe('increment turn time:', () => {
+  console.log(moment())
   it('increments using legacy millis', () => {
-    expect(incrementGameTime('1000', 1000)).toBe(2000)
+    expect(incrementGameTime(startStr, 1000)).toBe(2000)
   })
 
   it('increments using millis', () => {
