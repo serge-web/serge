89f7c22f06e9c962dd82bad39a03ddc21f5aa9ea
Ian Mayo <ian@planetmayo.com>
2021-08-20 11:26:31 +0100

    refactoring helpers

--- packages/components/src/local/molecules/channel-coa-message-detail/helpers/visibility.ts
+++ packages/components/src/local/molecules/channel-coa-message-detail/helpers/visibility.ts
@@ -1,14 +1,42 @@
 import { MessageCustom, ChannelData, ForceRole } from '@serge/custom-types'
 import { CollaborativeMessageStates, SpecialChannelTypes } from '@serge/config'
 
-export const formEditable = (message: MessageCustom, _role: ForceRole, _isUmpire = false): boolean => {
+/**
+ * @param message the message we're looking at
+ * @param state the state we're checking for
+ * @returns is this message in the required state?
+ */
+const isThisState = (message: MessageCustom, state: CollaborativeMessageStates): boolean => {
   const { collaboration } = message.details
-  return typeof collaboration !== 'undefined' && collaboration.status === CollaborativeMessageStates.InProgress && _isUmpire && collaboration.owner?.roleId === _role.roleId && _isUmpire
+  return typeof collaboration !== 'undefined' && collaboration.status === state
+}
+
+/**
+ * @param message the message we're looking at
+ * @param role the role we're checking for
+ * @returns is this message owned by the indicated role
+ */
+const isThisRole = (message: MessageCustom, role: ForceRole): boolean => {
+  const { collaboration } = message.details
+  return typeof collaboration !== 'undefined' && collaboration.owner?.roleId === role.roleId
+}
+
+/**
+ * @param channel the channel we're in
+ * @param role the role we're checking for
+ * @returns is this channel the type we're looking for
+ */
+const isThisChannelType = (channel: ChannelData, channelType: SpecialChannelTypes): boolean => {
+  return channel.format === channelType
+}
+
+export const formEditable = (message: MessageCustom, _role: ForceRole): boolean => {
+  return isThisState(message, CollaborativeMessageStates.InProgress) && isThisRole(message, _role)
 }
 
 export const ColEditRelManReview = (message: MessageCustom, channel: ChannelData, canReleaseMessages: boolean | undefined): boolean => {
   const { collaboration } = message.details
-  return channel.format === SpecialChannelTypes.CHANNEL_COLLAB_EDIT && typeof collaboration !== 'undefined' && collaboration.status === CollaborativeMessageStates.PendingReview && !!canReleaseMessages
+  return isThisChannelType(channel, SpecialChannelTypes.CHANNEL_COLLAB_EDIT) && typeof collaboration !== 'undefined' && collaboration.status === CollaborativeMessageStates.PendingReview && !!canReleaseMessages
 }
 
 export const ColEditCollPartAssClaim = (message: MessageCustom, channel: ChannelData, canCollaborate: boolean | undefined): boolean => {
