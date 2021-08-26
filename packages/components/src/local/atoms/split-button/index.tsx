a12effd22764e251ea9b57d0b60699c190227899
Ian Mayo <ian@planetmayo.com>
2021-08-26 15:43:54 +0100

    Improved handling of initial state

--- packages/components/src/local/atoms/split-button/index.tsx
+++ packages/components/src/local/atoms/split-button/index.tsx
@@ -32,9 +32,12 @@ export const SplitButton: React.FC<Props> = ({ label, options = [], onClick }: P
   const gridStyles = useButtonStyle()
   const [open, setOpen] = useState<boolean>(false)
   const anchorRef = useRef<any>(null)
-  const [selectedIndex, setSelectedIndex] = useState<number>(0)
+  const [selectedIndex, setSelectedIndex] = useState<number>(-1)
 
   const handleClick = (): void => {
+    if (selectedIndex === -1) {
+      return
+    }
     onClick && onClick(options[selectedIndex])
   }
 
@@ -65,7 +68,7 @@ export const SplitButton: React.FC<Props> = ({ label, options = [], onClick }: P
           ref={anchorRef}
           aria-label="split button"
         >
-          <Button onClick={handleClick}>{label} {options[selectedIndex]}</Button>
+          <Button onClick={handleClick}>{label} { selectedIndex >= 0 ? options[selectedIndex] : '...'}</Button>
           <Button
             color="primary"
             size="small"
