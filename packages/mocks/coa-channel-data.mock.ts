d8a31de4f872759bf8de657b2c08e7bd0f93355e
Ian Mayo <ian@planetmayo.com>
2021-08-27 12:41:38 +0100

    Steps to improve stories

--- packages/mocks/coa-channel-data.mock.ts
+++ packages/mocks/coa-channel-data.mock.ts
@@ -1,8 +1,10 @@
-import { ChannelData } from '@serge/custom-types'
+import { ChannelData, ForceRole } from '@serge/custom-types'
 import { SpecialChannelTypes } from '@serge/config'
 
+export const blueCollaborator: ForceRole = { forceId: 'Blue', forceName: 'Blue', roleId: 'rkrlw6f5emms', roleName: 'Comms' }
+export const blueReleaseManager: ForceRole = { forceId: 'Blue', forceName: 'Blue', roleId: 'rkrlw445e', roleName: 'CO' }
 
-export const channelDataCollaborativeEditing: ChannelData = {
+export const channelCollaborativeEditing: ChannelData = {
   collabOptions: {
     extraColumns: [],
     mode: 'edit',
@@ -10,144 +12,94 @@ export const channelDataCollaborativeEditing: ChannelData = {
     startWithReview: true
   },
   format: SpecialChannelTypes.CHANNEL_COLLAB_EDIT,
-  name: 'New CE',
+  name: 'Collab Editing',
   participants: [
     {
-      canCollaborate: false,
+      canCollaborate: true,
       canReleaseMessages: false,
-      force: 'White',
-      forceUniqid: 'umpire',
+      force: 'Blue',
+      forceUniqid: 'Blue',
       roles: [],
       subscriptionId: 'oqoj',
       templates: [
-        { title: 'RFI', _id: 'k16eedkj' }
+        { title: 'COA', _id: 'k16eedkj' }
+      ]
+    },
+    {
+      canCollaborate: true,
+      canReleaseMessages: true,
+      force: 'Blue',
+      forceUniqid: 'Blue',
+      roles: [blueReleaseManager.roleId],
+      subscriptionId: 'oqoj2',
+      templates: [
+        { title: 'COA', _id: 'k16eedkj' }
       ]
     }
   ],
   uniqid: 'ks8soryj'
 }
 
-export const channelDataCollaborativeEditingCollaborationParticipant: ChannelData = {
+export const blueAuthor: ForceRole = { forceId: 'Blue', forceName: 'Blue', roleId: 'rkrlw6f5emms', roleName: 'Comms' }
+export const whiteCollaborator: ForceRole = { forceId: 'umpire', forceName: 'White', roleId: 'rkrlasdd5f', roleName: 'Manager' }
+export const whiteReleaseMgr: ForceRole = { forceId: 'umpire', forceName: 'White', roleId: 'rkrlw6f5f', roleName: 'Game Control' }
+export const exconCollaborator: ForceRole = { forceId: 'Red', forceName: 'Red', roleId: 'rkrlss55f5e', roleName: 'Logs' }
+
+export const channelCollaborativeResponding: ChannelData = {
   collabOptions: {
     extraColumns: [],
-    mode: 'edit',
-    returnVerbs: ['Endorse', 'Request Changes'],
-    startWithReview: true
+    mode: 'response',
+    returnVerbs: [],
+    startWithReview: false
   },
-  format: SpecialChannelTypes.CHANNEL_COLLAB_EDIT,
-  name: 'New CE',
+  format: SpecialChannelTypes.CHANNEL_COLLAB_RESPONSE,
+  name: 'New CR',
   participants: [
+    {
+      canCollaborate: false,
+      canReleaseMessages: false,
+      force: 'Blue',
+      forceUniqid: 'Blue',
+      roles: [],
+      subscriptionId: 'jvrn1',
+      templates: [
+        { title: 'RFI', _id: 'k16eedkj' }
+      ]
+    },
+    {
+      canCollaborate: true,
+      canReleaseMessages: false,
+      force: 'EXCON',
+      forceUniqid: 'excon',
+      roles: [],
+      subscriptionId: 'jvrn2',
+      templates: [
+        { title: 'RFI', _id: 'k16eedkj' }
+      ]
+    },
     {
       canCollaborate: true,
       canReleaseMessages: false,
       force: 'White',
       forceUniqid: 'umpire',
       roles: [],
-      subscriptionId: 'oqoj',
+      subscriptionId: 'jvrn3',
       templates: [
         { title: 'RFI', _id: 'k16eedkj' }
       ]
-    }
-  ],
-  uniqid: 'ks8soryj'
-}
-
-export const channelDataColabEditCollaborativeReleaseManager: ChannelData = {
-  collabOptions: {
-    extraColumns: [],
-    mode: 'edit',
-    returnVerbs: ['Endorse', 'Request Changes'],
-    startWithReview: true
-  },
-  format: SpecialChannelTypes.CHANNEL_COLLAB_EDIT,
-  name: 'New CE',
-  participants: [
-    {
-      canCollaborate: false,
-      canReleaseMessages: true,
-      force: 'White',
-      forceUniqid: 'umpire',
-      roles: [],
-      subscriptionId: 'oqoj',
-      templates: [
-        { title: 'RFI', _id: 'k16eedkj' }
-      ]
-    }
-  ],
-  uniqid: 'ks8soryj'
-}
-
-export const channelDataCollaborativeResponding: ChannelData = {
-  collabOptions: {
-    extraColumns: [],
-    mode: 'response',
-    returnVerbs: [],
-    startWithReview: false
-  },
-  format: SpecialChannelTypes.CHANNEL_COLLAB_RESPONSE,
-  name: 'New CR',
-  participants: [
-    {
-      canCollaborate: false,
-      canReleaseMessages: false,
-      force: 'White',
-      forceUniqid: 'umpire',
-      roles: [],
-      subscriptionId: 'jvrn',
-      templates: [
-        { title: 'RFI', _id: 'k16eedkj' }
-      ]
-    }
-  ],
-  uniqid: 'ks8sztkw'
-}
-
-export const channelDataCollaborativeRespondingCollaborationParticipant: ChannelData = {
-  collabOptions: {
-    extraColumns: [],
-    mode: 'response',
-    returnVerbs: [],
-    startWithReview: false
-  },
-  format: SpecialChannelTypes.CHANNEL_COLLAB_RESPONSE,
-  name: 'New CR',
-  participants: [
+    },
     {
       canCollaborate: true,
-      canReleaseMessages: false,
-      force: 'White',
-      forceUniqid: 'umpire',
-      roles: [],
-      subscriptionId: 'jvrn',
-      templates: [
-        { title: 'RFI', _id: 'k16eedkj' }
-      ]
-    }
-  ],
-  uniqid: 'ks8sztkw'
-}
-
-export const channelDataCollaborativeRespondingReleaseManager: ChannelData = {
-  collabOptions: {
-    extraColumns: [],
-    mode: 'response',
-    returnVerbs: [],
-    startWithReview: false
-  },
-  format: SpecialChannelTypes.CHANNEL_COLLAB_RESPONSE,
-  name: 'New CR',
-  participants: [
-    {
-      canCollaborate: false,
       canReleaseMessages: true,
       force: 'White',
       forceUniqid: 'umpire',
-      roles: [],
-      subscriptionId: 'jvrn',
+      roles: [whiteReleaseMgr.roleId],
+      subscriptionId: 'jvrn4',
       templates: [
         { title: 'RFI', _id: 'k16eedkj' }
       ]
     }
+
   ],
   uniqid: 'ks8sztkw'
 }
