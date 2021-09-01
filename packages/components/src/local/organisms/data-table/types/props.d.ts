929e1f15c1ca593774b39f877adcc8dde0d57c5c
Lilit Karapetyan <lilit.karapetyan.web@gmail.com>
2021-09-01 11:43:21 +0400

    Snapshots

--- packages/components/src/local/organisms/data-table/types/props.d.ts
+++ packages/components/src/local/organisms/data-table/types/props.d.ts
@@ -4,7 +4,8 @@ export type RowDataType = string | { component: React.ReactElement | null, label
 export type RowWithCollapsibleType = {
   // collapsible is a function witch one will return a ReactElement and will be rendered only on call
   // cb: is a callback function with one should fire some logick on collapsible content change, ex close collapsible component
-  collapsible: (cb?: () => void) => React.ReactElement, cells: Array<RowDataType> 
+  collapsible: (cb?: () => void) => React.ReactElement
+  cells: Array<RowDataType>
 }
 export type RowType = Array<RowDataType | RowWithCollapsibleType>
 export default interface Props {
