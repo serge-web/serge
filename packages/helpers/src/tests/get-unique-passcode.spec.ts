d5ff595a18fc5190dad133df46ddfe15db3ae85a
sanjay radadiya <sanjay.radadiya91@gmail.com>
2021-09-01 14:58:31 +0530

    test case for getUniquePasscode to check valid return value

--- packages/helpers/src/tests/get-unique-passcode.spec.ts
+++ packages/helpers/src/tests/get-unique-passcode.spec.ts
@@ -8,3 +8,8 @@ it('Check force role unique passcode', () => {
   const res = getUniquePasscode(allForces, 'P')
   expect(res).not.toEqual('rkrlw6f5m')
 })
+
+it('Check force role unique passcode return value', () => {
+    const res = getUniquePasscode(allForces, 'R')
+    expect(res).toBeTruthy()
+  })
