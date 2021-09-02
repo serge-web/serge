fef96e06db032682f3a7296e8e8ebf8d4534c73e
Ian Mayo <ian@planetmayo.com>
2021-09-02 12:21:26 +0100

    outputting locations

--- packages/components/src/local/coa-status-board/helpers/get-columns.ts
+++ packages/components/src/local/coa-status-board/helpers/get-columns.ts
@@ -2,8 +2,18 @@ import { MessageCustom } from "@serge/custom-types"
 import { SpecialChannelColumns } from "../../../../../config/build/enums"
 
 const compressData = (message: any): string => {
-  console.log('msg', message)
-  return message
+  if(message) {
+    const listed = Object.keys(message).map((prop) => {
+      const items = message[prop]      
+      return items.map((item: any) => {
+        return (item.Country) ? prop + '-' + item.Country : 'Country missing'
+      })
+    })
+    // introduce space separator, to let them flow to newline
+    return '' + listed.join(', ')  
+  } else {
+    return '[Loc not found]'
+  }
 }
 
 /** get a list of the roles in this participant group, if it is
@@ -11,10 +21,16 @@ const compressData = (message: any): string => {
  * If no roles are specified, include all roles
  */
 const getColumns = (message: MessageCustom, columns: SpecialChannelColumns[]): string[] => {
+  console.log('msg', message)
+  const msg = message.message
   const res = columns.map((column: SpecialChannelColumns): string => {
     switch(column) {
       case SpecialChannelColumns.LOCATION: {
-        return compressData(message.message.LOCATION)
+        if(msg.LOCATION && msg.LOCATION.LOCATION) {
+          return compressData(msg.LOCATION.LOCATION)
+        } else {
+          return 'Note: LOCATION field not found'
+        }
       }
     }
     return 'pending'
--- packages/components/src/local/coa-status-board/index.tsx
+++ packages/components/src/local/coa-status-board/index.tsx
@@ -14,6 +14,9 @@ import ChannelCoaMessageDetail from '../molecules/channel-coa-message-detail'
 import { ForceData, ForceRole } from '@serge/custom-types'
 import getAssignees from './helpers/assignees'
 import moment from 'moment'
+import getColumns from './helpers/get-columns'
+import { SpecialChannelColumns } from '../../../../config/build/enums'
+import { capitalize } from 'lodash'
 
 /** combine force id and color
  */
@@ -102,6 +105,7 @@ export const CoaStatusBoard: React.FC<Props> = ({ templates, messages, channel, 
     // am I the owner?
     const myDocument = ownerComposite === myRoleFormatted
     const lastUpdated = collab ? moment(collab.lastUpdated).fromNow() : 'Pending'
+
     const res = [
       message.message.Reference || message._id,
       message.details.from.roleName,
@@ -113,35 +117,51 @@ export const CoaStatusBoard: React.FC<Props> = ({ templates, messages, channel, 
       myDocument,
       lastUpdated
     ]
+
+    // see if there are any other columns required
+    if (channel.collabOptions && channel.collabOptions.extraColumns) {
+      const newCols = getColumns(message, channel.collabOptions.extraColumns)
+      res.push(newCols)
+    }
+
     return res
   })
 
+  const columnHeaders = [
+    'ID',
+    {
+      filters: [...new Set(filtersRoles)],
+      label: 'From'
+    },
+    'Title',
+    {
+      filters: [
+        CollaborativeMessageStates.Unallocated,
+        CollaborativeMessageStates.BeingEdited,
+        CollaborativeMessageStates.BeingEdited,
+        CollaborativeMessageStates.PendingReview,
+        CollaborativeMessageStates.Released
+      ],
+      label: 'Status'
+    },
+    {
+      filters: listofOwners,
+      label: 'Owner'
+    },
+    'Updated'
+  ]
+
+  if (channel.collabOptions && channel.collabOptions.extraColumns) {
+    const newCols = channel.collabOptions.extraColumns.map((col: SpecialChannelColumns): string => {
+      return capitalize(col)
+    })
+    columnHeaders.push(...newCols)
+  }
+
   const dataTableProps = {
-    columns: [
-      'ID',
-      {
-        filters: [...new Set(filtersRoles)],
-        label: 'From'
-      },
-      'Title',
-      {
-        filters: [
-          CollaborativeMessageStates.Unallocated,
-          CollaborativeMessageStates.BeingEdited,
-          CollaborativeMessageStates.BeingEdited,
-          CollaborativeMessageStates.PendingReview,
-          CollaborativeMessageStates.Released
-        ],
-        label: 'Status'
-      },
-      {
-        filters: listofOwners,
-        label: 'Owner'
-      },
-      'Updated'
-    ],
+    columns: columnHeaders,
     data: data.map((row, rowIndex): any => {
-      const [id, mRole, forceColor, content, status, ownerName, ownerColor, myDocument, lastUpdated] = row
+      const [id, mRole, forceColor, content, status, ownerName, ownerColor, myDocument, lastUpdated, extraCols] = row
       const statusColors: { [property: string]: string } = {
         [CollaborativeMessageStates.Unallocated]: '#B10303',
         [CollaborativeMessageStates.PendingReview]: '#434343',
@@ -175,25 +195,32 @@ export const CoaStatusBoard: React.FC<Props> = ({ templates, messages, channel, 
         )
       }
 
+      const cells = [
+        id,
+        {
+          component: <Badge customBackgroundColor={forceColor} label={mRole} />,
+          label: mRole
+        },
+        content,
+        {
+          component: <Badge customBackgroundColor={status ? statusColors[status] : '#434343'} label={status} />,
+          label: status
+        },
+        {
+          component: ownerName ? <Badge customBackgroundColor={ ownerColor } customSize={myDocument && 'large'} label={isCollaborating && ownerName} /> : null,
+          label: ownerName
+        },
+        lastUpdated
+      ]
+
+      // extra cols?
+      if (extraCols) {
+        cells.push(...extraCols)
+      }
+
       return {
         collapsible,
-        cells: [
-          id,
-          {
-            component: <Badge customBackgroundColor={forceColor} label={mRole} />,
-            label: mRole
-          },
-          content,
-          {
-            component: <Badge customBackgroundColor={status ? statusColors[status] : '#434343'} label={status} />,
-            label: status
-          },
-          {
-            component: ownerName ? <Badge customBackgroundColor={ ownerColor } customSize={myDocument && 'large'} label={isCollaborating && ownerName} /> : null,
-            label: ownerName
-          },
-          lastUpdated
-        ]
+        cells: cells
       }
     })
   }
