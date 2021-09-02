f001d559e7f45cc086bfbdd56e919d4df3b7d1fc
Ian Mayo <ian@planetmayo.com>
2021-09-02 12:27:02 +0100

    Make it generic

--- packages/components/src/local/coa-status-board/helpers/get-columns.ts
+++ packages/components/src/local/coa-status-board/helpers/get-columns.ts
@@ -2,18 +2,18 @@ import { MessageCustom } from "@serge/custom-types"
 import { SpecialChannelColumns } from "../../../../../config/build/enums"
 
 const compressData = (message: any): string => {
-  if(message) {
-    const listed = Object.keys(message).map((prop) => {
-      const items = message[prop]      
-      return items.map((item: any) => {
-        return (item.Country) ? prop + '-' + item.Country : 'Country missing'
+  const listed = Object.keys(message).map((prop) => {
+    const items = message[prop]
+    const matches = items.map((item: any) => {
+      return Object.keys(item).map((prop2) => {
+        return '' + prop + ' - ' + item[prop2]
       })
     })
     // introduce space separator, to let them flow to newline
-    return '' + listed.join(', ')  
-  } else {
-    return '[Loc not found]'
-  }
+    return matches.join(', ')
+  })
+  // introduce space separator, to let them flow to newline
+  return '' + listed.join(', ')  
 }
 
 /** get a list of the roles in this participant group, if it is
