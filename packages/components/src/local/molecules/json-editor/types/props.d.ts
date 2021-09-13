77faf0592dcd06f61e8d84be43c237e0a95aee2e
Ian Mayo <ian@planetmayo.com>
2021-09-13 09:45:25 +0100

    document props

--- packages/components/src/local/molecules/json-editor/types/props.d.ts
+++ packages/components/src/local/molecules/json-editor/types/props.d.ts
@@ -4,8 +4,22 @@ export default interface Props {
   onChange?: (nextMessage: MessageCustom) => void
   getJsonEditorValue?: (value: { [property: string]: any }) => void
   message: MessageCustom
+  /**
+   * dictionary of templates, indexed by template name
+   */
   messageTemplates: TemplateBodysByKey
+  /**
+   * whether the form is editable (disable for read-only view)
+   */
   disabled?: boolean
+  /**
+   * persist fields in local storage, restore them on render.
+   * This was introduced to overcome rendering issue, but then
+   * the underlying issue was fixed.
+   */
   saveEditedMessage?: boolean
+  /**
+   * in read view (disabled) make textarea items tall enough to view all contents
+   */
   expandHeight?: boolean
 }
