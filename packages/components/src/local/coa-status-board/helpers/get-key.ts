3a7dca399b4beb481e89b92626e1915855a349f5
Ian Mayo <ian@planetmayo.com>
2021-09-22 17:28:50 +0100

    Extend logic on how to produce a new key

--- packages/components/src/local/coa-status-board/helpers/get-key.ts
+++ packages/components/src/local/coa-status-board/helpers/get-key.ts
@@ -1,8 +1,21 @@
 import { MessageCustom } from '@serge/custom-types'
 
-const getKey = (message: MessageCustom, canCollaborate: boolean, canReleaseMessages: boolean, canUnClaimMessages: boolean): string => {
-  const useReference = canCollaborate || canReleaseMessages || canUnClaimMessages
-  return useReference ? message._id + '-' + message.message.Reference : message._id
+/** produce a key for this message
+ * @param message the new message
+ * @param canCollaborate if this role can collaborate in this channel
+ * @param canReleaseMessages if this role can release messages in this channel
+ * @param canUnClaimMessages if this role can unclaim messages in this channel
+ * @param isFinalised if this document has been finalised/closed/released. Submitters of
+ *                    RFIs are only informed of an update if it's the finalised document
+ * @param isCollabEdit if this is a collab response channel
+ * @param isObserver if this role is an observer, and should see all updates as new messages
+ */
+const getKey = (message: MessageCustom, canCollaborate: boolean, canReleaseMessages: boolean,
+  canUnClaimMessages: boolean, isFinalisedResponse: boolean, isCollabEdit: boolean, isObserver: boolean): string => {
+  const useId = canCollaborate || canReleaseMessages || canUnClaimMessages || isObserver || isCollabEdit
+  const res = useId ? message._id : message.message.Reference + (isFinalisedResponse ? '-finalised' : '')
+  console.log('key', res, canCollaborate, canReleaseMessages, canUnClaimMessages, useId, message, message.message.Reference, isFinalisedResponse)
+  return res
 }
 
 export default getKey
