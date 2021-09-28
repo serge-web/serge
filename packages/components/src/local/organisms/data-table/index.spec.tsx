aa5fa8153024738ed2f319e0c291295ac160e26f
Lilit Karapetyan <lilit.karapetyan.web@gmail.com>
2021-09-28 13:13:58 +0400

    snap update

--- packages/components/src/local/organisms/data-table/index.spec.tsx
+++ packages/components/src/local/organisms/data-table/index.spec.tsx
@@ -7,23 +7,79 @@ import { MessageCustom } from '@serge/custom-types/message'
 import { GameMessagesMockRFI } from '@serge/mocks'
 import { mostRecentOnly } from '@serge/helpers'
 
-import DataTable, { ROW_WITH_COLLAPSIBLE_TYPE } from './index'
+import DataTable, { ROW_DATA_TYPE, ROW_WITH_COLLAPSIBLE_TYPE } from './index'
 import { RowWithCollapsibleType } from './types/props'
 
 const rfiMessages = (GameMessagesMockRFI as MessageCustom[]).filter((message: MessageCustom) => message.details.messageType === 'RFI')
 const newest = mostRecentOnly(rfiMessages) as MessageCustom[]
 
-const rfiData = newest.map((message: any) => {
+const rfiData: any[][] = []
+
+const data: RowWithCollapsibleType[] = newest.map((message, rowIndex) => {
   const messageItem = (message as MessageCustom)
-  return [
+  const row = [
     messageItem._id,
     messageItem.details.channel,
     messageItem.details.from.roleName,
     messageItem.details.from.forceColor,
     messageItem.message.Title,
-    messageItem.details.collaboration?.status,
-    messageItem.details.collaboration?.owner
+      messageItem.details.collaboration?.status,
+      messageItem.details.collaboration?.owner
   ]
+
+  rfiData.push(row)
+
+  const statusColors = {
+    Unallocated: '#B10303',
+    'In progress': '#E7740A',
+    'Pending review': '#434343',
+    Released: '#007219'
+  }
+  return {
+    type: ROW_WITH_COLLAPSIBLE_TYPE,
+    rowKey: 'rowKey' + rowIndex,
+    collapsible: (): React.ReactElement => (
+      <RfiForm onSubmit={console.log} onReject={console.log} message={(newest[rowIndex] as MessageCustom)} />
+    ),
+    cells: [
+      {
+        rowkey: 'id',
+        type: ROW_DATA_TYPE,
+        component: null,
+        label: messageItem._id
+      },
+      {
+        rowkey: 'id',
+        type: ROW_DATA_TYPE,
+        component: null,
+        label: messageItem.details.channel
+      },
+      {
+        rowkey: 'id',
+        type: ROW_DATA_TYPE,
+        component: <Badge customBackgroundColor={messageItem.details.from.forceColor} label={messageItem.details.from.roleName}/>,
+        label: messageItem.details.from.roleName
+      },
+      {
+        rowkey: 'id',
+        type: ROW_DATA_TYPE,
+        component: null,
+        label: messageItem.details.from.forceColor
+      },
+      {
+        rowkey: 'id',
+        type: ROW_DATA_TYPE,
+        component: <Badge customBackgroundColor={messageItem.details.collaboration?.status ? statusColors[messageItem.details.collaboration?.status] : '#434343'} customSize="large" label={messageItem.details.collaboration?.status}/>,
+        label: messageItem.message.Title
+      },
+      {
+        rowkey: 'id',
+        type: ROW_DATA_TYPE,
+        component: messageItem.details.collaboration?.owner ? <Badge customBackgroundColor="#434343" label={messageItem.details.collaboration?.owner}/> : null,
+        label: `${messageItem.details.collaboration?.owner}`
+      }
+    ]
+  }
 })
 
 const uniqueFieldValues = (messages: any[], col: number): any => {
@@ -60,40 +116,6 @@ const columns = [
     label: 'Owner'
   }
 ]
-
-const data = rfiData.map((row: any, rowIndex: any): RowWithCollapsibleType => {
-  const [id, channel, role, forceColor, content, status, owner] = row
-  const statusColors = {
-    Unallocated: '#B10303',
-    'In progress': '#E7740A',
-    'Pending review': '#434343',
-    Released: '#007219'
-  }
-  return {
-    type: ROW_WITH_COLLAPSIBLE_TYPE,
-    rowKey: 'rowKey' + rowIndex,
-    collapsible: (): React.ReactElement => (
-      <RfiForm onSubmit={console.log} onReject={console.log} message={(newest[rowIndex] as MessageCustom)} />
-    ),
-    cells: [
-      id,
-      channel,
-      {
-        component: <Badge customBackgroundColor={forceColor} label={role}/>,
-        label: role
-      },
-      content,
-      {
-        component: <Badge customBackgroundColor={status ? statusColors[status] : '#434343'} customSize="large" label={status}/>,
-        label: status
-      },
-      {
-        component: owner ? <Badge customBackgroundColor="#434343" label={owner}/> : null,
-        label: owner
-      }
-    ]
-  }
-})
 
 describe('DataTable component:', () => {
   it('renders correctly', () => {
