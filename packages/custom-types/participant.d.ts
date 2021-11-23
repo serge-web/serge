0f1bc9513c56bbc8b26815e07196ca3a7176aa0e
Ian Mayo <ian@planetmayo.com>
2021-11-23 12:58:43 +0000

    new participant types

--- packages/custom-types/participant.d.ts
+++ packages/custom-types/participant.d.ts
@@ -6,13 +6,13 @@ export interface ParticipantTemplate {
 }
 
 export default interface Participant {
-  force: string,
-  readonly forceUniqid: string,
+  force: string
+  readonly forceUniqid: string
   // specific set of roles that participate in this channel (or empty for all roles)
-  roles: Array<Role['roleId']>,
-  subscriptionId: string,
-  templates: ParticipantTemplate[],
-  icon?: any // TODO: Check it
+  roles: Array<Role['roleId']>
+  subscriptionId: string
+  templates: ParticipantTemplate[]
+  icon?: any 
   /** if this set of participants can take part in collaborative working */
   canCollaborate?: boolean
   /** if this set of participants can release collaborative messages */
@@ -20,3 +20,47 @@ export default interface Participant {
   /** can un-claim messages */
   canUnClaimMessages?: boolean
 }
+
+
+/** core properties for a participant */
+interface CoreParticipant {
+  force: string,
+  readonly forceUniqid: string,
+  // specific set of roles that participate in this channel (or empty for all roles)
+  roles: Array<Role['roleId']>,
+  subscriptionId: string,
+  icon?: any
+}
+
+/** participation in standard channels */
+export interface ParticipantCustom extends CoreParticipant {
+  // the templates this participant can create
+  templates: ParticipantTemplate[],
+}
+
+/** participation in chat channels */
+export interface ParticipantChat extends CoreParticipant {
+}
+
+/** increasing permissions in a collaborative editing channel
+ */
+ export enum CollaborativePermission {
+   /** can */
+   CannotCollaborate,
+   CanEdit,
+   CanSubmitForReview,
+   CanApprove,
+   CanRelease,
+   CanUnClaim
+}
+
+
+/** participation in collaborative editing channels */
+export interface ParticipantCollab extends CoreParticipant {
+  // partipant can create new documents in this channel
+  canCreate: boolean,
+  // participant views un-released documents
+  viewUnreleasedVersions: boolean
+  // level of access for the participant
+  permission: CollaborativePermission
+} 
