f3da497abe18844ff012e8e489f944179853d830
Ian Mayo <ian@planetmayo.com>
2021-11-25 09:27:15 +0000

    Introduce new collab states for v3

--- packages/config/src/enums.ts
+++ packages/config/src/enums.ts
@@ -106,6 +106,34 @@ export enum CollaborativeMessageStates {
   BeingEdited = 'Being edited',
 }
 
+/** the phases a v3 collaborative message goes through before
+ * being approved to send
+ */
+ export enum CollaborativeMessageStates2 {
+  /**
+   * message is waiting to be allocated to owner
+   */
+  Unallocated = 'Unallocated',
+  /**
+   * non collab-editing (e.g. RFI) document being edited
+   * //TODO: remove this when we retire RFI status board.
+   */
+  InProgress = 'In progress',
+  /**
+   * owner finished editing, waiting for review
+   */
+  PendingReview = 'Pending review',
+  /**
+   * approved and released
+   */
+  Released = 'Released',
+  /** 
+   * document cancelled 
+   */
+  Closed = 'Closed',
+}
+
+
 /** the commands that can be appled to a collaborative message
  */
  export enum CollaborativeMessageCommands {
