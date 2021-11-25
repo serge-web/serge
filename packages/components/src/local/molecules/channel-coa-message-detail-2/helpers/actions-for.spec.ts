c169d6d42c7ee5cf4d8b0fc822a70988a6264310
Ian Mayo <ian@planetmayo.com>
2021-11-25 09:39:22 +0000

    Working

--- packages/components/src/local/molecules/channel-coa-message-detail-2/helpers/actions-for.spec.ts
+++ packages/components/src/local/molecules/channel-coa-message-detail-2/helpers/actions-for.spec.ts
@@ -1,16 +1,34 @@
 import { CollaborativePermission as Permission, CollaborativeMessageStates2 as States } from '@serge/config'
 import { createActionTable, actionsFor, ActionTable } from './actions-for'
 
-describe('Changer tests', () => {
-  it('simple state changes', () => {
+describe('Action table & actions', () => {
+  it('Table generation', () => {
     const table: ActionTable = createActionTable(['Approve1'], ['Request1', 'Endorse1'], ['Finalise'])
-    expect(table).toBeTruthy()
+    expect(table[States.Unallocated]).toBeTruthy()
+    expect(table[States.Unallocated].length).toEqual(2) // we have handler in second slot
+    expect(table[States.Unallocated][0]).toBeFalsy() // empty first slot
+    expect(table[States.Unallocated][1]).toBeTruthy() // populated second slot
+    expect(table[States.Unallocated][1].length).toEqual(1) // just one handler
+    expect(table[States.Unallocated][1][0].handler).toBeTruthy() // populated second slot
+    expect(table[States.Unallocated][1][0].verbs.length).toEqual(2) // just one verb
+  })
+
+  it('Retrieve actions', () => {
+    const table: ActionTable = createActionTable(['Approve1'], ['Request1', 'Endorse1'], ['Finalise'])
     // initial state
-    const saveBeingEdited = actionsFor(table, States.InProgress, Permission.CanEdit)
-    expect(saveBeingEdited).toBeTruthy()
-    expect(saveBeingEdited.length).toEqual(1)
-    expect(saveBeingEdited[0].verbs.length).toEqual(1)
-    expect(saveBeingEdited[0].handler).toBeTruthy()
-    console.log(saveBeingEdited)
+    const canSaveBeingEdited = actionsFor(table, States.InProgress, Permission.CanEdit)
+    expect(canSaveBeingEdited).toBeTruthy()
+    expect(canSaveBeingEdited.length).toEqual(1)
+    expect(canSaveBeingEdited[0].verbs.length).toEqual(1)
+    expect(canSaveBeingEdited[0].verbs[0]).toEqual('Save')
+    expect(canSaveBeingEdited[0].handler).toBeTruthy()
+    const releaseBeingEdited = actionsFor(table, States.InProgress, Permission.CanEdit)
+    expect(releaseBeingEdited).toEqual(canSaveBeingEdited)
+
+    const canEditWithFinalised = actionsFor(table, States.Released, Permission.CanEdit)
+    expect(canEditWithFinalised.length).toEqual(0) // doesn't have permission to do anything
+
+    const canReleaseWithFinalised = actionsFor(table, States.Released, Permission.CanRelease)
+    expect(canReleaseWithFinalised.length).toEqual(1) // doesn't have permission to do anything
   })
 })
