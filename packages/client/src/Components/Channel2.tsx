967e55b59fb996d04829c75c10953ed773b7d277
Ian Mayo <ian@planetmayo.com>
2021-12-06 17:39:12 +0000

    Improve channel & template logic

--- packages/client/src/Components/Channel2.tsx
+++ packages/client/src/Components/Channel2.tsx
@@ -9,7 +9,7 @@ import {
   markUnread
 } from '../ActionsAndReducers/playerUi/playerUi_ActionCreators'
 import { usePlayerUiState, usePlayerUiDispatch } from '../Store/PlayerUi'
-import { ChannelCollab, MessageChannel, MessageCustom, TemplateBody } from '@serge/custom-types'
+import { ChannelCollab, MessageChannel, MessageCustom, ParticipantCollab, TemplateBody } from '@serge/custom-types'
 import { CoaStatusBoard2 } from "@serge/components";
 import { CHANNEL_COLLAB, SpecialChannelTypes } from "@serge/config";
 import '@serge/themes/App.scss'
@@ -23,7 +23,7 @@ const Channel2: React.FC<{ channelId: string }> = ({ channelId }) => {
   if (selectedForce === undefined) throw new Error('selectedForce is undefined')
 
   const channel = state.channels[channelId]
-  const v3Channel = channel as unknown as ChannelCollab
+  const v3Channel = channel.v3Channel as unknown as ChannelCollab
   if(!v3Channel) {
     console.warn('failed to receive v3 data')
     return (
@@ -65,14 +65,34 @@ const Channel2: React.FC<{ channelId: string }> = ({ channelId }) => {
   const messages = channelMessages ? channelMessages as MessageChannel[] : []
   const isLegacyCollabWorking = channelFormat === SpecialChannelTypes.CHANNEL_COLLAB_EDIT || channelFormat === SpecialChannelTypes.CHANNEL_COLLAB_RESPONSE
   
+  //
+  const role = {
+    forceId: selectedForce.uniqid,
+    forceName: selectedForce.name,
+    roleId: selectedRole,
+    roleName: selectedRoleName
+  }
+  const participationsForMyForce = v3Channel.participants.filter((p: ParticipantCollab) => p.force === role.forceName)
+  // participations relate to me if they contain no roles, or if they contain my role
+  const isParticipating = participationsForMyForce.filter((p: ParticipantCollab) => (p.roles.length === 0 || p.roles.includes(role.roleId)))
+  // can I create messages in the channel?
+  const canCreateMessages = isParticipating.filter((p: ParticipantCollab) => (p.canCreate)).length > 0
 
   const templates = channel.templates || []
+  const allTemplates = state.allTemplatesByKey
+  const newMessageTemplate = allTemplates[v3Channel.newMessageTemplate._id]
   // if this is a collab working channel, strip out any chat templates - since we only use structured messages
   // in collab working channels
-  const trimmedTemplates = isLegacyCollabWorking ? templates.filter((temp: TemplateBody) => temp.title !== 'Chat') : templates
+ // const trimmedTemplates = isLegacyCollabWorking ? templates.filter((temp: TemplateBody) => temp.title !== 'Chat') : templates
+  const trimmedTemplates = [newMessageTemplate]
+
   const observing = !!channel.observing
 
   const isCollabEdit = v3Channel.channelType === CHANNEL_COLLAB
+
+  if(channel.name === 'Blue RFI 2') [
+    console.log('in channel 2', channel, isCollabEdit, v3Channel)
+  ]
 
   return (
     <div className={channelTabClass} data-channel-id={channelId}>
@@ -84,12 +104,7 @@ const Channel2: React.FC<{ channelId: string }> = ({ channelId }) => {
             onMarkAllAsRead={markAllMsgAsRead}
             templates={state.allTemplatesByKey}
             messages={messages as MessageCustom[]}
-            role={{
-              forceId: selectedForce.uniqid,
-              forceName: selectedForce.name,
-              roleId: selectedRole,
-              roleName: selectedRoleName
-            }}
+            role={role}
             forces={state.allForces}
             isUmpire={!!isUmpire}
             isObserver={observing}
@@ -100,7 +115,7 @@ const Channel2: React.FC<{ channelId: string }> = ({ channelId }) => {
         )}
       </div>
       {
-        !observing && trimmedTemplates.length > 0 &&
+        !observing && canCreateMessages && trimmedTemplates.length > 0 &&
         <NewMessage
           orderableChannel={true}
           curChannel={channelId}
