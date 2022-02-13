7f89e63399d603f505bd3131087bfd5b31ea18e6
philb89 <everlasting2605@gmail.com>
2022-02-13 16:32:13 +0700

    clean code

--- packages/components/src/local/molecules/more-info/index.tsx
+++ packages/components/src/local/molecules/more-info/index.tsx
@@ -3,12 +3,16 @@ import IconButton from '@material-ui/core/IconButton'
 import Popover from '@material-ui/core/Popover'
 import { Close } from '@material-ui/icons'
 import Info from '@material-ui/icons/Info'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from './styles.module.scss'
 import MoreInfoProps from './types/props'
 
+const MAX_INFO_LENGTH = 100
+
 const MoreInfo: React.FC<MoreInfoProps> = ({ description, children }) => {
   const [anchorElm, setAnchorElm] = useState<HTMLElement | null>(null)
+  const [isDisplayFull, setDisplayFullMode] = useState<boolean>(false)
+  const [displayInfo, setDisplayInfo] = useState<string>('')
 
   const showMoreInfo = (e: React.MouseEvent<HTMLDivElement>): void => {
     setAnchorElm(e.currentTarget)
@@ -16,9 +20,26 @@ const MoreInfo: React.FC<MoreInfoProps> = ({ description, children }) => {
 
   const closeInfoCard = (): void => {
     setAnchorElm(null)
+    showLess()
+  }
+
+  const showFull = (): void => {
+    setDisplayInfo(description)
+    setDisplayFullMode(true)
+  }
+
+  const showLess = (): void => {
+    setDisplayInfo(description.substring(0, MAX_INFO_LENGTH))
+    setDisplayFullMode(false)
   }
 
   const open = Boolean(anchorElm)
+
+  useEffect(() => {
+    if (description.length >= MAX_INFO_LENGTH) {
+      showLess()
+    }
+  }, [description])
 
   return (
     <>
@@ -34,7 +55,8 @@ const MoreInfo: React.FC<MoreInfoProps> = ({ description, children }) => {
             className={styles.header}
           ></CardHeader>
           <CardContent className={styles.content}>
-            {description}
+            {displayInfo}
+            {!isDisplayFull && <span onClick={showFull}>...show more</span>}
           </CardContent>
         </Card>
       </Popover>
