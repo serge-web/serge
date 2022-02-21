1327b4b485dbd99dd8658dabecb8d89bd2bdae2a
Ian Mayo <ian@planetmayo.com>
2022-02-21 12:49:19 +0000

    new rule breaking push. Remove it.

--- packages/helpers/.eslintrc.js
+++ packages/helpers/.eslintrc.js
@@ -16,7 +16,6 @@ module.exports = {
   'rules': {
     "semi": [2, "never"],
     "@typescript-eslint/ban-ts-ignore": "off",
-    '@typescript-eslint/no-param-reassign': 'error', // @IM prevent function params being reassigned (substitute for const parameters)
     '@typescript-eslint/no-use-before-define': 'off', // Allows us to hoist variables and functions which I am a fan of, functions not variables that is.
     '@typescript-eslint/no-explicit-any': 'off', // Too strict for my case, sometimes I need an any type
     '@typescript-eslint/member-delimiter-style': ['error', { // Prevents us from using any delimiter for interface properties.
