0648e604c40c8b0292323a95623c10a722401878
Ian Mayo <ian@planetmayo.com>
2022-02-24 18:26:47 +0000

    test running, code refactored

--- packages/components/src/local/assets/helpers/orientation-for.spec.tsx
+++ packages/components/src/local/assets/helpers/orientation-for.spec.tsx
@@ -1,9 +1,8 @@
-import { Asset, AttributeValue, AttributeValues, ForceData, PlatformTypeData, RouteTurn } from "@serge/custom-types"
-import orientationFor2 from "./orientation-for"
-import { brgBetweenTwoHex } from "../../mapping/helpers/h3-helpers"
-
-import { smallScaleForces, platformTypes } from '@serge/mocks'
+import { Asset, AttributeValues, ForceData, RouteTurn } from "@serge/custom-types"
+import orientationFor from "./orientation-for"
+import { smallScaleForces } from '@serge/mocks'
 import { OrientationMarker } from "@serge/custom-types/platform-type-data"
+import { brgBetweenTwoHex } from "../../mapping/helpers/h3-helpers"
 
 const getAsset = (forces: ForceData[]): Asset | undefined => {
   if(forces && forces[1] && forces[1].assets) {
@@ -14,69 +13,30 @@ const getAsset = (forces: ForceData[]): Asset | undefined => {
 }
 
 const asset = getAsset(smallScaleForces)
-const pType = asset && platformTypes.find((value: PlatformTypeData) => value.name === asset.platformType)
-
-/** retrive the cell at the supplied human-readable coords ("A01") */
-const orientationFor = (current: string, history: Array<RouteTurn>,   planned: Array<RouteTurn>, 
-  attributes: AttributeValues, orientation?: OrientationMarker): number | undefined => {
-
-  const getAttribute = (attributes: AttributeValues, id: string): number | undefined => {
-    const attr = attributes.find((aValue: AttributeValue) => aValue.attrId === id)
-    return attr && attr.value
-  }
-
-  if(orientation) {
-    if(orientation.attribute && orientation.origin === "absolute") {
-      // don't worry about the direction
-      return getAttribute(attributes, orientation.attribute)
-    } else {
-      let dest = undefined
-      if (planned && planned.length) {
-        // direction to first step
-        const turn = planned[0]
-        if (turn.route && turn.route.length) {
-          dest = turn.route[0]
-        }
-      } else if (history && history.length) {
-        // direction from last history step
-        const turn = history[history.length-1]
-        if(turn && turn.route && turn.route.length) {
-          const routeSteps = turn.route.length
-          if (routeSteps) {
-            dest = turn.route[routeSteps-1]
-          }  
-        }
-      }
-      if (dest) {
-        const angle = brgBetweenTwoHex(current, dest)
-        if(orientation.attribute && orientation.attribute === 'relative') {
-          const offset = getAttribute(attributes, orientation.attribute) || 0
-          return offset + angle
-        } else {
-          return angle
-        }
-      } else {
-        return undefined
-      }
-    }
-  } else {
-    return undefined
-  }
-}
-
-if (!orientationFor2) {
-  console.log('break me')
-}
 
 it('Calculates correct orientation', () => {
   if (asset) {
-    const orientation = pType?.orientation
     const history: RouteTurn[] = asset.history || []
     const planned: RouteTurn[] = asset.plannedTurns || []
-    const attributes: AttributeValues = []
-    const current = 'C12'
-    expect(orientationFor(current, history, planned, attributes, orientation)).toEqual(undefined)
-    test permutations of orientation!
+    const attributes: AttributeValues = asset.attributeValues || []
+    const current = asset.position || ''
+
+    // start off without using attribute
+    const rel: OrientationMarker = { origin: 'relative' }
+
+    // check we're looking at the asset we expect to look at
+    expect(asset && asset.name).toEqual('SSN-1')
+
+    // force use of history
+    expect(orientationFor(current, history, [], attributes, rel)).toBeCloseTo(78, 0)
+    // allow use of planned
+    expect(orientationFor(current, history, planned, attributes, rel)).toBeCloseTo(15, 0)
+
+    //test permutations of orientation!
+    const relAttr: OrientationMarker = { origin: 'relative', attribute: 'asdic-dir'}
+    expect(orientationFor(current, history, planned, attributes, relAttr)).toBeCloseTo(150, 0)
+    const absAttr: OrientationMarker = { origin: 'absolute', attribute: 'asdic-dir'}
+    expect(orientationFor(current, history, planned, attributes, absAttr)).toBeCloseTo(135, 0)
   }
 })
 
@@ -84,6 +44,7 @@ it('Maths works as expected', () => {
   const pos1 = '8718a84daffffff'
   const pos2 = '8718a84d8ffffff'
   expect(brgBetweenTwoHex(pos1, pos2)).toBeCloseTo(78,0)
+  expect(brgBetweenTwoHex(pos2, pos1)).toBeCloseTo(258,0)
 })
 
 
