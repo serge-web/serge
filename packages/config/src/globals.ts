b3993163645d18f25bcb12ea5009c5a13eac029c
Ian Mayo <ian@planetmayo.com>
2022-04-06 16:21:19 +0100

    attempt fix to connect to host, instead of localhost

--- packages/config/src/globals.ts
+++ packages/config/src/globals.ts
@@ -131,7 +131,7 @@ export const hiddenPrefix = '_#_'
 
 export const clearAll = 'clearAll'
 export const allDbs = 'allDbs'
-export const socketPath = 'http://localhost:4000'
+export const socketPath = origin.replace('3000', '4000')
 export const replicate = 'replicate/'
 export const deletePath = 'delete/'
 export const localSettings = '_local/settings'
