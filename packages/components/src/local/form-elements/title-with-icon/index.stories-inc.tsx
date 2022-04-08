fdf7ac853e3370b065d83a827debfc87f18d916f
Ian Mayo <ian@planetmayo.com>
2022-04-08 17:06:58 +0100

    include story in build

--- packages/components/src/local/form-elements/title-with-icon/index.stories-inc.tsx
+++ packages/components/src/local/form-elements/title-with-icon/index.stories-inc.tsx
@@ -16,4 +16,4 @@ export default {
   }
 }
 
-export const Default: React.FC = () => <TitleWithIcon platformType="merchant-vessel" forceColor="blue">Test</TitleWithIcon>
+export const Default: React.FC = () => <TitleWithIcon icon="merchant-vessel.svg" platformType="merchant-vessel" forceColor="blue">Test</TitleWithIcon>
