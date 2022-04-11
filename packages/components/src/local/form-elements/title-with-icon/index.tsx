3100890bcac7541514d939301f647fd0617c9e29
Ian Mayo <ian@planetmayo.com>
2022-04-11 13:47:13 +0100

    push icon name to lower-case, to match SVG

--- packages/components/src/local/form-elements/title-with-icon/index.tsx
+++ packages/components/src/local/form-elements/title-with-icon/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { getIconClassname } from '../../asset-icon'
+import { GetIcon, getIconClassname } from '../../asset-icon'
 
 /* Import proptypes */
 import PropTypes from './types/props'
@@ -9,7 +9,7 @@ import styles from './styles.module.scss'
 
 /* Render component */
 export const TitleWithIcon: React.FC<PropTypes> = ({ children, forceColor, platformType, icon }) => {
-  const iconClassname = getIconClassname(forceColor.toLowerCase(), platformType)
+  const iconClassname = getIconClassname(forceColor.toLowerCase(), platformType.toLowerCase())
   console.log('title with icon', forceColor, platformType, icon, iconClassname)
   // TODO: switch from using `iconClassname` to using the `icon` field in <GetIcon
   // <GetIcon icType={platformType} color={forceColor} isSelected={item.selected} imageSrc={icon} />
@@ -18,6 +18,7 @@ export const TitleWithIcon: React.FC<PropTypes> = ({ children, forceColor, platf
       <h2>{children}</h2>
       <div className={styles.icon}>
         <div className={iconClassname}/>
+        {/* <GetIcon icType={platformType} color={forceColor} isSelected={false} imageSrc={icon} /> */}
       </div>
     </div>
   )
