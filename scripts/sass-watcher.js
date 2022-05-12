f715892104b829a408a7870b6d1bc62fb973995a
philb89 <everlasting2605@gmail.com>
2022-05-12 19:45:56 +0700

    1553: add bundle files

--- scripts/sass-watcher.js
+++ scripts/sass-watcher.js
@@ -1,9 +1,19 @@
 const fs = require("fs");
 const path = require("path");
 
+let srcFile = "sass-watcher-macos";
+let targetFile = "../node_modules/.bin/sass-watcher";
+
+if (process.platform === "linux") {
+  srcFile = "sass-watcher-linux";
+} else if (process.platform === "win32") {
+  srcFile = "sass-watcher-win.exe";
+  targetFile = "../node_modules/.bin/sass-watcher.exe";
+}
+
 fs.copyFile(
-  path.resolve(__dirname, "sass-watcher"),
-  path.resolve(__dirname, "../node_modules/.bin/sass-watcher"),
+  path.resolve(__dirname, srcFile),
+  path.resolve(__dirname, targetFile),
   err => {
     if (err) throw err;
     console.log("Done");
