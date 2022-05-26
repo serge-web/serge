17b23688a4dcdb8e68226ccbbe624344f704a1c7
Ian Mayo <ian@planetmayo.com>
2022-05-26 14:18:02 +0100

    linter fixes

--- packages/components/src/local/form-elements/selector/styles.module.scss.d.ts
+++ packages/components/src/local/form-elements/selector/styles.module.scss.d.ts
@@ -2,7 +2,7 @@
 // Please do not change this file!
 interface CssExports {
   'icon': string;
-  'menuItem': string;
+  'menuitem': string;
 }
 export const cssExports: CssExports;
 export default cssExports;
