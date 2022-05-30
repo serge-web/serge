703ebe364a427aaab01ad5e84dfc1abcfa1b8e54
Ian Mayo <ian@planetmayo.com>
2022-05-30 12:13:15 +0100

    linter fix

--- packages/components/src/local/molecules/editable-row/types/props.d.ts
+++ packages/components/src/local/molecules/editable-row/types/props.d.ts
@@ -34,8 +34,8 @@ export default interface PropTypes {
   noSwitchOnReset?: boolean
   isGenerator?: boolean
   participantKey?: number
-  /** 
-   * for multiple selection entries, display then as a 
+  /**
+   * for multiple selection entries, display then as a
    * vertical list
    */
   presentAsList?: boolean
