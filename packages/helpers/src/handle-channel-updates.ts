70666f1f4e904e62f8fe9c7713853486430c438c
Ian Mayo <ian@planetmayo.com>
2022-06-22 12:03:59 +0100

    more robust processing

--- packages/helpers/src/handle-channel-updates.ts
+++ packages/helpers/src/handle-channel-updates.ts
@@ -93,8 +93,9 @@ export const isMessageHasBeenRead = (id: string, currentWargame: string, forceId
 )
 
 export const clipInfoMEssage = (message: MessageInfoType | MessageInfoTypeClipped, hasBeenRead = false): MessageInfoTypeClipped => {
-  if (message.messageType !== INFO_MESSAGE && message.messageType !== INFO_MESSAGE_CLIPPED) {
-    throw new TypeError(`Message should be INFO_MESSAGE: "${INFO_MESSAGE}" type`)
+  if (message.messageType !== undefined && message.messageType !== INFO_MESSAGE && message.messageType !== INFO_MESSAGE_CLIPPED) {
+    console.warn('wrong message type', message)
+    // throw new TypeError(`Message should be INFO_MESSAGE: "${INFO_MESSAGE}" type`)
   }
   return {
     messageType: INFO_MESSAGE_CLIPPED,
