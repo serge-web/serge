be22df6068aef97e89375a8295b89753802b3435
philb89 <everlasting2605@gmail.com>
2022-08-07 20:22:56 +0700

    1728: Fix issue of <Select/> control in Attribute Editor

--- packages/components/src/local/plan-turn-form/index.tsx
+++ packages/components/src/local/plan-turn-form/index.tsx
@@ -144,10 +144,10 @@ export const PlanTurnForm: React.FC<PropTypes> = ({
       icon={icon.icon}
     >
       {formHeader}
-      { deleteEmptyTaskGroup &&
+      {deleteEmptyTaskGroup &&
         <Button onClick={deleteEmptyTaskGroup}>Group Empty - <b>Delete</b></Button>
       }
-      { plansSubmitted &&
+      {plansSubmitted &&
         <h5 className='sub-title'>(Form disabled, plans submitted)</h5>
       }
     </TitleWithIcon>
@@ -165,26 +165,26 @@ export const PlanTurnForm: React.FC<PropTypes> = ({
     </FormGroup>
     {statusVal.mobile
       ? speed.length > 0 &&
-        <FormGroup title="Speed (kts)" titlePosition="absolute">
-          <Speed
-            value = { validSpeedVal }
-            options = { speed }
-            onClick = { !formDisabled && speedHandler }
-          />
-        </FormGroup>
+      <FormGroup title="Speed (kts)" titlePosition="absolute">
+        <Speed
+          value={validSpeedVal}
+          options={speed}
+          onClick={!formDisabled && speedHandler}
+        />
+      </FormGroup>
       : <FormGroup title="For">
-        <Input className={clInput} disabled={formDisabled} name="turns" value={turnsVal} onChange={changeHandler}/>
+        <Input className={clInput} disabled={formDisabled} name="turns" value={turnsVal} onChange={changeHandler} />
         <span className={styles.text}>turns</span>
       </FormGroup>
     }
     {attributes && attributes.length > 0 &&
       <FormGroup title="Attributes" titlePosition="absolute">
         <div className={styles.attributelist}>
-          { attributes.map((item: AttributeEditorData): ReactElement => {
+          {attributes.map((item: AttributeEditorData): ReactElement => {
             const label = item.nameRead + item.valueRead
-            return <Badge title={item.description} key={item.attrId} allCaps={false} label={label}/>
+            return <Badge title={item.description} key={item.attrId} allCaps={false} label={label} />
           })}
-          { attributesAreEditable &&
+          {attributesAreEditable &&
             <span className={styles.editattributes}><Button onClick={openEditModal}>Edit</Button></span>
           }
         </div>
@@ -193,7 +193,7 @@ export const PlanTurnForm: React.FC<PropTypes> = ({
     <FormGroup title="Condition">
       <span className={styles.text}>{condition}</span>
     </FormGroup>
-    { !formDisabled &&
+    {!formDisabled &&
       <Button disabled={!saveEnabled} onClick={submitForm}>{statusVal.mobile ? 'Plan turn' : 'Next Turn'}</Button>
     }
   </div>
