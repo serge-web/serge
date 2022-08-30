c755a88f05c1c44c805c36c779d6a176db48283f
Ian Mayo <ian@planetmayo.com>
2022-08-30 14:30:58 +0100

    improve tests, update snapshot

--- packages/components/src/local/organisms/support-panel/index.spec.tsx
+++ packages/components/src/local/organisms/support-panel/index.spec.tsx
@@ -1,12 +1,23 @@
 /* global it expect */
-import { ChannelPlanning } from '@serge/custom-types'
-import { p9wargame, planningMessages } from '@serge/mocks'
+import { ChannelPlanning, ParticipantTemplate, TemplateBody } from '@serge/custom-types'
+import { checkV3ParticipantStates } from '@serge/helpers'
+import { p9wargame, planningMessages, planningMessageTemplatesMock } from '@serge/mocks'
 import { noop } from 'lodash'
 import React from 'react'
 import renderer from 'react-test-renderer'
 import SupportPanel from './index'
 
 const planningChannel = p9wargame.data.channels.channels[0] as ChannelPlanning
+
+const blueForce = p9wargame.data.forces.forces[1]
+const blueRole = blueForce.roles[0]
+
+const thisPart = checkV3ParticipantStates(planningChannel, blueForce.uniqid, blueRole.roleId, false)
+const myTemplateIds = thisPart.templatesIDs
+const myTemplates = planningMessageTemplatesMock.filter((value: TemplateBody) =>
+  myTemplateIds.find((id: ParticipantTemplate) => id._id === value._id)
+)
+
 
 describe('Support Panel component: ', () => {
   it('renders component correctly', () => {
@@ -17,14 +28,14 @@ describe('Support Panel component: ', () => {
         forceNames={[]}
         hideForcesInChannel={false}
         messages={planningMessages}
-        selectedForce={p9wargame.data.forces.forces[1].uniqid}
-        selectedRole={p9wargame.data.forces.forces[1].roles[0].roleId}
+        selectedForce={blueForce.uniqid}
+        selectedRole={blueRole.roleId}
         channel={planningChannel}
         forces={[p9wargame.data.forces]}
         onUnread={noop}
         onReadAll={noop}
         onRead={noop}
-        templates={[]}
+        templates={myTemplates}
       />)
       .toJSON()
     expect(tree).toMatchSnapshot()
