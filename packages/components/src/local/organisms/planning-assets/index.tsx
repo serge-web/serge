82eb72fa5d3e2dabbc04282dbc09398ea9a99cea
philb89 <everlasting2605@gmail.com>
2022-08-31 16:17:56 +0700

    support asset comprising

--- packages/components/src/local/organisms/planning-assets/index.tsx
+++ packages/components/src/local/organisms/planning-assets/index.tsx
@@ -1,48 +1,7 @@
 import { Asset } from '@serge/custom-types'
-import React, { forwardRef, useEffect, useState } from 'react'
-import PropTypes, { Row } from './types/props'
-
-import AddBox from '@material-ui/icons/AddBox'
-import ArrowDownward from '@material-ui/icons/ArrowDownward'
-import Check from '@material-ui/icons/Check'
-import ChevronLeft from '@material-ui/icons/ChevronLeft'
-import ChevronRight from '@material-ui/icons/ChevronRight'
-import Clear from '@material-ui/icons/Clear'
-import DeleteOutline from '@material-ui/icons/DeleteOutline'
-import Edit from '@material-ui/icons/Edit'
-import FilterList from '@material-ui/icons/FilterList'
-import FirstPage from '@material-ui/icons/FirstPage'
-import LastPage from '@material-ui/icons/LastPage'
-import Remove from '@material-ui/icons/Remove'
-import SaveAlt from '@material-ui/icons/SaveAlt'
-import Search from '@material-ui/icons/Search'
-import ViewColumn from '@material-ui/icons/ViewColumn'
 import MaterialTable, { Column } from 'material-table'
-
-type TableIcons = {
-  [x: string]: React.ForwardRefExoticComponent<React.RefAttributes<SVGSVGElement | null>>
-}
-
-// need to wrap it as a component to use for Orders and PlanningAssets
-const tableIcons: TableIcons = {
-  Add: forwardRef<SVGSVGElement | null>((props, ref) => <AddBox {...props} ref={ref} />),
-  Check: forwardRef<SVGSVGElement | null>((props, ref) => <Check {...props} ref={ref} />),
-  Clear: forwardRef<SVGSVGElement | null>((props, ref) => <Clear {...props} ref={ref} />),
-  Delete: forwardRef<SVGSVGElement | null>((props, ref) => <DeleteOutline {...props} ref={ref} />),
-  DetailPanel: forwardRef<SVGSVGElement | null>((props, ref) => <ChevronRight {...props} ref={ref} />),
-  Edit: forwardRef<SVGSVGElement | null>((props, ref) => <Edit {...props} ref={ref} />),
-  Export: forwardRef<SVGSVGElement | null>((props, ref) => <SaveAlt {...props} ref={ref} />),
-  Filter: forwardRef<SVGSVGElement | null>((props, ref) => <FilterList {...props} ref={ref} />),
-  FirstPage: forwardRef<SVGSVGElement | null>((props, ref) => <FirstPage {...props} ref={ref} />),
-  LastPage: forwardRef<SVGSVGElement | null>((props, ref) => <LastPage {...props} ref={ref} />),
-  NextPage: forwardRef<SVGSVGElement | null>((props, ref) => <ChevronRight {...props} ref={ref} />),
-  PreviousPage: forwardRef<SVGSVGElement | null>((props, ref) => <ChevronLeft {...props} ref={ref} />),
-  ResetSearch: forwardRef<SVGSVGElement | null>((props, ref) => <Clear {...props} ref={ref} />),
-  Search: forwardRef<SVGSVGElement | null>((props, ref) => <Search {...props} ref={ref} />),
-  SortArrow: forwardRef<SVGSVGElement | null>((props, ref) => <ArrowDownward {...props} ref={ref} />),
-  ThirdStateCheck: forwardRef<SVGSVGElement | null>((props, ref) => <Remove {...props} ref={ref} />),
-  ViewColumn: forwardRef<SVGSVGElement | null>((props, ref) => <ViewColumn {...props} ref={ref} />)
-}
+import React, { useEffect, useState } from 'react'
+import PropTypes, { Row } from './types/props'
 
 export const PlanningAssets: React.FC<PropTypes> = ({ forces, playerForce, render }: PropTypes) => {
   const [rows, setRows] = useState<Row[]>([])
@@ -72,6 +31,18 @@ export const PlanningAssets: React.FC<PropTypes> = ({ forces, playerForce, rende
         status: asset.status?.state || ''
       })
 
+      const comprisingAssets = asset.comprising?.map(compAsset => {
+        return {
+          id: compAsset.uniqid,
+          condition: compAsset.condition,
+          name: compAsset.name,
+          platformType: compAsset.platformType || '',
+          status: compAsset.status?.state || '',
+          parentId: asset.uniqid
+        }
+      }) || []
+      tableRows.push(...comprisingAssets)
+
       return Object.values(asset) as string[]
     })
 
@@ -94,11 +65,12 @@ export const PlanningAssets: React.FC<PropTypes> = ({ forces, playerForce, rende
 
   return <MaterialTable
     title={'Planning Assets'}
-    icons={tableIcons}
     columns={jestWorkerId ? [] : columns}
     data={jestWorkerId ? [] : rows}
+    parentChildData={(row, rows) => rows.find(a => a.id === row.parentId)}
     options={{
-      sorting: true
+      sorting: true,
+      selection: !jestWorkerId // fix unit-test for material table
     }}
   />
 }
