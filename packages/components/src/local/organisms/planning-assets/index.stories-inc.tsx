2043909d0898dd9e22d594069f8309c82bc74819
Ian Mayo <ian@planetmayo.com>
2022-09-01 14:06:37 +0100

    let player choose force

--- packages/components/src/local/organisms/planning-assets/index.stories-inc.tsx
+++ packages/components/src/local/organisms/planning-assets/index.stories-inc.tsx
@@ -7,8 +7,15 @@ import PlanningAssets from './index'
 import docs from './README.md'
 import MessageListPropTypes from './types/props'
 import { P9Mock } from '@serge/mocks'
+import { ForceData } from '@serge/custom-types'
+import { UMPIRE_FORCE } from '@serge/config'
 
 const wrapper: React.FC = (storyFn: any) => <div style={{ height: '600px' }}>{storyFn()}</div>
+
+const forces = P9Mock.data.forces.forces
+const blueForce = forces[1]
+
+const forceIds = forces.map((force: ForceData): string => force.uniqid)
 
 export default {
   title: 'local/organisms/PlanningAssets',
@@ -18,19 +25,33 @@ export default {
     readme: {
       // Show readme before story
       content: docs
+    },
+    options: {
+      // We have no addons enabled in this story, so the addon panel should be hidden
+      showPanel: true
+    },
+    controls: {
+      expanded: true
     }
   },
-  argTypes: {}
+  argTypes: {
+    playerForce: {
+      name: 'Player force',
+      defaultValue: forceIds[1],
+      control: {
+        type: 'select',
+        options: forceIds
+      }
+    }
+  }
 }
-
-const blueForce = P9Mock.data.forces.forces[1]
 
 const Template: Story<MessageListPropTypes> = (args) => {
   const { forces, playerForce, isUmpire, render, opFor } = args
-
+  const force = playerForce === UMPIRE_FORCE ? undefined : playerForce
   return <PlanningAssets
     forces={forces}
-    playerForce={playerForce}
+    playerForce={force}
     isUmpire={isUmpire}
     render={render}
     opFor={opFor}
@@ -39,7 +60,7 @@ const Template: Story<MessageListPropTypes> = (args) => {
 
 export const Default = Template.bind({})
 Default.args = {
-  forces: P9Mock.data.forces.forces,
+  forces: forces,
   playerForce: undefined,
   isUmpire: true,
   render: noop,
