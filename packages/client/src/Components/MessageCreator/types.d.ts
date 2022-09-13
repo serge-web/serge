37ad1a1b6652b0c6b8f17a95afbca2310ede9c2c
Ian Mayo <ian@planetmayo.com>
2022-09-13 10:04:48 +0100

    drop legacy components

--- packages/client/src/Components/MessageCreator/types.d.ts
+++
@@ -1,9 +0,0 @@
-interface Props {
-  schema: any,
-  curChannel: string,
-  privateMessage: string
-  onMessageSend?: (e: MouseEvent<HTMLButtonElement>) => void
-  onCancel?: (e: MouseEvent<HTMLButtonElement>) => void
-  confirmCancel?: boolean
-}
-export default Props
