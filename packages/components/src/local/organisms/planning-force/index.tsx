379bfcf033c2750a7e33bb5b94ca47e5135a2590
philb89 <everlasting2605@gmail.com>
2022-09-26 16:34:41 +0700

    1861: display assets icon (phase 2)

--- packages/components/src/local/organisms/planning-force/index.tsx
+++ packages/components/src/local/organisms/planning-force/index.tsx
@@ -1,31 +1,50 @@
 import L, { LatLng, latLng } from 'leaflet'
-import React from 'react'
+import React, { useCallback } from 'react'
 import { LayerGroup, Marker, Tooltip } from 'react-leaflet'
+import AssetIcon from '../../asset-icon'
+// import MarkerClusterGroup from 'react-leaflet-cluster'
+import cx from 'classnames'
+import * as ReactDOMServer from 'react-dom/server'
 import { AssetRow } from '../planning-assets/types/props'
+import styles from './styles.module.scss'
 import PropTypes from './types/props'
 
-export const PlanningForces: React.FC<PropTypes> = ({ assets, opFor, selectedItem }) => {
+export const PlanningForces: React.FC<PropTypes> = ({ assets, selectedItem }) => {
   // temporarily use alternate icon for opForces
-  const iconForThisForce = opFor ? 'layers.png' : 'marker-icon-2x.png'
+  // const iconForThisForce = opFor ? 'layers.png' : 'marker-icon-2x.png'
+
+  const getAssetIcon = useCallback((icon: string, isSelected: boolean, isDestroyed: boolean) => {
+    const [imageSrc, bgColor] = icon.split(',')
+    return (
+      ReactDOMServer.renderToString(<div className={cx({ [styles.iconBase]: true, [styles.selected]: isSelected })} style={{ backgroundColor: bgColor }}>
+        <AssetIcon imageSrc={imageSrc} destroyed={isDestroyed} isSelected={isSelected} />
+      </div>)
+    )
+  }, [])
+
   return <>
     {
-      assets.length > 0 && <LayerGroup key={'first-forces-layer'}>
+      assets.length > 0 &&
+      <LayerGroup key={'first-forces-layer'}>
         {
+          // <MarkerClusterGroup chunkedLoading>
+          //   {
           assets.map((asset: AssetRow, index: number) => {
             const loc: LatLng = asset.position ? asset.position : latLng([0, 0])
             const isSelected = asset.id === selectedItem
-            // override the marker if this is the selected asset
-            // TODO: apply some styling to the component
-            const marker = isSelected ? 'selected-marker.png' : iconForThisForce
+
             return <Marker
-              key={'asset-icon-' + index}
+              key={`asset-icon-${index}`}
               position={loc}
               icon={L.divIcon({
-                html: '<img src="./images/' + marker + '" style="width:20px;height:30px"/>'
+                iconSize: [30, 30],
+                html: getAssetIcon(asset.icon, isSelected, false)
               })} >
               <Tooltip>{asset.name}</Tooltip>
             </Marker>
           })
+          //   }
+          // </MarkerClusterGroup>
         }
       </LayerGroup >
     }
