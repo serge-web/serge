f59fcfacef92c7e2c3c0bf8db98a88fc61ade336
Ian Mayo <ian@planetmayo.com>
2022-09-30 14:13:23 +0100

    define content for geometries in orders, provide mock data

--- packages/mocks/planning-messages.mock.ts
+++ packages/mocks/planning-messages.mock.ts
@@ -1,5 +1,101 @@
 
-import { MessagePlanning, } from '@serge/custom-types'
+import { MessagePlanning, PlannedActivityGeometry, } from '@serge/custom-types'
+
+const sampleActivity: PlannedActivityGeometry[] = [
+  {
+    uniqid: 'aa4',
+    geometry: {
+      "type": "Feature",
+      "properties": {},
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [
+          [
+            135.0659,
+            -4.2149
+          ],
+          [
+            134.868,
+            -7.362
+          ],
+          [
+            137.636,
+            -12.404
+          ],
+          [
+            136.9228,
+            -15.1781
+          ]
+        ]
+      }
+    }
+  },  {
+    uniqid: 'aa5',
+    geometry: {
+      "type": "Feature",
+      "properties": {},
+      "geometry": {
+        "type": "Polygon",
+        "coordinates": [
+          [
+            [
+              136.9335,
+              -15.1993
+            ],
+            [
+              132.978,
+              -14.9872
+            ],
+            [
+              131.7480,
+              -18.375375
+            ],
+            [
+              136.64794,
+              -20.0972
+            ],
+            [
+              138.62548,
+              -17.39257
+            ],
+            [
+              136.933,
+              -15.1993
+            ]
+          ]
+        ]
+      }
+    }
+  },  {
+    uniqid: 'aa6',
+    geometry: {
+      "type": "Feature",
+      "properties": {},
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [
+          [
+            136.7138,
+            -15.26298
+          ],
+          [
+            136.0986,
+            -12.16822
+          ],
+          [
+            133.8574,
+            -7.73276
+          ],
+          [
+            134.8681,
+            -4.36832
+          ]
+        ]
+      }
+    }
+  }
+]
+
 
 export const planningMessages: MessagePlanning[] = [
   {
@@ -23,6 +119,7 @@ export const planningMessages: MessagePlanning[] = [
       Description: "More land operations",
       Location: "Region-A",
       Status: "Minor",
+      geometry: sampleActivity,
       title: "Operation Bravo-12",
       Assets: [
         {
@@ -49,7 +146,7 @@ export const planningMessages: MessagePlanning[] = [
     _rev: '2',
     hasBeenRead: false
   },
- 
+
   {
     "messageType": "PlanningMessage",
     "details": {
