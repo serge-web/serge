baf3aadbe4b60ff33f7fdf9f91f6a577eac773e8
philb89 <everlasting2605@gmail.com>
2022-10-03 23:51:57 +0700

    update LayerGroup

--- packages/components/src/local/organisms/support-mapping/index.tsx
+++ packages/components/src/local/organisms/support-mapping/index.tsx
@@ -1,52 +1,53 @@
 
 import 'leaflet/dist/leaflet.css'
-import { useMap } from 'react-leaflet-v4'
+import { ScaleControl, TileLayer, useMap } from 'react-leaflet-v4'
 import React, { useEffect } from 'react'
 import PropTypes from './types/props'
 import MapControl from '../../map-control'
+import { MapConstants } from './helper/MapConstants'
 
 export const SupportMapping: React.FC<PropTypes> = ({
   position, bounds,
-  actionItems, actionCallback, toolbarChildren, maxWidth
+  actionItems, actionCallback, toolbarChildren, maxWidth, children
 }) => {
+  const TileLayerProps = MapConstants.TileLayer
   const map = useMap()
 
   useEffect(() => {
-    if (map && map.invalidateSize) {
+    if (map) {
       map.invalidateSize()
-    }
-    // diagnostics:
-    if (map && map.invalidateSize === undefined) {
-      console.log('warning, poss wrong map element', map)
     }
   }, [maxWidth])
 
   useEffect(() => {
-    if ((bounds !== undefined) && map && map.flyToBounds) {
+    if (bounds && map) {
       map.flyToBounds(bounds, { duration: 0.6 })
     }
   }, [bounds])
 
   useEffect(() => {
-    if (position && map && map.flyTo) {
+    if (position && map) {
       const defaultZoom = 10
       map.flyTo(position, defaultZoom, { duration: 0.6 })
     }
   }, [position])
 
   return (
-    <MapControl
-      bounds={bounds}
-      zoomStepSize={1}
-      actionItems={actionItems}
-      mapVer='v4'
-      actionCallback={actionCallback}>
-      <>
-        {toolbarChildren &&
-          toolbarChildren
-        }
-      </>
-    </MapControl>
+    <>
+      <MapControl
+        bounds={bounds}
+        zoomStepSize={1}
+        actionItems={actionItems}
+        mapVer='v4'
+        actionCallback={actionCallback}>
+        <>
+          {toolbarChildren}
+        </>
+      </MapControl>
+      <TileLayer {...TileLayerProps} />
+      <ScaleControl position='topright' />
+      {children}
+    </>
   )
 }
 
