95c207494dc3dc5ecfec9efd1d25b5a951a42888
Ian Mayo <ian@planetmayo.com>
2022-10-12 18:08:47 +0100

    fix compiler error

--- packages/components/src/local/mapping/index.tsx
+++ packages/components/src/local/mapping/index.tsx
@@ -926,7 +926,7 @@ export const Mapping: React.FC<PropTypes> = ({
                 </Item>
               </div>}
               <CellLabelStyleSelector cellLabelStyle={cellLabelStyle} setCellLabelStyle={setCellLabelStyle} />
-              <ViewAs viewAsForce={viewAsForce} viewAsCallback={viewAsCallback} forces={playerForce === UMPIRE_FORCE ? forcesState : []} />
+              <ViewAs isUmpire={isUmpire} viewAsForce={viewAsForce} viewAsCallback={viewAsCallback} forces={playerForce === UMPIRE_FORCE ? forcesState : []} />
             </>
           </MapControl>
           {mappingConstraintState && mappingConstraintState.tileLayer &&
