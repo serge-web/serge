1d17e8035e6d5607c4631e15ece26701bc1912ec
philb89 <everlasting2605@gmail.com>
2022-10-15 21:48:36 +0700

    integrate leaflet control select

--- packages/components/typings.d.ts
+++ packages/components/typings.d.ts
@@ -1,3 +1,6 @@
+
+import * as Leaflet from 'leaflet';
+
 declare module '*.md' {
   const content: string
   export = content;
@@ -8,3 +11,25 @@ declare module '@json-editor/json-editor'
 // TODO: Remove the line below when the store package has been converted
 declare module '@serge/store'
 declare module 'whatwg-fetch'
+
+type SelectOption = {
+  position: 'topleft' | 'topright' | 'bottomleft' | 'bottomright',
+  items: any[],
+  multi?: boolean,
+  selectedDefault?: boolean,
+  onOpen?: () => void,
+  onClose?: () => void,
+  onSelect?: (item: any) => void,
+  onGroupOpen?: (groupItem: any) => void
+}
+
+class Select extends Control {
+  constructor(options?: SelectOption);
+  addTo(map: Map): this;
+}
+
+declare module 'leaflet' {
+  export namespace control {
+    function select(options?: SelectOption): Select;
+  }
+}
