31eafda8da4028f128a70ced1af922a2155a4f5c
Ian Mayo <ian@planetmayo.com>
2022-10-29 17:38:24 +0100

    pass selected orders to order renderer

--- packages/components/src/local/organisms/map-planning-orders/types/props.d.ts
+++ packages/components/src/local/organisms/map-planning-orders/types/props.d.ts
@@ -2,8 +2,10 @@ import { ForceData, MessagePlanning, PlanningActivity } from '@serge/custom-type
 import React from 'react'
 
 export default interface PropTypes {
-  /** the set of orders to display */
+  /** the full set of orders */
   orders: MessagePlanning[]
+  /** the currently selected set of ordres */
+  selectedOrders: string[]
   /** a reference list of plannign activiites (used to get color/styling) */
   activities: PlanningActivity[]
   /** handler for a set of orders being clicked */
