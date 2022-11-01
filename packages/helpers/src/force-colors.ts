2448d08736f4ed8922362d29e3eb75ec011435bb
Ian Mayo <ian@planetmayo.com>
2022-11-01 18:24:35 +0000

    introduce prop to allow umpire forces to be excluded

--- packages/helpers/src/force-colors.ts
+++ packages/helpers/src/force-colors.ts
@@ -1,9 +1,12 @@
 import { ForceData } from '@serge/custom-types'
 import { ForceStyle } from './is-perceived-by'
 
-/** produce an array of force ids & colors */
-export default function forceColors (allForces: ForceData[]): Array<ForceStyle> {
-  const forceColors: Array<ForceStyle> = allForces.map((force: ForceData) => {
+/** produce an array of force ids & colors
+ * optionally exclude umpire forces
+ */
+export default function forceColors (allForces: ForceData[], excludeUmpire?: boolean): Array<ForceStyle> {
+  const filtered = excludeUmpire ? allForces.filter((force) => !force.umpire) : allForces
+  const forceColors: Array<ForceStyle> = filtered.map((force: ForceData) => {
     return { forceId: force.uniqid, force: force.name, color: force.color }
   })
   return forceColors
