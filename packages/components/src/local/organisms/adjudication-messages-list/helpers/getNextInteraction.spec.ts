9fb5569bbc2ba7712322fd22202af3d7e91dd776
Ian Mayo <ian@planetmayo.com>
2022-11-05 12:45:32 +0000

    momre implementation

--- packages/components/src/local/organisms/adjudication-messages-list/helpers/getNextInteraction.spec.ts
+++ packages/components/src/local/organisms/adjudication-messages-list/helpers/getNextInteraction.spec.ts
@@ -1,9 +1,8 @@
-import { INTERACTION_MESSAGE, PLANNING_MESSAGE } from '@serge/config'
-import { ForceData, GroupedActivitySet, InteractionDetails, InteractionMessageStructure, MessageDetails, MessageDetailsFrom, MessageInteraction, MessagePlanning, PerForcePlanningActivitySet, PlanningActivity, Role } from '@serge/custom-types'
+import { PLANNING_MESSAGE } from '@serge/config'
+import { ForceData, GroupedActivitySet, MessageInteraction, MessagePlanning, PerForcePlanningActivitySet, PlanningActivity, Role } from '@serge/custom-types'
 import { MockPerForceActivities, MockPlanningActivities, P9Mock, planningMessagesBulk } from '@serge/mocks'
 import moment from 'moment'
-import { PlanningContact } from '../../support-panel/helpers/gen-order-data'
-import { getNextInteraction, timeOfLatestInteraction } from './getNextInteraction'
+import { getNextInteraction, interactionFor, timeOfLatestInteraction } from './getNextInteraction'
 
 const wargame = P9Mock.data
 const forces = wargame.forces.forces
@@ -45,68 +44,18 @@ const filledInPerForcePlanningActivities: PerForcePlanningActivitySet[] = perFor
 })
 
 const planningMessages2 = messages.filter(msg => msg.messageType === PLANNING_MESSAGE) as MessagePlanning[]
-const interactions = messages.filter(msg => msg.messageType === INTERACTION_MESSAGE) as MessageInteraction[]
-
-it('create an interaction', () => {
-  const inter = getNextInteraction(planningMessages2, filledInPerForcePlanningActivities, interactions)
-  // try the host platform in the task group
-  expect(inter).toBeTruthy()
-})
-
-const interactionFor = (contact: PlanningContact, selectedForce: ForceData, selectedRoleId: string, selectedRoleName: string, turnNumber: number, channelId: string, adjudicateTemplateId: string): MessageInteraction => {
-  const interDetails: InteractionDetails = {
-    orders1: contact.first.activity._id,
-    orders2: contact.second.activity._id,
-    startTime: moment(contact.timeStart).toISOString(),
-    endTime: moment(contact.timeEnd).toISOString(),
-    geometry: contact.intersection,
-    complete: false
-  }
-  const from: MessageDetailsFrom = {
-    force: selectedForce.uniqid,
-    forceId: selectedForce.uniqid,
-    forceColor: selectedForce.color,
-    iconURL: selectedForce.iconURL,
-    roleId: selectedRoleId,
-    roleName: selectedRoleName
-  }
-  const details: MessageDetails = {
-    channel: channelId,
-    from: from,
-    interaction: interDetails,
-    messageType: adjudicateTemplateId,
-    timestamp: moment().toISOString(),
-    turnNumber: turnNumber
-  }
-  const message: InteractionMessageStructure = {
-    Reference: '',
-    narrative: '',
-    perceptionOutcomes: [],
-    locationOutcomes: [],
-    healthOutcomes: []
-  }
-  const msgInter: MessageInteraction = {
-    details: details,
-    message: message,
-    _id: '',
-    _rev: '',
-    messageType: INTERACTION_MESSAGE
-  }
-  return msgInter
-}
 
 it('process successive interactions', () => {
   const interactions: MessageInteraction[] = []
   const results: Record<string, any> = []
-  for (let i = 0; i < 10; i++) {
-    const contact = getNextInteraction(planningMessages2, filledInPerForcePlanningActivities, interactions)
+  for (let ctr = 0; ctr < 2; ctr++) {
+    const contact = getNextInteraction(planningMessages2, filledInPerForcePlanningActivities, interactions, ctr)
     const lastInter = interactions.length && moment(timeOfLatestInteraction(interactions)).toISOString()
-    // TODO: - find out why we don't get contact
     expect(contact).toBeTruthy()
     if (contact) {
       const msgInter2: MessageInteraction = interactionFor(contact, forces[1], forces[1].roles[0].roleId, forces[1].roles[0].name, 4, 'channelId', 'adj-template')
       interactions.push(msgInter2)
-      results.push({ ctr: i, val: moment(contact.timeStart).toISOString(), last: lastInter })
+      results.push({ contactStart: moment(contact.timeStart).toISOString(), id: contact.id, latestInter: lastInter, count: interactions.length })
     }
   }
   console.table(results)
--- packages/components/src/local/organisms/adjudication-messages-list/helpers/getNextInteraction.ts
+++ packages/components/src/local/organisms/adjudication-messages-list/helpers/getNextInteraction.ts
@@ -1,7 +1,9 @@
-import { MessageInteraction, MessagePlanning, PerForcePlanningActivitySet, PlannedProps } from '@serge/custom-types'
+import { INTERACTION_MESSAGE } from '@serge/config'
+import { ForceData, InteractionDetails, InteractionMessageStructure, MessageDetails, MessageDetailsFrom, MessageInteraction, MessagePlanning, PerForcePlanningActivitySet, PlannedProps } from '@serge/custom-types'
+import { Feature, Geometry } from 'geojson'
 import _ from 'lodash'
 import moment from 'moment'
-import { findPlannedGeometries, findTouching, GeomWithOrders, injectTimes, invertMessages, ordersOverlappingTime, PlanningContact, putInBin, SpatialBin, spatialBinning } from '../../support-panel/helpers/gen-order-data'
+import { findPlannedGeometries, findTouching, injectTimes, invertMessages, ordersOverlappingTime, PlanningContact, putInBin, SpatialBin, spatialBinning } from '../../support-panel/helpers/gen-order-data'
 
 const useDate = (msg: MessageInteraction): string => {
   const inter = msg.details.interaction
@@ -9,6 +11,48 @@ const useDate = (msg: MessageInteraction): string => {
     throw Error('Interaction missing for message:' + msg.message.Reference)
   }
   return inter.startTime
+}
+
+export const interactionFor = (contact: PlanningContact, selectedForce: ForceData, selectedRoleId: string, selectedRoleName: string, turnNumber: number, channelId: string, adjudicateTemplateId: string): MessageInteraction => {
+  const interDetails: InteractionDetails = {
+    orders1: contact.first.activity._id,
+    orders2: contact.second.activity._id,
+    startTime: moment(contact.timeStart).toISOString(),
+    endTime: moment(contact.timeEnd).toISOString(),
+    geometry: contact.intersection,
+    complete: false
+  }
+  const from: MessageDetailsFrom = {
+    force: selectedForce.uniqid,
+    forceId: selectedForce.uniqid,
+    forceColor: selectedForce.color,
+    iconURL: selectedForce.iconURL,
+    roleId: selectedRoleId,
+    roleName: selectedRoleName
+  }
+  const details: MessageDetails = {
+    channel: channelId,
+    from: from,
+    interaction: interDetails,
+    messageType: adjudicateTemplateId,
+    timestamp: moment().toISOString(),
+    turnNumber: turnNumber
+  }
+  const message: InteractionMessageStructure = {
+    Reference: '',
+    narrative: '',
+    perceptionOutcomes: [],
+    locationOutcomes: [],
+    healthOutcomes: []
+  }
+  const msgInter: MessageInteraction = {
+    details: details,
+    message: message,
+    _id: '',
+    _rev: '',
+    messageType: INTERACTION_MESSAGE
+  }
+  return msgInter
 }
 
 export const timeOfLatestInteraction = (interactions: MessageInteraction[]): number => {
@@ -20,7 +64,7 @@ export const timeOfLatestInteraction = (interactions: MessageInteraction[]): num
   return moment(date).valueOf()
 }
 
-const timeOfFirstPlan = (orders: MessagePlanning[]): number => {
+const timeOfStartOfFirstPlan = (orders: MessagePlanning[]): number => {
   if (!orders.length) {
     throw Error('should not be called with zero length orders')
   }
@@ -31,55 +75,108 @@ const timeOfFirstPlan = (orders: MessagePlanning[]): number => {
   return sortFunc(sorted[0])
 }
 
+const timeOfEndOfLastPlan = (orders: MessagePlanning[]): number => {
+  if (!orders.length) {
+    throw Error('should not be called with zero length orders')
+  }
+  const reverseSortFunc = (order: MessagePlanning) => {
+    return -moment(order.message.endDate).valueOf()
+  }
+  const sortFunc = (order: MessagePlanning) => {
+    return moment(order.message.endDate).valueOf()
+  }
+  const sorted = _.sortBy(orders, reverseSortFunc)
+  return sortFunc(sorted[0])
+}
+
+const tStart = (geom: Feature<Geometry>): string => {
+  const props = geom.properties as PlannedProps
+  return 'Start:' + props.startDate
+}
+const tEnd = (geom: Feature<Geometry>): string => {
+  const props = geom.properties as PlannedProps
+  return 'End:' + props.endDate
+}
+
+const showGeom = (geom: Feature<Geometry>): string => {
+  const geoAny = geom.geometry as any
+  return 'Geom:' + geom.geometry.type + '-' + geoAny.coordinates
+}
+
 export const getNextInteraction = (orders: MessagePlanning[],
-  activities: PerForcePlanningActivitySet[], interactions: MessageInteraction[]): PlanningContact | undefined => {
-  // console.log('getting next interaction after', datetime, orders.length, activities.length, interactions.length)
+  activities: PerForcePlanningActivitySet[], interactions: MessageInteraction[], _ctr: number): PlanningContact | undefined => {
+  const earliestTime = interactions.length ? timeOfLatestInteraction(interactions) : timeOfStartOfFirstPlan(orders)
 
-  const latestInteraction = interactions.length ? timeOfLatestInteraction(interactions) : timeOfFirstPlan(orders)
-
-  const trimmedOrders = ordersOverlappingTime(orders, latestInteraction)
+  const trimmedOrders = ordersOverlappingTime(orders, earliestTime)
 
   const newGeometries = invertMessages(trimmedOrders, activities)
   const withTimes = injectTimes(newGeometries)
 
-  const startBeforeTime = (val: GeomWithOrders) => {
-    const props = val.geometry.properties as PlannedProps
-    return latestInteraction && props.startTime <= latestInteraction
-  }
-  const endAfterTime = (val: GeomWithOrders) => {
-    const props = val.geometry.properties as PlannedProps
-    return latestInteraction && props.endTime >= latestInteraction
-  }
+  const trimmedGeoms = withTimes // .filter((val) => startBeforeTime(val)).filter((val) => endAfterTime(val))
 
-  const trimmedGeoms = latestInteraction ? withTimes.filter((val) => startBeforeTime(val)).filter((val) => endAfterTime(val)) : withTimes
+  console.log('start get next. Interactions:', interactions.length, ' earliest:', moment(earliestTime).toString())
+  console.table(trimmedGeoms.map((val) => {
+    // return { id: val._id, start: val.message.startDate, end: val.message.endDate }
+    return { geometry: val.id, start: val.activity.message.startDate, end: val.activity.message.endDate }
+  }))
 
-  const geometriesInTimeWindow = findPlannedGeometries(trimmedGeoms, latestInteraction, 160)
-
-  //  console.log('geoms in window.', moment(latestInteraction).toISOString(), withTimes.length, geomsStartingBeforeTime.length, gemosFinishingAfterTime.length, geometriesInTimeWindow.length)
-  //  console.table(withTimes.map((value) => { return { id: value.id, time: value.geometry.properties && moment(value.geometry.properties.startTime).toISOString() } }))
-
-  // now do spatial binning
-  const bins = spatialBinning(geometriesInTimeWindow, 2)
-  const binnedOrders = putInBin(geometriesInTimeWindow, bins)
-
+  // calculate suitable window. First 10%?
+  const latestTime = timeOfEndOfLastPlan(orders)
+  const diffMins = moment.duration(moment(latestTime).diff(moment(earliestTime))).asMinutes()
+  let interactionWindow = Math.max(diffMins / 1000, 1)
+  console.log('window', moment(earliestTime).toString(), moment(latestTime).toString(), diffMins, interactionWindow)
   const contacts: PlanningContact[] = []
+  while (contacts.length === 0) {
+    const geometriesInTimeWindow = findPlannedGeometries(trimmedGeoms, earliestTime, interactionWindow)
 
-  const interactionsProcessed = interactions.map((val) => {
-    const inter = val.details.interaction
-    if (!inter) {
-      throw Error('Interaction missing')
-    }
-    return inter.orders1 + ' ' + inter.orders2
-  })
-  const interactionsConsidered: string[] = []
-  const interactionsTested: Record<string, PlanningContact | null> = {}
+    console.table(geometriesInTimeWindow.map((val) => {
+      // return { id: val._id, start: val.message.startDate, end: val.message.endDate }
+      return { geomInWindow: val.id, start: tStart(val.geometry), end: tEnd(val.geometry) }
+    }))
 
-  binnedOrders.forEach((bin: SpatialBin, _index: number) => {
-    const newContacts = findTouching(bin.orders, interactionsConsidered, interactionsProcessed,
-      interactionsTested)
-    //    console.log('bin', _index, bin.orders.length, newContacts.length, interactionsConsidered.length, interactionsProcessed.length, Object.keys(interactionsTested).length)
-    contacts.push(...newContacts)
-  })
+    console.log('trying window: ', interactionWindow, moment(earliestTime).toString(), trimmedOrders.length, geometriesInTimeWindow.length)
+
+    //  console.log('geoms in window.', moment(latestInteraction).toISOString(), withTimes.length, geomsStartingBeforeTime.length, gemosFinishingAfterTime.length, geometriesInTimeWindow.length)
+    //  console.table(withTimes.map((value) => { return { id: value.id, time: value.geometry.properties && moment(value.geometry.properties.startTime).toISOString() } }))
+
+    // now do spatial binning
+    const bins = spatialBinning(geometriesInTimeWindow, 2)
+    const binnedOrders = putInBin(geometriesInTimeWindow, bins)
+
+    const interactionsProcessed = interactions.map((val) => {
+      const inter = val.details.interaction
+      if (!inter) {
+        throw Error('Interaction missing')
+      }
+      return inter.orders1 + ' ' + inter.orders2
+    })
+    const interactionsConsidered: string[] = []
+    const interactionsTested: Record<string, PlanningContact | null> = {}
+
+    binnedOrders.forEach((bin: SpatialBin, _index: number) => {
+      const newContacts = findTouching(bin.orders, interactionsConsidered, interactionsProcessed,
+        interactionsTested)
+      //    console.log('bin', _index, bin.orders.length, newContacts.length, interactionsConsidered.length, interactionsProcessed.length, Object.keys(interactionsTested).length)
+      contacts.push(...newContacts)
+    })
+
+    interactionWindow *= 2
+  }
+
+  const showInteraction = (contacts: PlanningContact[]): string => {
+    const contact = contacts[0]
+    let res = contact.id
+    res += tStart(contact.first.geometry) + ' ' + tEnd(contact.first.geometry)
+    res += ' '
+    res += tStart(contact.second.geometry) + ' ' + tEnd(contact.second.geometry)
+    res += ' '
+    res += showGeom(contact.first.geometry)
+    res += ' '
+    res += showGeom(contact.second.geometry)
+    return res
+  }
+
+  console.log('contacts found', showInteraction(contacts))
 
   if (contacts.length) {
     // sort then
@@ -90,12 +187,6 @@ export const getNextInteraction = (orders: MessagePlanning[],
     // console.table(sortedContacts.map((value) => { return { id: value.id, time: moment(value.timeStart).toISOString() } }))
 
     const first = sortedContacts[0]
-
-    // CHANGE LOGIC:
-    // - drop irrelevant orders: one which start after current time or end before current time
-    // - get permutations
-    // - sort into ascending order (start time)
-    // - de
 
     return first
   } else {
