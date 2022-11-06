611458233ccd637699014fff0fdb2ab1f91a158a
Ian Mayo <ian@planetmayo.com>
2022-11-06 15:36:34 +0000

    switch to activity IDs in the template

--- packages/components/src/local/molecules/json-editor/index.stories-inc.tsx
+++ packages/components/src/local/molecules/json-editor/index.stories-inc.tsx
@@ -6,15 +6,18 @@ import JsonEditor from './index'
 import docs from './README.md'
 
 // Import mock
-import { messageDataCollaborativeEditing, messageDataCollaborativeResponding, MessageTemplatesMoskByTitle, P9Mock, planningMessages as planningChannelMessages, planningMessageTemplatesMock, WargameMock } from '@serge/mocks'
+import { messageDataCollaborativeEditing, messageDataCollaborativeResponding, MessageTemplatesMoskByTitle, MockPerForceActivities, MockPlanningActivities, P9Mock, planningMessages as planningChannelMessages, planningMessageTemplatesMock, WargameMock } from '@serge/mocks'
 import { Story } from '@storybook/react/types-6-0'
 
 import { PLANNING_MESSAGE } from '@serge/config'
-import { Asset, MessageInfoTypeClipped, MessageInteraction, MessagePlanning, MessageStructure } from '@serge/custom-types'
+import { Asset, GroupedActivitySet, MessageInfoTypeClipped, MessageInteraction, MessagePlanning, MessageStructure, PlanningActivity } from '@serge/custom-types'
 import { AssetRow } from 'src/local/organisms/planning-assets/types/props'
+import { fixPerForcePlanningActivities } from '../../organisms/planning-channel/helpers/collate-plans-helper'
 import Props from './types/props'
 
 const wrapper: React.FC = (storyFn: any) => <div style={{ height: '600px' }}>{storyFn()}</div>
+
+console.clear()
 
 export default {
   title: 'local/molecules/JsonEditor',
@@ -34,8 +37,7 @@ export default {
     }
   }
 }
-const storeNewValue = (value: { [property: string]: any }): void => {
-  console.log('store data', value)
+const storeNewValue = (_value: { [property: string]: any }): void => {
 }
 
 const template = MessageTemplatesMoskByTitle[messageDataCollaborativeEditing[0].details.messageType]
@@ -46,6 +48,7 @@ const templateMessageCreator = {
 }
 
 const Template: Story<Props> = ({ messageId, disabled, template, messageContent, modifyForEdit, customiseTemplate }) => {
+  console.log('cust template', customiseTemplate)
   return (
     <JsonEditor
       storeNewValue={storeNewValue}
@@ -90,24 +93,32 @@ const planningMessages = planningChannelMessages.filter((msg: MessageInteraction
 const landActivityTemplate = planningMessageTemplatesMock.find((template) => template.title === planningMessages[0].details.messageType)
 const landMessage = planningMessages[0]
 
-const customiseTemplate = (_document: MessageStructure | undefined, schema: Record<string, any>): Record<string, any> => {
-  const forces = P9Mock.data.forces.forces
-  const blueAssets = forces[1].assets ? forces[1].assets : []
-  const redAssets = forces[2].assets ? forces[2].assets : []
-  const toRow = (asset: Asset): AssetRow => {
-    const row: AssetRow = {
-      id: asset.uniqid,
-      icon: 'icon',
-      name: asset.name,
-      condition: asset.condition,
-      status: asset.status ? asset.status.state : 'unknown',
-      platformType: asset.platformTypeId
-    }
-    return row
-  }
-  const blueRows = blueAssets.map((asset) => toRow(asset))
-  const redRows = redAssets.map((asset) => toRow(asset))
+const planningActivities = MockPlanningActivities
+const perForcePlanningActivities = MockPerForceActivities
+const filledInPerForcePlanningActivities = fixPerForcePlanningActivities(perForcePlanningActivities, planningActivities)
+
+const customiseLandTemplate = (_document: MessageStructure | undefined, schema: Record<string, any>): Record<string, any> => {
   if (schema) {
+    const forces = P9Mock.data.forces.forces
+    const blueAssets = forces[1].assets ? forces[1].assets : []
+    const redAssets = forces[2].assets ? forces[2].assets : []
+    const toRow = (asset: Asset): AssetRow => {
+      const row: AssetRow = {
+        id: asset.uniqid,
+        icon: 'icon',
+        name: asset.name,
+        condition: asset.condition,
+        status: asset.status ? asset.status.state : 'unknown',
+        platformType: asset.platformTypeId
+      }
+      return row
+    }
+    const blueRows = blueAssets.map((asset) => toRow(asset))
+    const redRows = redAssets.map((asset) => toRow(asset))
+  
+    // and the activities
+    const isBlue = _document && _document.Reference.includes('Blue')
+  
     const oldOwnAssets = schema.properties?.ownAssets?.items?.properties?.asset?.enum
     if (oldOwnAssets) {
       schema.properties.ownAssets.items.properties.asset.enum = blueRows.map((asset: AssetRow) => asset.id)
@@ -118,16 +129,37 @@ const customiseTemplate = (_document: MessageStructure | undefined, schema: Reco
       schema.properties.otherAssets.items.enum = redRows.map((asset: AssetRow) => asset.id)
       schema.properties.otherAssets.items.options.enum_titles = redRows.map((asset: AssetRow) => asset.name)
     }
+    const activities = schema.properties?.activity
+    console.log('activities', activities)
+    if (activities) {
+      const forceActivities = isBlue ? filledInPerForcePlanningActivities[0] : filledInPerForcePlanningActivities[1]
+      const acts: Array<{id: string, name: string}> = []
+      forceActivities.groupedActivities.map((val: GroupedActivitySet) => {
+        val.activities.forEach((val2: string | PlanningActivity) => {
+          if (typeof(val) === 'string') {
+            throw Error('Should not have string in planning activities')
+          }
+          const plan = val2 as PlanningActivity
+          acts.push({id: plan.uniqid, name: val.category + '-' + plan.name})
+        })
+      })
+      activities.enum = acts.map((val) => val.id)
+      activities.options.enum_titles = acts.map((val) => val.name)
+    }
   }
+
+
   return schema
 }
 
+console.log('land message', landMessage.message)
+
 export const PlanningMessage = Template.bind({})
 PlanningMessage.args = {
+  customiseTemplate: customiseLandTemplate,
   template: landActivityTemplate,
   messageContent: landMessage.message,
   messageId: 'id_2b',
   disabled: false,
-  gameDate: WargameMock.data.overview.gameDate,
-  customiseTemplate: customiseTemplate
+  gameDate: WargameMock.data.overview.gameDate
 }
--- packages/components/src/local/organisms/support-panel/helpers/customise-template.ts
+++ packages/components/src/local/organisms/support-panel/helpers/customise-template.ts
@@ -1,18 +1,38 @@
-import { MessageStructure } from '@serge/custom-types'
+import { GroupedActivitySet, MessageStructure, PerForcePlanningActivitySet, PlanningActivity } from '@serge/custom-types'
 import { AssetRow } from '../../planning-assets/types/props'
 
-export const customiseTemplate = (_document: MessageStructure | undefined, schema: Record<string, any>, ownAssets: AssetRow[], otherAssets: AssetRow[]): Record<string, any> => {
+export const customiseTemplate = (_document: MessageStructure | undefined, schema: Record<string, any>, 
+  ownAssets: AssetRow[], otherAssets: AssetRow[], activities?: PerForcePlanningActivitySet[]): Record<string, any> => {
   if (schema) {
-    const oldOwnAssets = schema.properties?.ownAssets?.items?.properties?.asset?.enum
+    const oldOwnAssets = schema.properties?.ownAssets?.items?.properties?.asset
     if (oldOwnAssets) {
-      schema.properties.ownAssets.items.properties.asset.enum = ownAssets.map((asset: AssetRow) => asset.id)
-      schema.properties.ownAssets.items.properties.asset.options.enum_titles = ownAssets.map((asset: AssetRow) => asset.name)
+      oldOwnAssets.enum = ownAssets.map((asset: AssetRow) => asset.id)
+      oldOwnAssets.options.enum_titles = ownAssets.map((asset: AssetRow) => asset.name)
     }
-    const oldOwnTargets = schema.properties?.otherAssets?.items?.enum
+    const oldOwnTargets = schema.properties?.otherAssets?.items
     if (oldOwnTargets) {
-      schema.properties.otherAssets.items.enum = otherAssets.map((asset: AssetRow) => asset.id)
-      schema.properties.otherAssets.items.options.enum_titles = otherAssets.map((asset: AssetRow) => asset.name)
+      oldOwnTargets.enum = otherAssets.map((asset: AssetRow) => asset.id)
+      oldOwnTargets.options.enum_titles = otherAssets.map((asset: AssetRow) => asset.name)
     }
+    const oldActivity = schema.properties?.activity?.items
+    if (oldActivity && activities) {
+      const isBlue = _document && _document.Reference && _document.Reference.includes('Blue')
+      const myActivities = isBlue ? activities[0] : activities[1]
+  
+      const acts: Array<{id: string, name: string}> = []
+      myActivities.groupedActivities.map((val: GroupedActivitySet) => {
+        val.activities.forEach((val2: string | PlanningActivity) => {
+          if (typeof(val) === 'string') {
+            throw Error('Should not have string in planning activities')
+          }
+          const plan = val2 as PlanningActivity
+          acts.push({id: plan.uniqid, name: val.category + '-' + plan.name})
+        })
+      })
+      oldActivity.enum = acts.map((val) => val.id)
+      oldActivity.options.enum_titles = acts.map((val) => val.name)
+    }
+
   }
   console.log('returning', schema)
   return schema
