e55e90b420f0b90cc75cc2ccb3ca075bd8958046
Ian Mayo <ian@planetmayo.com>
2022-11-07 09:18:26 +0000

    improve docs

--- packages/components/src/local/organisms/planning-channel/helpers/collate-plans-helper.ts
+++ packages/components/src/local/organisms/planning-channel/helpers/collate-plans-helper.ts
@@ -1,6 +1,11 @@
 import { GroupedActivitySet, PerForcePlanningActivitySet, PlanningActivity } from '@serge/custom-types'
 import { deepCopy } from '@serge/helpers'
 
+/** as a "time-saver" in generating activities, it's possible to record per force activity
+ * sets using the id's of activities instead of the whole activity.
+ * This method swaps the strings for the actual activities.  It also injects the
+ * category (environment) into the id, to make them all unique
+ */
 export const fixPerForcePlanningActivities = (perForcePlanningActivities: PerForcePlanningActivitySet[],
   planningActivities: PlanningActivity[]): PerForcePlanningActivitySet[] => {
   return perForcePlanningActivities.map((force: PerForcePlanningActivitySet): PerForcePlanningActivitySet => {
@@ -15,9 +20,6 @@ export const fixPerForcePlanningActivities = (perForcePlanningActivities: PerFor
               const activity = deepCopy(planningActivities.find((act: PlanningActivity) => act.uniqid === actId)) as PlanningActivity
               // inject the category
               activity.uniqid = group.category.toLowerCase() + '-' + activity.uniqid
-              if (!activity) {
-                throw Error('Planning activity not found:' + actId)
-              }
               return activity
             } else {
               return act
