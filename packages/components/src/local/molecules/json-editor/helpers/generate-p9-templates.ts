8b41d2a016e0f4029b4c05d9d6346c61eba54cb1
Ian Mayo <ian@planetmayo.com>
2022-11-12 16:12:45 +0000

    auto generate required fields

--- packages/components/src/local/molecules/json-editor/helpers/generate-p9-templates.ts
+++ packages/components/src/local/molecules/json-editor/helpers/generate-p9-templates.ts
@@ -1,16 +1,10 @@
 import moment from 'moment'
 
 export const generateTemplate = (title: string, dataStruct: Record<string, any>): Record<string, any> => {
+  const fieldNames = Object.keys(dataStruct).map((name: string) => name)
   const wrappedDetails = {
     format: 'grid',
-    required: [
-      'title',
-      'description',
-      'startDate',
-      'endDate',
-      'ownAssets',
-      'otherAssets'
-    ],
+    required: fieldNames,
     properties: dataStruct,
     title: 'Land Activity',
     type: 'object'
