5ec26282b477320cbb497dfb977d4d01bde7ba02
Ian Mayo <ian@planetmayo.com>
2022-11-14 18:19:10 +0000

    implement adjudication

---
+++ packages/helpers/src/handle-adjudication-outcomes.ts
@@ -0,0 +1,42 @@
+import { ForceData, MessageAdjudicationOutcomes, Perception } from '@serge/custom-types'
+import { findAsset } from '@serge/helpers'
+
+/** modify the forces object to apply the adjudication outcomes */
+export default (payload: MessageAdjudicationOutcomes, allForces: ForceData[]): ForceData[] => {
+  payload.health.forEach((health) => {
+    const asset = findAsset(allForces, health.asset)
+    asset.health = health.health
+  })
+
+  payload.movement.forEach((movement) => {
+    const asset = findAsset(allForces, movement.asset)
+    asset.location = movement.location
+  })
+
+  payload.perception.forEach((perception) => {
+    const asset = findAsset(allForces, perception.asset)
+    const by = perception.force
+    // drop the existing perception
+    asset.perceptions = asset.perceptions.filter((percept) => percept.by !== by)
+
+    // generate new perception
+    const res: Perception = {
+      by: by
+    }
+    if (perception.perceivedForce) {
+      res.force = perception.perceivedForce
+    }
+    if (perception.perceivedHealth) {
+      res.health = perception.perceivedHealth
+    }
+    if (perception.perceivedType) {
+      res.typeId = perception.perceivedType
+    }
+    if (perception.perceivedName) {
+      res.name = perception.perceivedName
+    }
+    // store the perception
+    asset.perceptions.push(res)
+  })
+  return allForces
+}
---
+++ packages/helpers/src/tests/handle-adjudication-outcomes.test.ts
@@ -0,0 +1,92 @@
+import { expect, it } from '@jest/globals'
+import { ForceData, MessageAdjudicationOutcomes } from '@serge/custom-types'
+import { deepCopy, findAsset } from '@serge/helpers'
+import handleAdjudicationOutcomes from '../handle-adjudication-outcomes'
+
+const allForces: ForceData[] = [
+  {
+    name: 'f-Blue',
+    assets: [
+      {
+        uniqid: 'alpha',
+        contactId: 'C1334',
+        platformTypeId: 'p12',
+        condition: 'some-cond',
+        name: 'alpha',
+        perceptions: [{
+          by: 'f-Red',
+          force: 'Blue',
+          typeId: 'frigate'
+        }, {
+          by: 'f-Green',
+          force: 'f-Red',
+          health: 33
+        }]
+      },
+      {
+        uniqid: 'bravo',
+        name: 'Big Brravo',
+        contactId: 'C154',
+        platformTypeId: 'p12',
+        condition: 'some-cond',
+        perceptions: []
+      }
+    ],
+    uniqid: 'id2',
+    overview: '',
+    roles: [],
+    iconURL: '',
+    color: '',
+    umpire: false,
+    dirty: false
+  }
+]
+
+const emptyPayload: MessageAdjudicationOutcomes = {
+  reference: 'umpire-234',
+  health: [],
+  perception: [],
+  movement: []
+}
+
+it('empty lists results in no change', () => {
+  const forces = deepCopy(allForces)
+  const beforeStr = JSON.stringify(forces)
+  const updated = handleAdjudicationOutcomes(emptyPayload, forces)
+  const afterStr = JSON.stringify(updated)
+  expect(beforeStr).toEqual(afterStr)
+})
+
+const validPayload: MessageAdjudicationOutcomes = {
+  reference: 'umpire-234',
+  health: [{ asset: 'alpha', health: 34 }],
+  perception: [
+    { force: 'f-Red', asset: 'alpha', perceivedHealth: 22, perceivedName: 'alfred', perceivedForce: 'f-Green' },
+    { force: 'f-Green', asset: 'bravo', perceivedForce: 'f-Orange' }
+  ],
+  movement: []
+}
+
+it('correctly updates perception with perception entry not present', () => {
+  const forces = deepCopy(allForces)
+  const beforeStr = JSON.stringify(forces)
+  const updated = handleAdjudicationOutcomes(validPayload, forces)
+  const afterStr = JSON.stringify(updated)
+  expect(beforeStr).not.toEqual(afterStr)
+  const one = findAsset(updated, 'alpha')
+  expect(one.perceptions).toBeTruthy()
+  expect(one.perceptions.length).toEqual(2)
+  expect(one.perceptions[1].by).toEqual('f-Red')
+  expect(one.perceptions[1].name).toEqual('alfred')
+  expect(one.perceptions[1].typeId).toBeUndefined()
+  expect(one.perceptions[1].force).toEqual('f-Green')
+  expect(one.perceptions[1].health).toEqual(22)
+  const two = findAsset(updated, 'bravo')
+  expect(two.perceptions).toBeTruthy()
+  expect(two.perceptions.length).toEqual(1)
+  expect(two.perceptions[0].by).toEqual('f-Green')
+  expect(two.perceptions[0].force).toEqual('f-Orange')
+  expect(two.perceptions[0].name).toBeUndefined()
+  expect(two.perceptions[0].typeId).toBeUndefined()
+  expect(two.perceptions[0].health).toBeUndefined()
+})
