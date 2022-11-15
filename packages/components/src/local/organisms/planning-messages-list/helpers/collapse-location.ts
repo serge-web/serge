45f1e5fa08fe9fa1c02a05571348a2e248885808
Ian Mayo <ian@planetmayo.com>
2022-11-15 20:08:26 +0000

    don't try to collapse it twice

--- packages/components/src/local/organisms/planning-messages-list/helpers/collapse-location.ts
+++ packages/components/src/local/organisms/planning-messages-list/helpers/collapse-location.ts
@@ -6,36 +6,39 @@ export const collapseLocation = (document: Record<string, any>, activities?: Per
   const res = deepCopy(document)
   const parent = res.message || res
   if (parent && parent.location) {
-    const loc = parent.location as PlannedActivityGeometry[]
-    let str = ''
-    let flatGeoms: PlanningActivityGeometry[] = []
-    if (!activities) {
-      console.warn('collapseLocation called without force activites')
-    } else {
-      // get the lists of activities
-      const justActs = activities.groupedActivities.map((group) => group.activities)
-      const flatActs = _.flatten(justActs) as PlanningActivity[]
-      // get the activities with geometries
-      const withGeoms = flatActs.filter((act) => act.geometries)
-      // now extract the geometries
-      const geoms = withGeoms.map((act) => act.geometries)
-      flatGeoms = _.flatten(geoms) as PlanningActivityGeometry[]
-    }
-    loc.forEach((geom) => {
-      let name = geom.uniqid
-      // now find this geometry
-      if (flatGeoms.length > 0) {
-        const theAct = flatGeoms.find((act) => act.uniqid === geom.uniqid)
-        if (theAct) {
-          name = theAct.name
-        } else {
-          console.warn('failed to find activity for', name)
-        }
+    if (typeof(parent.location) !== 'string') {
+      const loc = parent.location as PlannedActivityGeometry[]
+      let str = ''
+      let flatGeoms: PlanningActivityGeometry[] = []
+      if (!activities) {
+        console.warn('collapseLocation called without force activites')
+      } else {
+        // get the lists of activities
+        const justActs = activities.groupedActivities.map((group) => group.activities)
+        const flatActs = _.flatten(justActs) as PlanningActivity[]
+        // get the activities with geometries
+        const withGeoms = flatActs.filter((act) => act.geometries)
+        // now extract the geometries
+        const geoms = withGeoms.map((act) => act.geometries)
+        flatGeoms = _.flatten(geoms) as PlanningActivityGeometry[]
       }
-      str = str + '* ' + name + '\n'
-    })
-    parent.location = str
-    parent.hiddenStore = JSON.stringify(loc)
+      loc.forEach((geom) => {
+        let name = geom.uniqid
+        // now find this geometry
+        if (flatGeoms.length > 0) {
+          const theAct = flatGeoms.find((act) => act.uniqid === geom.uniqid)
+          if (theAct) {
+            name = theAct.name
+          } else {
+            console.warn('failed to find activity for', name)
+          }
+        }
+        str = str + '* ' + name + '\n'
+      })
+      parent.location = str
+      parent.hiddenStore = JSON.stringify(loc)
+    
+    }
   }
   return res
 }
