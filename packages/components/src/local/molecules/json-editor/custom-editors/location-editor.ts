063b9353eeb202ca39064fcaad45a84f6d5c4ad8
philb89 <everlasting2605@gmail.com>
2022-11-18 12:36:39 +0700

    [WIP] handle onChange event

--- packages/components/src/local/molecules/json-editor/custom-editors/location-editor.ts
+++ packages/components/src/local/molecules/json-editor/custom-editors/location-editor.ts
@@ -1,46 +1,64 @@
-/* eslint-disable @typescript-eslint/no-this-alias */
 import JSONEditor from '@json-editor/json-editor'
 
 export const initLocationEditor = (editCallback: () => void): void => {
   JSONEditor.defaults.editors.location = JSONEditor.AbstractEditor.extend({
     build: function () {
-      const self = this
+      this.group = document.createElement('div')
+      this.group.classList.add('form-group')
+      this.container.appendChild(this.group)
 
-      self.group = document.createElement('div')
-      self.group.classList.add('form-group')
-      self.container.appendChild(self.group)
+      this.label = document.createElement('label')
+      this.label.classList.add('control-label')
+      this.label.innerText = this.schema.title ? this.schema.title : ''
+      this.group.appendChild(this.label)
 
-      self.label = document.createElement('label')
-      self.label.classList.add('control-label')
-      self.label.innerText = self.schema.title ? self.schema.title : ''
-      self.group.appendChild(self.label)
+      this.textArea = document.createElement('textarea')
+      this.textArea.name = this.formname
+      this.textArea.classList.add('form-control')
+      this.textArea.addEventListener('change', (e: any) => {
+        e.preventDefault()
+        e.stopPropagation()
+        this.onChange(e.currentTarget.value)
+        if (e.currentTarget.value) {
+          this.addEditButton()
+        } else {
+          console.log('xx> remove edit button')
+          this.removeEditButton()
+        }
+      })
 
-      self.textArea = document.createElement('textarea')
-      self.textArea.name = self.formname
-      self.textArea.classList.add('form-control')
+      this.groupTextArea = document.createElement('div')
+      this.groupTextArea.classList.add('location-editor')
+      this.groupTextArea.appendChild(this.textArea)
 
-      self.groupTextArea = document.createElement('div')
-      self.groupTextArea.classList.add('location-editor')
-      self.groupTextArea.appendChild(self.textArea)
-
-      if (self.schema.readOnly || self.schema.readonly || self.schema.template) {
+      if (this.schema.readOnly || this.schema.readonly || this.schema.template) {
         this.disable(true)
         this.textArea.setAttribute('readonly', 'true')
-      } else {
-        self.editButton = document.createElement('button')
-        self.editButton.innerText = 'Edit'
-        self.editButton.name = 'editInLocation'
-        self.editButton.classList.add('btn', 'btn-secondary', 'json-editor-btn-add', 'json-editor-btntype-add')
-        self.editButton.style.height = '40px'
-        self.editButton.style.transform = 'translateX(-60px)'
-        self.editButton.addEventListener('click', () => editCallback())
-        self.groupTextArea.appendChild(self.editButton)
       }
-      self.group.appendChild(self.groupTextArea)
+      this.group.appendChild(this.groupTextArea)
+    },
+
+    addEditButton: function () {
+      if (this.groupTextArea.childNodes.length < 2) {
+        this.editButton = document.createElement('button')
+        this.editButton.innerText = 'Edit'
+        this.editButton.classList.add('btn', 'btn-secondary', 'json-editor-btn-add', 'json-editor-btntype-add')
+        this.editButton.addEventListener('click', () => editCallback())
+        this.groupTextArea.appendChild(this.editButton)
+      }
+    },
+
+    removeEditButton: function () {
+      if (this.groupTextArea.childNodes.length === 2) {
+        this.groupTextArea.removeChild(this.editButton)
+      }
     },
 
     setValue: function (value: string) {
-      this.textArea.innerHTML = value
+      this.textArea.value = value
+      if (value) {
+        this.addEditButton()
+      }
     },
 
     getValue: function () {
@@ -49,8 +67,9 @@ export const initLocationEditor = (editCallback: () => void): void => {
   })
 
   JSONEditor.defaults.resolvers.unshift((schema: any) => {
-    if (schema.format === 'customLocation' && schema.type === 'location') {
+    if (schema.format === 'location' && schema.type === 'string') {
       return 'location'
     }
+    return schema
   })
 }
--- packages/components/src/local/molecules/json-editor/helpers/setupEditor.ts
+++ packages/components/src/local/molecules/json-editor/helpers/setupEditor.ts
@@ -23,16 +23,47 @@ const setupEditor = (editor: Editor | null, schema: any, ref: RefObject<HTMLDivE
   const disableArrayDeleteLastRow = 'disable_array_delete_last_row'
   const promptBeforeDelete = 'prompt_before_delete'
   if (schema && schema.type && ref.current) {
+    // if (schema.properties.location) {
+    //   setTimeout(() => {
+    //     const hiddenStores = document.querySelectorAll('div[data-schemapath$="hiddenStore"]') as NodeListOf<HTMLDivElement>
+    //     Array.from(hiddenStores).forEach(hiddenStores => {
+    //       hiddenStores.style.display = 'none'
+    //     })
+
+    //     const locationSections = document.querySelectorAll('div[data-schemaid="locationArea"]') as NodeListOf<HTMLDivElement>
+    //     Array.from(locationSections).forEach(locationSection => {
+    //       // do not manipuate processed textarea
+    //       if (locationSection.childNodes.length === 3) {
+    //         return
+    //       }
+
+    //       locationSection.style.display = 'flex'
+    //       locationSection.style.alignItems = 'center'
+
+    //       const textArea = locationSection.firstChild as HTMLDivElement
+    //       textArea.style.width = '30%'
+
+    //       const editButton = document.createElement('button')
+    //       editButton.innerText = 'Edit'
+    //       editButton.name = 'editInLocation'
+    //       editButton.classList.add('btn', 'btn-secondary', 'json-editor-btn-add', 'json-editor-btntype-add')
+    //       editButton.style.height = '40px'
+    //       editButton.style.transform = 'translateX(-60px)'
+    //       editButton.addEventListener('click', () => editCallback())
+    //       locationSection.appendChild(editButton)
+    //     })
+    //   })
+
     // test custom editor
     schema.properties.location = {
-      format: 'customLocation',
+      format: 'location',
       options: {
         grid_columns: 6
       },
       propertyOrder: 55,
       // readonly: 'readonly',
       title: 'Location',
-      type: 'location'
+      type: 'string'
     }
 
     const newEditor = new JSONEditor(ref.current, {
