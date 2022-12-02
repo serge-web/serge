858892605e2670ab8364baab49b9f98a60d59e26
Ian Mayo <ian@planetmayo.com>
2022-12-02 11:32:54 +0000

    Introduce new button icon

--- packages/components/src/local/atoms/button/types/props.d.ts
+++ packages/components/src/local/atoms/button/types/props.d.ts
@@ -1,6 +1,6 @@
 import { ButtonProps } from '@material-ui/core/Button'
 
-type Icons = 'add' | 'save' | 'delete' | 'edit' | 'copy'
+type Icons = 'add' | 'save' | 'delete' | 'edit' | 'copy' | 'functions'
 export type Props = ButtonProps & {
   /**
    * Button custom color
