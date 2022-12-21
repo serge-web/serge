a37834c6d38671b0dbac51c8f7bfa04ed39d592e
Ian Mayo <ian@planetmayo.com>
2022-12-21 18:33:00 +0000

    allow for table of targets

--- packages/components/src/local/organisms/support-panel/helpers/customise-assets.ts
+++ packages/components/src/local/organisms/support-panel/helpers/customise-assets.ts
@@ -11,8 +11,18 @@ export const customiseAssets = (_document: MessageStructure | undefined, schema:
     }
     const oldOwnTargets = schema.properties?.otherAssets?.items
     if (oldOwnTargets) {
-      oldOwnTargets.enum = otherAssets.map((asset: AssetRow) => asset.id)
-      oldOwnTargets.options.enum_titles = otherAssets.map((asset: AssetRow) => asset.name)
+      // this may be a plain list of assets, or it may be a table of assets (like above)
+      if (oldOwnTargets.properties) {
+        // list of targets
+        const targetsTable = oldOwnTargets.properties.asset
+        if (targetsTable) {
+          targetsTable.enum = otherAssets.map((asset: AssetRow) => asset.id)
+          targetsTable.options.enum_titles = otherAssets.map((asset: AssetRow) => asset.name)
+        }
+          } else {
+        oldOwnTargets.enum = otherAssets.map((asset: AssetRow) => asset.id)
+        oldOwnTargets.options.enum_titles = otherAssets.map((asset: AssetRow) => asset.name)  
+      }
     }
   }
   return schema
