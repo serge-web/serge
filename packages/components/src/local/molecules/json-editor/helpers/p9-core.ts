4f895a3b982bdcf98b09e79ea1130a040af193ed
Ian Mayo <ian@planetmayo.com>
2023-01-04 09:39:51 +0000

    switch opfor to being array of entities

--- packages/components/src/local/molecules/json-editor/helpers/p9-core.ts
+++ packages/components/src/local/molecules/json-editor/helpers/p9-core.ts
@@ -138,21 +138,33 @@ export const coreTemplate = {
       disable_array_reorder: true
     },
     items: {
+      type: 'object',
+      format: 'grid',
       title: 'Asset',
-      type: 'string',
-      enum: [
-        'ra',
-        'rb',
-        'rc',
-        'da'
+      required: [
+        'asset',
+        'number'
       ],
-      options: {
-        enum_titles: [
-          'Boat A',
-          'Bike B',
-          'Band C',
-          'Plane A'
-        ]
+      properties: {
+        asset: {
+          title: 'Target',
+          type: 'string',
+          enum: [
+            'ta',
+            'tb',
+            'tc',
+            'pa'
+          ],
+          options: {
+            grid_columns: 2,
+            enum_titles: [
+              'Opp Tank A',
+              'Opp Tank B',
+              'Opp Tank C',
+              'Opp Plane A'
+            ]
+          }
+        }
       }
     }
   },
