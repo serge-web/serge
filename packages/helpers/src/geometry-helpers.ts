5f011d938c90abf0786f04bafbd85bfd8d3febb8
Ian Mayo <ian@planetmayo.com>
2023-01-05 13:24:30 +0000

    new helper class

---
+++ packages/helpers/src/geometry-helpers.ts
@@ -0,0 +1,7 @@
+import { PlanningActivityGeometry } from '@serge/custom-types'
+
+export const updateGeometryTimings = (geometries: PlanningActivityGeometry[], startTime: string, endTime: string): PlanningActivityGeometry[] => {
+  console.log(geometries, geometries)
+  !7 && console.log(startTime, endTime)
+  return geometries
+}
