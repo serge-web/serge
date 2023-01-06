3023a1363e201420e194bc9558fa51d7ab14bec0
Ian Mayo <ian@planetmayo.com>
2023-01-06 18:59:18 +0000

    revert changes

--- packages/components/src/local/organisms/planning-force/index.tsx
+++ packages/components/src/local/organisms/planning-force/index.tsx
@@ -2,15 +2,13 @@ import cx from 'classnames'
 import L, { LatLng, latLng } from 'leaflet'
 import React, { useCallback } from 'react'
 import * as ReactDOMServer from 'react-dom/server'
-import { Circle } from 'react-leaflet'
 import { LayerGroup, Marker, Tooltip } from 'react-leaflet-v4'
 import AssetIcon from '../../asset-icon'
 import { AssetRow } from '../planning-assets/types/props'
 import styles from './styles.module.scss'
 import PropTypes from './types/props'
 
-export const PlanningForces: React.FC<PropTypes> = ({ assets, selectedAssets, setSelectedAssets, interactive, opFor }) => {
-
+export const PlanningForces: React.FC<PropTypes> = ({ assets, selectedAssets, setSelectedAssets, interactive }) => {
   const getAssetIcon = (asset: AssetRow, isSelected: boolean, isDestroyed: boolean): string => {
     const [imageSrc, bgColor] = asset.icon.split(',')
     /** note: we only fill in the background for icons that require shading.  The NATO assets,
@@ -34,20 +32,6 @@ export const PlanningForces: React.FC<PropTypes> = ({ assets, selectedAssets, se
     }
     setSelectedAssets([...selectedAssets])
   }
-
-
-  const getRangeRing = useCallback((asset: AssetRow, index: number) => {
-    const attrs = asset.attributes
-    const mezRange = attrs['a_Mez_Range']
-    if (mezRange) {
-      console.log('range ring', asset.name, mezRange)
-      const centre = asset.position ? asset.position : latLng([0, 0])
-      const rad = mezRange * 1000
-      return <Circle center={centre} id={index} radius={rad} />
-    } else {
-      return undefined
-    }
-  }, [selectedAssets])
 
   const getMarkerOption = useCallback((asset: AssetRow, index: number) => {
     const loc: LatLng = asset.position ? asset.position : latLng([0, 0])
@@ -79,30 +63,17 @@ export const PlanningForces: React.FC<PropTypes> = ({ assets, selectedAssets, se
           interactive &&
           assets.map((asset: AssetRow, index: number) => {
             const markerOption = getMarkerOption(asset, index)
-            const rangeRing = getRangeRing(asset, index)
-            if (rangeRing) {
-              return <>
-              { rangeRing }
-              <Marker
+            return <Marker
               {...markerOption}
             >
               <Tooltip>{asset.name}</Tooltip>
             </Marker>
-              </>
-            } else {
-              return <Marker
-              {...markerOption}
-            >
-              <Tooltip>{asset.name}</Tooltip>
-            </Marker>
-            }
           })
         }
         {
           !interactive &&
           assets.map((asset: AssetRow, index: number) => {
             const markerOption = getMarkerOption(asset, index)
-
             return <Marker
               pmIgnore
               interactive={false}
