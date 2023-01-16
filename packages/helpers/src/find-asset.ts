3f56a5f3be9737b727ec05d2ba130888fee03699
Ian Mayo <ian@planetmayo.com>
2023-01-16 20:10:29 +0000

    introduce new helper, to find force AND asset for a asset-id

--- packages/helpers/src/find-asset.ts
+++ packages/helpers/src/find-asset.ts
@@ -27,6 +27,30 @@ export const isAsset = (asset: Asset, assetId: string | undefined, assetName?: s
   }
 }
 
+/** find the asset with the provided id, and it's parent force */
+export const findForceAndAsset = (allForces: ForceData[], assetId: string): {force: ForceData, asset: Asset} => {
+  let res: {force: ForceData, asset: Asset} | undefined
+  allForces.find((force) => {
+    const assets: Asset[] | undefined = force.assets
+    if (Array.isArray(assets)) {
+      assets.find(asset => {
+        const match = isAsset(asset, assetId)
+        if (match) {
+          res = { force: force, asset: match }
+        }
+        return !!res
+      })
+      // if the above find works, we'll return true, which will
+      // terminate the find process. If it returns undefined,
+      // we'll return false, and carry on to the next force
+      return !!res
+    }
+    return false
+  })
+  if (res !== undefined) return res
+  throw new Error('Asset not found from id:' + assetId)
+}
+
 /** find the asset with the provided id */
 export default (allForces: ForceData[], assetId: string | undefined, assetName?: string): Asset => {
   let res: Asset | undefined
