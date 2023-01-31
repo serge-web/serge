4868371f642656913693ebf3dc4ea260261b743a
Ian Mayo <ian@planetmayo.com>
2023-01-31 12:52:54 +0000

    show list of turns from backend

--- packages/components/src/local/organisms/support-panel/helpers/TurnFilter.tsx
+++ packages/components/src/local/organisms/support-panel/helpers/TurnFilter.tsx
@@ -1,16 +1,17 @@
-import { MenuItem, Select } from '@material-ui/core'
+import { Chip, MenuItem, Select } from '@material-ui/core'
+import { TurnPeriods } from '@serge/custom-types'
 import React, { ChangeEvent, CSSProperties } from 'react'
 import styles from '../styles.module.scss'
 
 type TurnFilterProps = {
   /** control label */
   label: string
-  /** the current turn number and (and the max value shown) */
-  currentTurn: number
   /** handler - with the turn to display (or -1 for all) */
   onChange: (turn: number) => void
   /** selected turn value */
   value: number
+  /** list of turns in this game */
+  allPeriods: TurnPeriods
 }
 
 export const SHOW_ALL_TURNS = -1
@@ -18,18 +19,23 @@ const style: CSSProperties = {
   fontSize: 14
 }
 
-const TurnFilter: React.FC<TurnFilterProps> = ({ label, currentTurn, value, onChange }) => {
+const TurnFilter: React.FC<TurnFilterProps> = ({ label, allPeriods, value, onChange }) => {
   const onLocalChange = (event: ChangeEvent<{ value: unknown }>) => {
     onChange(event.target.value as number)
   }
 
-  return <div className={styles['turn-dropdown']}>
+  if (allPeriods) {
+    return <div className={styles['turn-dropdown']}>
     <p>{label}</p>
     <Select value={value} style={style} onChange={onLocalChange}>
       <MenuItem style={style} value={SHOW_ALL_TURNS}>All turns</MenuItem>
-      {Array.from(Array(currentTurn + 1).keys()).map(turn => <MenuItem key={turn} style={style} value={turn}>{`Turn ${turn}`}</MenuItem>)}
+      {allPeriods.map(turn => <MenuItem key={turn.gameTurn} style={style} value={turn.gameTurn}>{`Turn ${turn.gameTurn}`}</MenuItem>)}
     </Select>
   </div>
+  }
+  else {
+    return <Chip label={'Waiting for list of turns'} />
+  }
 }
 
 export default TurnFilter
