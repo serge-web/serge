102c3d44703deb11261e80256a5f832e0090bc0f
Ian Mayo <ian@planetmayo.com>
2023-02-06 17:36:32 +0000

    linter fix

--- packages/components/src/local/organisms/planning-channel/index.tsx
+++ packages/components/src/local/organisms/planning-channel/index.tsx
@@ -197,7 +197,7 @@ export const PlanningChannel: React.FC<PropTypes> = ({
           rows: [],
           color: force.color
         }
-        res.push(item)  
+        res.push(item)
       }
     })
     const doRows = (rows: AssetRow[]) => {
