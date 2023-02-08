7b51caf482c685dac9cfa03e4a8aab631eb55c42
Ian Mayo <ian@planetmayo.com>
2023-02-08 10:23:54 +0000

    always provide default start/end dates for new orders

--- packages/components/src/local/organisms/support-panel/index.tsx
+++ packages/components/src/local/organisms/support-panel/index.tsx
@@ -259,11 +259,12 @@ export const SupportPanel: React.FC<PropTypes> = ({
     })
 
     // set the default dates, if this is a planning message
-    const docAsAny = document as any
-    if (docAsAny.ownAssets !== undefined) {
-      const plan = document as PlanningMessageStructureCore
-      if (gameDate && (!plan.startDate || !plan.endDate)) {
+    const plan = document as PlanningMessageStructureCore
+    if (gameDate) {
+      if (!plan.startDate) {
         plan.startDate = gameDate
+      }
+      if (!plan.endDate) {
         plan.endDate = gameDate
       }
     }
