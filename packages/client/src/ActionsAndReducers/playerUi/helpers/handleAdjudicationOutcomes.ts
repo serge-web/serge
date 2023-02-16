7e4d5c99b694dbe1ac98399c655b879d62787285
Ian Mayo <ian@planetmayo.com>
2023-02-16 20:50:14 +0000

    send interaction time

--- packages/client/src/ActionsAndReducers/playerUi/helpers/handleAdjudicationOutcomes.ts
+++ packages/client/src/ActionsAndReducers/playerUi/helpers/handleAdjudicationOutcomes.ts
@@ -1,9 +1,10 @@
 import { ForceData, InteractionDetails, MessageAdjudicationOutcomes } from '@serge/custom-types'
 import { handleOutcomes } from '@serge/helpers'
+import moment from 'moment'
 
 /** apply the adjudication outcomes to the game data
  * 
  */
 export default (interaction: InteractionDetails, payload: MessageAdjudicationOutcomes, allForces: ForceData[]): ForceData[] => {
-  return handleOutcomes(interaction, payload, allForces)
+  return handleOutcomes(interaction, payload, allForces, moment.utc(interaction.endTime).valueOf())
 }
