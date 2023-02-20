5d6d302d6c38e81890dd21f75db06a19f2b4fcb1
Ian Mayo <ian@planetmayo.com>
2023-02-20 10:44:52 +0000

    linter fix

--- packages/components/src/local/organisms/adjudication-messages-list/helpers/getNextInteraction.ts
+++ packages/components/src/local/organisms/adjudication-messages-list/helpers/getNextInteraction.ts
@@ -562,7 +562,7 @@ export const eventOutcomesFor = (plan: MessagePlanning, outcomes: MessageAdjudic
         force.assets && force.assets.forEach((asset) => {
           if (asset.attributes && airfields.includes(asset.attributes.a_Airfield as string)) {
             // TODO: check if the squadron are busy with other tasking (by searching through live orders)
-            squadronsAtAirfields.push({force, asset})
+            squadronsAtAirfields.push({ force, asset })
           }
         })
       })
