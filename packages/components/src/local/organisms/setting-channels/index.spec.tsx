c2c9e0f5d41946318fd987fca569cc1113fdc13b
Lilit Karapetyan <lilit.karapetyan.web@gmail.com>
2023-02-24 11:35:11 +0400

    to take templates from WargameExportedMock

--- packages/components/src/local/organisms/setting-channels/index.spec.tsx
+++ packages/components/src/local/organisms/setting-channels/index.spec.tsx
@@ -3,18 +3,18 @@ import React from 'react'
 import renderer from 'react-test-renderer'
 import SettingChannels from './index'
 import { ChannelTypes } from './types/props'
-import { WargameExportedMock, MessageTemplatesMock } from '@serge/mocks'
+import { WargameExportedMock } from '@serge/mocks'
 
 const onChange = (updates: { channels: Array<ChannelTypes> }): void => { console.log(updates.channels) }
-
+const { data } = WargameExportedMock
 describe('SettingChannels component:', () => {
   it('renders correctly', () => {
     const tree = renderer
       .create(<SettingChannels
-        channels={WargameExportedMock.data.channels.channels || []}
-        forces={WargameExportedMock.data.forces.forces}
+        channels={data.channels.channels || []}
+        forces={data.forces.forces}
         onChange={onChange}
-        messageTemplates={MessageTemplatesMock}
+        messageTemplates={data.templates.templates}
       />)
       .toJSON()
     expect(tree).toMatchSnapshot()
