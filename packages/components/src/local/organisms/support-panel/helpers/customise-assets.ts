12b4c5211cf8ce8edb4e996a29350c52e86ce9c9
Ian Mayo <ian@planetmayo.com>
2023-02-27 19:37:00 +0000

    use function for reduced code

--- packages/components/src/local/organisms/support-panel/helpers/customise-assets.ts
+++ packages/components/src/local/organisms/support-panel/helpers/customise-assets.ts
@@ -6,7 +6,8 @@ export const customiseAssets = (_document: MessageStructure | undefined, schema:
   const res = { ...schema }
   if (schema) {
     const oldOwnAssets = res.properties?.ownAssets?.items?.properties?.asset
-    const liveOwnAssets = ownAssets.filter((row) => row.health && row.health > 0)
+    const isAlive = (asset: AssetRow) => { return (asset.health == undefined) || asset.health > 0}
+    const liveOwnAssets = ownAssets.filter(isAlive)
     if (oldOwnAssets) {
       oldOwnAssets.enum = liveOwnAssets.map((asset: AssetRow) => asset.id)
       oldOwnAssets.options.enum_titles = liveOwnAssets.map((asset: AssetRow) => asset.name)
@@ -14,7 +15,7 @@ export const customiseAssets = (_document: MessageStructure | undefined, schema:
     const oldTargets = res.properties?.otherAssets?.items?.properties?.asset
     if (oldTargets) {
       // note. if this is an umpire, there aren't any other assets
-      const liveOtherAssets = otherAssets.filter((row) => row.health && row.health > 0)
+      const liveOtherAssets = otherAssets.filter(isAlive)
       const assetList = liveOtherAssets.length > 0 ? liveOtherAssets : liveOwnAssets
       oldTargets.enum = assetList.map((asset: AssetRow) => asset.id)
       oldTargets.options.enum_titles = assetList.map((asset: AssetRow) => asset.name)
