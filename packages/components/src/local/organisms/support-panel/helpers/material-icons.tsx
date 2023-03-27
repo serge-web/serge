6cf2be2871345b9aa10681303cf15cf88ecb3bdb
philb89 <everlasting2605@gmail.com>
2023-03-27 21:54:00 +0700

    2467: Run-time console errors/warnings

--- packages/components/src/local/organisms/support-panel/helpers/material-icons.tsx
+++ packages/components/src/local/organisms/support-panel/helpers/material-icons.tsx
@@ -1,16 +1,16 @@
 import { faChevronRight, faSearch, faSortUp, faSquare, faTimes } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import React from 'react'
+import React, { forwardRef } from 'react'
 
 /** centrally provide a set of local icon overrides for the
  * standard icons used in `material-table`
  *
  */
 export const materialIcons = {
-  Search: (): any => <FontAwesomeIcon title='Free text search' icon={faSearch} />,
-  ResetSearch: (): any => <FontAwesomeIcon title='Reset search' icon={faTimes} />,
-  Check: (): any => <FontAwesomeIcon title='Reset search' icon={faSquare} />,
-  DetailPanel: (): any => <FontAwesomeIcon title='Reset search' icon={faChevronRight} />,
-  SortArrow: (): any => <FontAwesomeIcon title='Sort' icon={faSortUp} />,
-  Filter: (): any => <FontAwesomeIcon title='Sort' icon={faSearch} />
+  Search: forwardRef(() => <FontAwesomeIcon title='Free text search' icon={faSearch} />),
+  ResetSearch: forwardRef(() => <FontAwesomeIcon title='Reset search' icon={faTimes} />),
+  Check: forwardRef(() => <FontAwesomeIcon title='Reset search' icon={faSquare} />),
+  DetailPanel: forwardRef(() => <FontAwesomeIcon title='Reset search' icon={faChevronRight} />),
+  SortArrow: forwardRef(() => <FontAwesomeIcon title='Sort' icon={faSortUp} />),
+  Filter: forwardRef(() => <FontAwesomeIcon title='Sort' icon={faSearch} />)
 }
