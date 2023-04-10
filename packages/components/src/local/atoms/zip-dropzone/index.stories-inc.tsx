15a211f399f04cfc5ee55fdd57dfc86ffcca8e5d
Lilit Karapetyan <lilit.karapetyan.web@gmail.com>
2023-04-10 14:36:57 +0400

    storybook cganges

---
+++ packages/components/src/local/atoms/zip-dropzone/index.stories-inc.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import ZipFileUploader from './index'
+import docs from './README.md'
+import Button from '../button'
+import { withKnobs } from '@storybook/addon-knobs'
+
+const wrapper: React.FC = (storyFn: any) => <div style={{ height: '600px', position: 'relative' }}>{storyFn()}</div>
+
+export default {
+  title: 'local/atoms/ZipDropzone',
+  component: ZipFileUploader,
+  decorators: [withKnobs, wrapper],
+  parameters: {
+    readme: {
+      // Show readme before story
+      content: docs
+    }
+  }
+}
+
+export const Default: React.FC = () => {
+  const handleChange = (data: any, filename: string): void => {
+    console.log(data)
+    console.log(filename)
+  }
+  return ( 
+    <ZipFileUploader onChange={handleChange} > 
+      <Button
+        color='secondary'>
+        Upload Zip
+      </Button>
+    </ZipFileUploader> 
+  )
+}
+
+
