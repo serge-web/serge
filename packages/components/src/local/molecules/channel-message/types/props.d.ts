1bcfccac3c768bb747b1ed8543b63bd04f92e337
Lilit Karapetyan <lilit.karapetyan.web@gmail.com>
2023-06-05 13:41:14 +0400

    fix typeError

--- packages/components/src/local/molecules/channel-message/types/props.d.ts
+++ packages/components/src/local/molecules/channel-message/types/props.d.ts
@@ -1,15 +1,15 @@
 import MessageListHeaderProps from '../../channel-message-header/types/props'
 import MessageListDetailProps from '../../channel-message-detail/types/props'
-import { MessageCustom, MessagePlanning } from '@serge/custom-types'
+import { MessageCustom } from '@serge/custom-types'
 
 export default interface Props extends MessageListHeaderProps, MessageListDetailProps {
   /**
    * Handle on reading detail
    */
-  onRead?: { (detail: MessageCustom | MessagePlanning): void }
+  onRead?: { (detail: MessageCustom): void }
 
   /**
    * handle unread message
    */
-  onUnread?: { (detail: MessageCustom | MessagePlanning): void }
+  onUnread?: { (detail: MessageCustom): void }
 }
