99045f9e365b82d34f15f854cbc2cecf8a76b2eb
Lilit Karapetyan <lilit.karapetyan.web@gmail.com>
2023-08-03 13:58:17 +0400

    add serverPath

--- client/src/Components/local/molecules/admin-panel-footer/index.tsx
+++ client/src/Components/local/molecules/admin-panel-footer/index.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 import Props from './types/props'
 /* Import Stylesheet */
 import styles from './styles.module.scss'
-
+import { serverPath } from 'src/config'
 /* Render component */
 export const AdminPanelFooter: React.FC<Props> = ({ force, selectedRoleName, byPassUrl, onIconClick }) =>
   <div className={styles['role-info']} data-tour='role-info-step' style={{ backgroundColor: force.color }}>
@@ -16,7 +16,7 @@ export const AdminPanelFooter: React.FC<Props> = ({ force, selectedRoleName, byP
       }
       <div className={styles['force-type']}>{force.name}</div>
     </div>
-    <img className={styles['role-icon']} src={force.iconURL || force.icon} alt="" onClick={onIconClick} data-tour="second-step" style={{ backgroundColor: force.color }} />
+    <img className={styles['role-icon']} src={serverPath + force.iconURL || force.icon} alt="" onClick={onIconClick} data-tour="second-step" style={{ backgroundColor: force.color }} />
   </div>
 
 export default AdminPanelFooter
--- client/src/Components/local/molecules/forces-in-channel/index.tsx
+++ client/src/Components/local/molecules/forces-in-channel/index.tsx
@@ -7,6 +7,7 @@ import React from 'react'
 import styles from './styles.module.scss'
 /* Import Types */
 import Props from './types/props'
+import { serverPath } from 'src/config'
 
 /* Render component */
 export const ForcesInChannel: React.FC<Props> = ({ icons, colors, names, onMarkAllAsRead, messages = [] }: Props) => {
@@ -22,7 +23,7 @@ export const ForcesInChannel: React.FC<Props> = ({ icons, colors, names, onMarkA
             style={{
               ...(colors[i] && { borderColor: colors[i], backgroundColor: colors[i] })
             }}
-            src={url} alt=""
+            src={serverPath + url} alt=""
           />
         })
       }
