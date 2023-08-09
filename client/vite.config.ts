228c3916da30f9ec7ea58ed128ea420dc43d61d3
Lilit Karapetyan <lilit.karapetyan.web@gmail.com>
2023-08-09 13:30:18 +0400

    add comments

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -10,23 +10,36 @@ import macrosPlugin from 'vite-plugin-babel-macros'
 import sass from 'sass'
 
 // https://vitejs.dev/config/
+// Load environment variables
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
 
   return {
+    // Prefix for environment variables
     envPrefix: 'REACT_APP',
-    plugins: [react(), checker({
-      overlay: { initialIsOpen: false },
-      typescript: true,
-      eslint: {
-        lintCommand: 'eslint "./src/**/*.{ts,tsx}"'
-      }
-    }), 
-    viteTsconfigPaths(), 
-    svgrPlugin(), 
-    envCompatible(),
-    macrosPlugin()
+
+    // Plugins for Vite
+    plugins: [
+      // Enable React support
+      react(),
+      // Plugin for type checking and linting
+      checker({
+        overlay: { initialIsOpen: false },
+        typescript: true,
+        eslint: {
+          lintCommand: 'eslint "./src/**/*.{ts,tsx}"'
+        }
+      }), 
+      // Plugin to handle TypeScript paths
+      viteTsconfigPaths(), 
+      // Plugin for handling SVG files as React components
+      svgrPlugin(), 
+      // Plugin for handling environment variables
+      envCompatible(),
+      macrosPlugin()
     ],
+
+    // CSS configuration
     css: {
       preprocessorOptions: {
         scss: {
@@ -34,25 +47,38 @@ export default defineConfig(({ mode }) => {
         }
       }
     },
+
+    // Build configuration
     build: {
       rollupOptions: {
         external: ['jss-plugin-{}', 'jss-plugin-window'] 
       },
       outDir: 'build'
     },
+
+    // Resolve configuration
     resolve: {
       alias: {
+        // Alias for node-fetch package
         'node-fetch': 'just-use-native-fetch',
+
+        // Alias for custom source paths
         // eslint-disable-next-line no-useless-escape
         '/^src\/(.*)/': path.resolve(__dirname, 'src/$1')
       }
     },
+
+    // Define global constants
     define: {
+      // Define environment variables for the app
       // 'process.env.REACT_APP_VERSION': JSON.stringify(env.REACT_APP_VERSION),
       'process.env.REACT_APP_SERVER_PATH': JSON.stringify(env.REACT_APP_SERVER_PATH),
       'process.env.JEST_WORKER_ID': env.JEST_WORKER_ID
     },
+
+    // Development server configuration
     server: {
+      // Open the browser automatically
       open: true,
       host: true,
       port: 3000
