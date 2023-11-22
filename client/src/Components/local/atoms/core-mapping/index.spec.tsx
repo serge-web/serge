c964f4ed0f05fad72f586879049d3e210d2f62de
Ian Mayo <ian@planetmayo.com>
2023-11-22 11:51:34 +0000

    introduce default props

--- client/src/Components/local/atoms/core-mapping/index.spec.tsx
+++ client/src/Components/local/atoms/core-mapping/index.spec.tsx
@@ -2,16 +2,30 @@
 import React from 'react'
 import renderer from 'react-test-renderer'
 import CoreMapping from './index'
+import { Phase } from 'src/config'
+import { CHANNEL_CORE_MAPPING, CoreMappingChannel } from 'src/custom-types/core-mapping'
 
 jest.mock('react-leaflet-v4', () => ({
   MapContainer: (): React.ReactElement => <></>,
   TileLayer: (): React.ReactElement => <></>
 }))
 
+const channel: CoreMappingChannel = {
+  uniqid: 'core',
+  name: 'core-mapping',
+  channelType: CHANNEL_CORE_MAPPING,
+  participants: [],
+  renderers: [],
+  constraints: {
+    bounds: [[1, 1], [2, 2]],
+    minZoom: 5
+  }
+}
+
 describe('Core Mapping component:', () => {
   it('renders correctly', () => {
     const tree = renderer
-      .create(<CoreMapping />)
+      .create(<CoreMapping playerForce={'f-red'} messages={[]} channel={channel} playerRole={'mgr'} currentTurn={1} forces={[]} currentPhase={Phase.Planning}/>)
       .toJSON()
     expect(tree).toMatchSnapshot()
   })
--- client/src/Components/local/atoms/core-mapping/index.stories-inc.tsx
+++ client/src/Components/local/atoms/core-mapping/index.stories-inc.tsx
@@ -2,6 +2,8 @@ import { withKnobs } from '@storybook/addon-knobs'
 import React from 'react'
 import CoreMapping from './index'
 import docs from './README.md'
+import { Phase } from 'src/config'
+import { CHANNEL_CORE_MAPPING, CoreMappingChannel } from 'src/custom-types/core-mapping'
 
 const wrapper: React.FC = (storyFn: any) => <div style={{ height: '600px', position: 'relative' }}>{storyFn()}</div>
 
@@ -17,8 +19,19 @@ export default {
   }
 }
 
+const channel: CoreMappingChannel = {
+  uniqid: 'core',
+  name: 'core-mapping',
+  channelType: CHANNEL_CORE_MAPPING,
+  participants: [],
+  renderers: [],
+  constraints: {
+    bounds: [[1, 1], [2, 2]],
+    minZoom: 5
+  }
+}
 export const Default: React.FC = () => {
   return (
-    <CoreMapping />
+    <CoreMapping playerForce={'f-red'} messages={[]} channel={channel} playerRole={'mgr'} currentTurn={1} forces={[]} currentPhase={Phase.Planning}/>
   )
 }
