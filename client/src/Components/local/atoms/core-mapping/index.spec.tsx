0be5c580b8eae69acc9e666d97860d48c2c75845
philb89 <everlasting2605@gmail.com>
2023-11-22 17:08:09 +0700

    fix unit test

--- client/src/Components/local/atoms/core-mapping/index.spec.tsx
+++ client/src/Components/local/atoms/core-mapping/index.spec.tsx
@@ -3,6 +3,11 @@ import React from 'react'
 import renderer from 'react-test-renderer'
 import CoreMapping from './index'
 
+jest.mock('react-leaflet-v4', () => ({
+  MapContainer: (): React.ReactElement => <></>,
+  TileLayer: (): React.ReactElement => <></>
+}))
+
 describe('Core Mapping component:', () => {
   it('renders correctly', () => {
     const tree = renderer
