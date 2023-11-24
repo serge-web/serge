513058a88801710d7cda7f7eee3e1ead55745cad
Ian Mayo <ian@planetmayo.com>
2023-11-24 11:04:26 +0000

    minor refactoring

--- client/src/Components/local/atoms/core-mapping/renderers/core-renderer.tsx
+++ client/src/Components/local/atoms/core-mapping/renderers/core-renderer.tsx
@@ -3,9 +3,11 @@ import React from 'react'
 import { GeoJSON } from 'react-leaflet-v4'
 import { RENDERER_CORE } from 'src/custom-types'
 import { CoreRendererProps } from '../types/props'
+import { Feature, Geometry } from 'geojson'
 
 export class CoreRenderer extends React.Component<CoreRendererProps> {
   render (): React.ReactElement {
-    return <GeoJSON data={this.props.features} filter={(feature): boolean => feature.properties._type === RENDERER_CORE} key={'feature_no_contact' + Math.random()}/>
+    const filter = (feature: Feature<Geometry, any>): boolean => feature.properties._type === RENDERER_CORE
+    return <GeoJSON data={this.props.features} filter={filter} key={'feature_no_contact' + Math.random()}/>
   }
 }
--- client/src/Components/local/atoms/core-mapping/renderers/milsymbol-renderer.tsx
+++ client/src/Components/local/atoms/core-mapping/renderers/milsymbol-renderer.tsx
@@ -3,9 +3,11 @@ import React from 'react'
 import { GeoJSON } from 'react-leaflet-v4'
 import { RENDERER_MILSYM } from 'src/custom-types'
 import { CoreRendererProps } from '../types/props'
+import { Feature, Geometry } from 'geojson'
 
 export class MilSymbolRenderer extends React.Component<CoreRendererProps> {
   render (): React.ReactElement {
-    return <GeoJSON data={this.props.features} filter={(feature): boolean => feature.properties._type === RENDERER_MILSYM} key={'feature_no_contact' + Math.random()}/>
+    const filter = (feature: Feature<Geometry, any>): boolean => feature.properties._type === RENDERER_MILSYM
+    return <GeoJSON data={this.props.features} filter={filter} key={'feature_no_contact' + Math.random()}/>
   }
 }
