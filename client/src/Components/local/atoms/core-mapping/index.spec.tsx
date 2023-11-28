85ce0e28da8efe66b5c365e8e6219bb87ff2c37b
Ian Mayo <ian@planetmayo.com>
2023-11-28 13:29:32 +0000

    provide missing prop

--- client/src/Components/local/atoms/core-mapping/index.spec.tsx
+++ client/src/Components/local/atoms/core-mapping/index.spec.tsx
@@ -26,10 +26,12 @@ const channel: CoreMappingChannel = {
   }
 }
 
+const bounds = L.latLngBounds(L.latLng(51.405, -0.02), L.latLng(51.605, -0.13))
+
 describe('Core Mapping component:', () => {
   it('renders correctly', () => {
     const tree = renderer
-      .create(<CoreMapping playerForce={'f-red'} messages={[]} channel={channel} playerRole={'mgr'} currentTurn={1} forces={[]} currentPhase={Phase.Planning}/>)
+      .create(<CoreMapping bounds={bounds} playerForce={'f-red'} messages={[]} channel={channel} playerRole={'mgr'} currentTurn={1} forces={[]} currentPhase={Phase.Planning}/>)
       .toJSON()
     expect(tree).toMatchSnapshot()
   })
