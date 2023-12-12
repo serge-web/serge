caa207365c747ecc8295a74d7212bcbb655a8912
philb89 <everlasting2605@gmail.com>
2023-12-12 11:06:58 +0700

    2704: implement basic UI for contorl panel

--- client/src/Components/local/atoms/mapping-panel/index.tsx
+++ client/src/Components/local/atoms/mapping-panel/index.tsx
@@ -1,34 +1,101 @@
-import React from 'react'
+import { faMinusCircle } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { Theme, makeStyles } from '@material-ui/core'
+import React, { ChangeEvent } from 'react'
 import { Panel, PanelGroup } from 'react-resizable-panels'
 import ResizeHandle from './helpers/ResizeHandle'
 import styles from './styles.module.scss'
 
+const useStyles = makeStyles((_: Theme) => ({
+  removeIcon: {
+    color: 'red'
+  }
+  
+}))
+
 export const MappingPanel = (): React.ReactElement => {
+  const classes = useStyles()
+  
+  const onFilter = (e: ChangeEvent<HTMLInputElement>) => {
+    console.log('filter: ', e.target.value)
+  }
+
   return (
     <PanelGroup className={styles.panelGroup} direction="vertical">
       
       <Panel
         collapsible={true}
-        defaultSizePercentage={20}
+        defaultSizePixels={150}
+        minSizePixels={150}
         order={1}
         className={styles.filterPanel}
       >
-        <div>Filters</div>
+        <div className={styles.header}>Filters</div>
+        <div className={styles.itemsBox}>
+          <p>Name:</p>
+          <div>
+            <input onChange={onFilter} placeholder='Headquarters' />
+          </div>
+          <FontAwesomeIcon icon={faMinusCircle} className={classes.removeIcon} />
+        </div>
+        <div className={styles.itemsBox}>
+          <p>Important:</p>
+          <div>
+            <select>
+              <option value='opt1'>Option 1</option>
+              <option value='opt2'>Option 2</option>
+            </select>
+          </div>
+          <FontAwesomeIcon icon={faMinusCircle} className={classes.removeIcon} />
+        </div>
+        <div className={styles.button}>
+          <button>Add</button>
+        </div>
       </Panel>
       <ResizeHandle />
       
       <Panel collapsible={true} order={2} className={styles.itemsPanel}>
-        <div>Items</div>
+        <div className={styles.header}>Items</div>
       </Panel>
       <ResizeHandle />
       
       <Panel
         collapsible={true}
-        defaultSizePercentage={20}
+        defaultSizePixels={190}
+        minSizePixels={190}
         order={3}
         className={styles.propertiesPanel}
       >
-        <div>bottom</div>
+        <div className={styles.header}>Properties</div>
+        <div className={styles.itemsBox}>
+          <p>Name:</p>
+          <div>
+            <input onChange={onFilter} placeholder='Headquarters' />
+          </div>
+        </div>
+        <div className={styles.itemsBox}>
+          <p>Important:</p>
+          <div>
+            <select>
+              <option value='yes'>Yes</option>
+              <option value='no'>No</option>
+            </select>
+          </div>
+        </div>
+        <div className={styles.itemsBox}>
+          <p>Size:</p>
+          <div>
+            <select>
+              <option value='small'>Small</option>
+              <option value='medium'>Medium</option>
+              <option value='large'>Large</option>
+            </select>
+          </div>
+        </div>
+        <div className={styles.button}>
+          <button>Cancel</button>
+          <button>Save</button>
+        </div>
       </Panel>
       
     </PanelGroup>
