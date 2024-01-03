dee0efb9d3a39f3a240ebf2a7cf27065a25151db
Ian Mayo <ian@planetmayo.com>
2024-01-03 16:07:42 +0000

    more tidying

--- client/src/config/enums.ts
+++ client/src/config/enums.ts
@@ -1,6 +1,5 @@
 /**
  * list the possible phases in a wargame
- * TODO: the concept of phase isn't restricted to the mapping component.  It should be declared higher up the tree
  */
 export enum Phase {
   /* players are planning their next turn
