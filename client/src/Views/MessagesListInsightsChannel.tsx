b5157fa13b970295f67df4f7a805c2d8e3d4041b
Lilit Karapetyan <lilit.karapetyan.web@gmail.com>
2024-01-29 16:45:59 +0400

    convert to tsx

--- client/src/Views/MessagesListInsightsChannel.tsx
+++ client/src/Views/MessagesListInsightsChannel.tsx
@@ -1,18 +1,21 @@
 import { faEnvelope, faEnvelopeOpen } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { formatFullDate, isMessageReaded } from 'src/Helpers'
+import { MessageFeedback } from 'src/custom-types'
 import 'src/themes/App.scss'
 import classNames from 'classnames'
 import React from 'react'
 import Badge from 'react-bootstrap/Badge'
 import { usePlayerUiState } from '../Store/PlayerUi'
 
-const MessagesListChatChannel = ({ messages, markAllAsRead }) => {
+interface Props { messages: MessageFeedback[], markAllAsRead: () => void }
+
+const MessagesListChatChannel: React.FC<Props> = ({ messages, markAllAsRead }) => {
   const { currentWargame, selectedForce, selectedRole } = usePlayerUiState()
   const selectedForceId = selectedForce ? selectedForce.uniqid : ''
-  const isMessageRead = (message) => isMessageReaded(currentWargame, selectedForceId, selectedRole, message._id)
+  const isMessageRead = (message: MessageFeedback) => isMessageReaded(currentWargame, selectedForceId, selectedRole, message._id)
   const isMessageUnreadExists = messages.some(message => !isMessageRead(message))
-
+  
   return (
     <>
       <span className='message-status-icon'>
@@ -23,7 +26,7 @@ const MessagesListChatChannel = ({ messages, markAllAsRead }) => {
           <React.Fragment key={`feedback${i}`}>
             <div className='info-wrap'>
               <Badge pill variant='primary'>{message.details.from.force}</Badge>
-              <Badge pill variant='secondary'>{message.details.from.role}</Badge>
+              <Badge pill variant='secondary'>{message.details.from.roleName}</Badge>
               {message.name && <Badge pill variant='warning'>{message.name}</Badge>}
               <span>{formatFullDate(message.details.timestamp)}</span>
             </div>
