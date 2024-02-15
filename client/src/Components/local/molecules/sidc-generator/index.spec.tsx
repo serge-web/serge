47e3ecfca64bf67d4e84961128c95b0a7d3a325a
Lilit Karapetyan <lilit.karapetyan.web@gmail.com>
2024-02-15 13:26:04 +0400

    fixed snap

--- client/src/Components/local/molecules/sidc-generator/index.spec.tsx
+++ client/src/Components/local/molecules/sidc-generator/index.spec.tsx
@@ -1,7 +1,5 @@
 import React from 'react'
 import renderer from 'react-test-renderer'
-import { ThemeProvider } from '@material-ui/core/styles'
-import useStyles from './helpers/SidcGeneratorStyles'
 import { CUSTOM_SIDC } from 'src/config'
 
 import SIDCGenerator from './index'
@@ -9,9 +7,7 @@ import SIDCGenerator from './index'
 const onSave = (originalNumber: string): void => console.log(originalNumber)
 it('SIDCGenerator Component', () => {
   const tree = renderer.create(
-    <ThemeProvider theme={useStyles}>
-      <SIDCGenerator onSave={onSave} sidcValue={CUSTOM_SIDC} />
-    </ThemeProvider>
+    <SIDCGenerator onSave={onSave} sidcValue={CUSTOM_SIDC} />
   ).toJSON()
 
   expect(tree).toMatchSnapshot()
