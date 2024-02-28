3cf8919f643cb4857c3aab410816b364d0c72615
Lilit Karapetyan <lilit.karapetyan.web@gmail.com>
2024-02-28 10:21:00 +0400

    show side by side

--- client/src/Components/local/molecules/sidc-generator/styles.module.scss.d.ts
+++ client/src/Components/local/molecules/sidc-generator/styles.module.scss.d.ts
@@ -2,6 +2,7 @@ interface CssExports {
     'form': string;
     'label': string;
     'item': string;
+    'value': string;
   }
   export const cssExports: CssExports;
   export default cssExports;
