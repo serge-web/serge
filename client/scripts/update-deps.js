852e8afb2fd5038b04997a495af11b765362eb81
philb89 <everlasting2605@gmail.com>
2024-03-09 12:20:35 +0700

    update the way how to load leaflet ruler in static web

--- client/scripts/update-deps.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-const fs = require('fs')
-const path = require('path')
-
-const filePath = path.resolve(__dirname, '../build/index.html')
-let index = fs.readFileSync(filePath, 'utf-8')
-index = index.replace('</head>', '\t<link rel="stylesheet" href="/leaflet/ruler/leaflet.ruler.css" />\n</head>').replace("<div id='root'></div>", '<script type="module" src="/leaflet/ruler/leaflet.ruler.js"></script>\n\t<div id="root"></div>')
-fs.writeFileSync(filePath, index)
