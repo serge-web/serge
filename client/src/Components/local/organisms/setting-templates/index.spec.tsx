3fc17d420c57185d480b60c7885492f680839a7c
Lilit Karapetyan <lilit.karapetyan.web@gmail.com>
2024-03-12 13:15:26 +0400

    fixed spec

--- client/src/Components/local/organisms/setting-templates/index.spec.tsx
+++ client/src/Components/local/organisms/setting-templates/index.spec.tsx
@@ -1,14 +1,28 @@
 import React from 'react'
-import renderer from 'react-test-renderer'
+import renderer, { act } from 'react-test-renderer'
 import { MessageTemplatesMock } from 'src/mocks'
 import { TemplateBody } from 'src/custom-types'
 import SettingForces from './index'
 
-const onChange = (updates: { templates: Array<TemplateBody> }): void => console.log({ 'update template data': updates })
+const onChange = (updates: { templates: Array<TemplateBody> }): void =>
+  console.log({ 'update template data': updates })
 
 const onSave = (): void => {
   console.log('Your save logic', MessageTemplatesMock)
 }
+
+jest.mock('@ginkgo-bioworks/react-json-schema-form-builder', () => ({
+  FormBuilder: ({ schema, uischema, onChange }: any) => {
+    console.log('schema', schema)
+    console.log('uischema', uischema)
+    act(() => {
+      onChange('{}', '{}') 
+    })
+
+    // Return a placeholder component
+    return <div data-testid="form-builder-mock" />
+  }
+}))
 
 describe('SettingForces component:', () => {
   it('renders correctly', () => {
@@ -18,6 +32,8 @@ describe('SettingForces component:', () => {
         { createNodeMock: () => document.createElement('textarea') }
       )
       .toJSON()
+
+    // Expectations go here
     expect(tree).toMatchSnapshot()
   })
 })
