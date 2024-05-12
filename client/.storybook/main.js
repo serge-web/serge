438763fb79b17dbf2134c1becbca840f92d3f9e7
philb89 <everlasting2605@gmail.com>
2024-05-12 10:34:44 +0700

    update loading style in storybook

--- client/.storybook/main.js
+++ client/.storybook/main.js
@@ -12,7 +12,6 @@ module.exports = {
     '@storybook/addon-interactions',
     '@storybook/preview-api',
     '@storybook/addon-controls',
-    '@storybook/preset-scss',
     '@storybook/addon-webpack5-compiler-swc',
     '@chromatic-com/storybook'
   ],
@@ -31,6 +30,35 @@ module.exports = {
           '@babel/plugin-proposal-nullish-coalescing-operator',
         ],
       },
+    },
+    {
+      test: /\.module.scss$/,
+      use: [
+        require.resolve('style-loader'),
+        {
+          loader: 'css-modules-typescript-loader',
+          options: {
+            mode: process.env.CI ? 'verify' : 'emit'
+          }
+        },
+        {
+          loader: require.resolve('css-loader'),
+          options: {
+            importLoaders: 1,
+            modules: {
+              localIdentName: 'c-[hash:base64:5]__[folder]--[local]'
+            }
+          }
+          },
+          {
+            loader: require.resolve('sass-loader'),
+            options: {
+            // Prefer `dart-sass`
+            implementation: require('sass'),
+          }
+        }
+      ],
+      include: path.resolve(__dirname, '../'),
     },
     {
       test: /\.(png|jpe?g|gif)$/i,
