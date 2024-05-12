b2f560c4e759d5d27b5862abd2aa70d6dd7854a4
philb89 <everlasting2605@gmail.com>
2024-05-12 10:59:59 +0700

    clean code

--- client/.storybook/main.ts
+++ client/.storybook/main.ts
@@ -1,99 +1,89 @@
-import path from 'path';
+import path from "path";
 
 export default {
-  stories: [
-    '../src/**/*.stories.tsx'
-  ],
-  staticDirs: ['../static'],
+  stories: ["../src/**/*.stories.tsx"],
+  staticDirs: ["../static"],
   addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
-    '@storybook/preview-api',
-    '@storybook/addon-controls',
-    '@storybook/addon-webpack5-compiler-swc',
-    '@chromatic-com/storybook'
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    "@storybook/preview-api",
+    "@storybook/addon-controls",
+    "@storybook/addon-webpack5-compiler-swc",
+    "@chromatic-com/storybook",
   ],
-  webpackFinal: async config => {
+  webpackFinal: async (config) => {
     config.module.rules.push(
-    {
-      test: /\.(js|jsx)$/,
-      loader: require.resolve('babel-loader'),
-      options: {
-        presets: [
-          '@babel/preset-env',
-          '@babel/preset-react',
-        ],
-        plugins: [
-          '@babel/plugin-proposal-nullish-coalescing-operator',
-        ],
-      },
-    },
-    {
-      test: /\.module.scss$/,
-      use: [
-        require.resolve('style-loader'),
-        {
-          loader: 'css-modules-typescript-loader',
-          options: {
-            mode: process.env.CI ? 'verify' : 'emit'
-          }
+      {
+        test: /\.(js|jsx)$/,
+        loader: require.resolve("babel-loader"),
+        options: {
+          presets: ["@babel/preset-env", "@babel/preset-react"],
+          plugins: ["@babel/plugin-proposal-nullish-coalescing-operator"],
         },
-        {
-          loader: require.resolve('css-loader'),
-          options: {
-            importLoaders: 1,
-            modules: {
-              localIdentName: 'c-[hash:base64:5]__[folder]--[local]'
-            }
-          }
+      },
+      {
+        test: /\.module.scss$/,
+        use: [
+          require.resolve("style-loader"),
+          {
+            loader: "css-modules-typescript-loader",
+            options: {
+              mode: process.env.CI ? "verify" : "emit",
+            },
           },
           {
-            loader: require.resolve('sass-loader'),
+            loader: require.resolve("css-loader"),
             options: {
-            // Prefer `dart-sass`
-            implementation: require('sass'),
-          }
-        }
-      ],
-      include: path.resolve(__dirname, '../'),
-    },
-    {
-      test: /\.(png|jpe?g|gif)$/i,
-      use: [
-        {
-          loader: 'file-loader',
-          options: {
-            outputPath: 'static/assets/',
-            publicPath: 'static/assets/'
-          }
-        },
-      ],
-    },
-    {
-      test: /\.(woff|woff2|eot|ttf)$/,
-      use: [
-        {
-          loader: 'file-loader',
-          options: {
-            name: '[name].[ext]'
-          }
-        }
-      ]
-    }
+              importLoaders: 1,
+              modules: {
+                localIdentName: "c-[hash:base64:5]__[folder]--[local]",
+              },
+            },
+          },
+          {
+            loader: require.resolve("sass-loader"),
+            options: {
+              // Prefer `dart-sass`
+              implementation: require("sass"),
+            },
+          },
+        ],
+        include: path.resolve(__dirname, "../"),
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              outputPath: "static/assets/",
+              publicPath: "static/assets/",
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+            },
+          },
+        ],
+      }
     );
-    config.resolve.alias['src'] = path.resolve(__dirname, '../src');
-    config.resolve.extensions.push('.ts', '.tsx', '.md');
+    config.resolve.alias["src"] = path.resolve(__dirname, "../src");
+    config.resolve.extensions.push(".ts", ".tsx", ".md");
     return config;
   },
-
   framework: {
-    name: '@storybook/react-webpack5',
-    options: {}
+    name: "@storybook/react-webpack5",
+    options: {},
   },
-
   docs: {
-    autodocs: true
+    autodocs: true,
   },
 };
-
