ee14927cf56195d9065729749981028b9a79c575
philb89 <everlasting2605@gmail.com>
2024-06-03 15:12:13 +0700

    Implement logic for participants tab

--- client/src/Components/local/organisms/setting-channels/helpers/coreMapping.tsx
+++ client/src/Components/local/organisms/setting-channels/helpers/coreMapping.tsx
@@ -1,4 +1,5 @@
 import {
+  faCheck,
   faEdit,
   faTrash
 } from '@fortawesome/free-solid-svg-icons'
@@ -23,9 +24,8 @@ import {
 } from '@material-ui/core'
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown'
 import PopupState, { bindMenu, bindTrigger } from 'material-ui-popup-state'
-import React from 'react'
+import React, { ChangeEvent } from 'react'
 import { Column } from 'src/Components/local/react-table/types/props'
-import { PropertyType } from 'src/custom-types'
 import styles from '../styles.module.scss'
 
 export const CoreMappingTabs = ['Map', 'Renderers', 'Participants']
@@ -55,12 +55,16 @@ export const EditParticipantColumns: readonly Column[] = [
   { id: 'action', label: '' }
 ]
 export const ZoomOptions = Array.from(Array(20).keys()).map(v => v + 1)
+export const RendererOptions = ['core', 'milSym']
+export const PhaseOptions = ['planning', 'adjudication']
 
-type SimpleTableProps = {
-  columns: readonly Column[]
-  data: any[]
-  onEdit: (row: PropertyType) => void
-  onRemove: (row: PropertyType) => void
+export type EditParticipantType = {
+  tableData: any[]
+  force: string
+  roles: string[]
+  phases: string[]
+  renderers: string[]
+  id: string
 }
 
 export type ButtonOptions = {
@@ -68,7 +72,61 @@ export type ButtonOptions = {
   label: string
 }
 
-export const SimpleTable: React.FC<SimpleTableProps> = ({ columns, data, onEdit, onRemove }) => {
+type RenderElmProps = {
+  prop: any
+  ckey: string
+  rowId?: number
+  onChange?: (data: any) => void
+}
+
+type SimpleTableProps = {
+  columns: readonly Column[]
+  data: any[]
+  editInline?: number
+  forceNames?: string[]
+  onEdit?: (row: any) => void
+  onRemove: (row: any) => void
+  onChange?: (data: any) => void
+}
+
+export const SimpleTable: React.FC<SimpleTableProps> = ({ columns, data, editInline, forceNames = [], onEdit, onRemove, onChange }) => {
+  const ArrayElm: React.FC<RenderElmProps> = ({ prop, ckey }) => {
+    return prop[ckey].map((value: any) => (value && <Chip key={value} label={value} className={styles.tableCellText} />))
+  }
+
+  const CheckboxElm: React.FC<RenderElmProps> = ({ prop, ckey, rowId, onChange }) => {
+    const onElmChange = (_: ChangeEvent<HTMLInputElement>, checked: boolean) => {
+      prop[ckey] = checked
+      onChange && onChange(prop)
+    }
+
+    return <FormControlLabel
+      style={{ marginLeft: '0' }}
+      control={<Checkbox checked={prop[ckey]} onChange={onElmChange}/>} label={undefined} disabled={editInline !== rowId}
+    />
+  }
+
+  const TextElm: React.FC<RenderElmProps> = ({ prop, ckey, rowId, onChange }) => {
+    const elm = prop[ckey]
+    if (!elm) {
+      return <></>
+    }
+
+    const onElmChange = (event: React.ChangeEvent<{ name?: string | undefined, value: unknown }>) => {
+      if (ckey === 'permissionOnForce' && data.find(d => d[ckey] === event.target.value)) {
+        alert(`This force name "${event.target.value}" is in use, please choose another one`)
+        return
+      }
+      prop[ckey] = event.target.value
+      onChange && onChange(prop)
+    }
+
+    if (editInline === rowId && ckey === 'permissionOnForce') {
+      return <SimpleSelect title='' value={elm} options={forceNames} labelWidth="10px" onChange={onElmChange} />
+    }
+    return <Chip label={elm} className={styles.tableCellText} />
+  }
+
   return <TableContainer component={Paper} style={{ maxHeight: '400px' }}>
     <Table stickyHeader>
       <TableHead>
@@ -89,22 +147,25 @@ export const SimpleTable: React.FC<SimpleTableProps> = ({ columns, data, onEdit,
           <TableRow key={idx} style={{ cursor: 'pointer' }}>
             {Object.keys(prop).map(key => (key !== 'id' && <TableCell key={key} style={{ padding: '4px' }}>
               {Array.isArray(prop[key])
-                ? <>
-                  {prop[key].map((value: any) => (value && <Chip key={value} label={value} className={styles.tableCellText} />))}
-                </>
+                ? <ArrayElm prop={prop} ckey={key} />
                 : typeof prop[key] === 'boolean'
-                  ? <FormControlLabel
-                    style={{ marginLeft: '0' }}
-                    control={<Checkbox checked={prop[key]} />} label={undefined} disabled
-                  />
-                  : prop[key] && <Chip label={prop[key]} className={styles.tableCellText} />}
-              
+                  ? <CheckboxElm prop={prop} ckey={key} rowId={prop.id} onChange={onChange}/>
+                  : <TextElm prop={prop} ckey={key} rowId={prop.id} onChange={onChange}/>
+              }
             </TableCell>))}
             <TableCell align="right" style={{ minWidth: '70px' }}>
               <FontAwesomeIcon
-                icon={faEdit}
+                icon={editInline === prop.id ? faCheck : faEdit}
                 style={{ marginRight: '5px' }}
-                onClick={() => onEdit(prop)}
+                onClick={() => {
+                  if (onEdit) {
+                    if (Number.isInteger(editInline) && editInline !== -1) {
+                      onEdit({ id: -1 })
+                    } else {
+                      onEdit(prop)
+                    }
+                  }
+                }}
               />
               <FontAwesomeIcon icon={faTrash} onClick={() => onRemove(prop)}/>
             </TableCell>
@@ -147,21 +208,25 @@ export const AddButton: React.FC<{ className?: string, options: ButtonOptions[],
 
 export const SimpleSelect: React.FC<{
   title: string
-  value?: number | string
+  value?: number | string | number[] | string[]
   options: number[] | string[]
   labelWidth: string
   width?: string
+  multiple?: boolean
   onChange: (
     event: React.ChangeEvent<{ name?: string | undefined, value: unknown }>,
     child: React.ReactNode
   ) => void
-}> = ({ title, options, value, labelWidth, width, onChange }) =>
+}> = ({ title, options, value, labelWidth, width, onChange, multiple }) =>
   <Box className={styles.mapFieldItem} sx={{ width }}>
     <InputLabel variant="standard" style={{ minWidth: labelWidth }}>{title}</InputLabel>
-    <Select fullWidth onChange={onChange} value={value}>
+    <Select fullWidth onChange={onChange} value={value} multiple={multiple} renderValue={value => <>{Array.isArray(value) ? value.join(', ') : value}</>}>
       {options.map((option) => (
         <MenuItem key={option} value={option}>
-          {option}
+          {multiple ? <FormControlLabel
+            style={{ marginLeft: '0' }}
+            control={<Checkbox checked={(value as any)?.includes(option)} />} label={option}
+          /> : option}
         </MenuItem>
       ))}
     </Select>
