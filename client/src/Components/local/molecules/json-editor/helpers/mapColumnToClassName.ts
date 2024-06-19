6332f17d486467a1ad33dc425ee64414cd2043db
Lilit Karapetyan <lilit.karapetyan.web@gmail.com>
2024-06-19 14:41:32 +0400

    Added comments explaining the function

--- client/src/Components/local/molecules/json-editor/helpers/mapColumnToClassName.ts
+++ client/src/Components/local/molecules/json-editor/helpers/mapColumnToClassName.ts
@@ -1,5 +1,32 @@
 type UiSchema = Record<string, any>;
 
+/**
+ * Transforms the uiSchema by mapping 'ui:column' to 'ui:className' for each field.
+ * 
+ * Before:
+ * {
+ *   newInput1: {
+ *     'ui:column': '6'
+ *   },
+ *   anotherInput: {
+ *     'ui:column': '4'
+ *   }
+ * }
+ * 
+ * After:
+ * {
+ *   newInput1: {
+ *     'ui:className': 'col-6'
+ *   },
+ *   anotherInput: {
+ *     'ui:className': 'col-4'
+ *   }
+ * }
+ * 
+ * @param uiSchema - The original uiSchema object
+ * @returns The transformed uiSchema with 'ui:column' replaced by 'ui:className'
+ */
+
 const mapColumnToClassName = (uiSchema: UiSchema): UiSchema => {
   const newUiSchema: UiSchema = {}
 
