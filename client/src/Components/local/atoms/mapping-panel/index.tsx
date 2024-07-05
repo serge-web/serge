1b96c9fc28c129a4586995f53719f147230fbc01
philb89 <everlasting2605@gmail.com>
2024-07-05 20:57:20 +0700

    update fixes some minor issues

--- client/src/Components/local/atoms/mapping-panel/index.tsx
+++ client/src/Components/local/atoms/mapping-panel/index.tsx
@@ -145,7 +145,7 @@ export const MappingPanel: React.FC<MappingPanelProps> = ({ onClose, features, r
         result[propKey] = {
           value: properties[propKey] as any,
           choices: get(extraProps, 'choices', []),
-          disabled: onlyEditOwnProps && extraProps?.playerEditable !== undefined && extraProps?.playerEditable === false
+          disabled: onlyEditOwnProps && !extraProps?.playerEditable
         }
         return result
       }, {})
