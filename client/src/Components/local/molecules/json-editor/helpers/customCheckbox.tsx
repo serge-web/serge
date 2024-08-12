42216b95ff3d24cbe0493ab10d81f2894b4b4958
Lilit Karapetyan <lilit.karapetyan.web@gmail.com>
2024-08-12 11:59:53 +0400

    fixed: Description of checkbox is not showed in 'Preview Form' and GamePlay

--- client/src/Components/local/molecules/json-editor/helpers/customCheckbox.tsx
+++ client/src/Components/local/molecules/json-editor/helpers/customCheckbox.tsx
@@ -2,10 +2,14 @@ import React from 'react'
 import { WidgetProps } from '@rjsf/utils'
 
 const CustomCheckbox = (props: WidgetProps): React.JSX.Element => {
+  const { schema } = props
   return (
     <>
-      <input type='checkbox' className={props.value ? 'checked' : 'unchecked'} checked={props.value} required={props.required} onClick={() => props.onChange(!props.value)} />
-      <label>{props.label}</label> 
+      <div className='form-label'>
+        <input type='checkbox' className={props.value ? 'checked' : 'unchecked'} checked={props.value} required={props.required} onClick={() => props.onChange(!props.value)} />
+        <label>{props.label}</label>
+      </div> 
+      <small>{schema.description}</small> 
     </>
   )
 }
